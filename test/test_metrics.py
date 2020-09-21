7a68214aafaede6b866f9eab5131fcd8250ee327
Clark Chin <xi2.chen@intel.com>
2020-09-21 14:47:36 +0800

    [green] metric topk refactor     1. support 1x1 label type int     2. support Nxclass_num(2 axis) and class_num(1 axis) preds input     3. support on-hot Nxclass_num(2 axis) and N(or Nx1) labels

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -17,6 +17,44 @@ class TestMetrics(unittest.TestCase):
         acc_result = acc.result()
         self.assertEqual(acc_result, 0.5)
 
+    def test_tensorflow_topk(self):
+        metrics = METRICS('tensorflow')
+        top1 = metrics['topk']()
+        top2 = metrics['topk'](k=2)
+        top3 = metrics['topk'](k=3)
+
+        predicts = [[0, 0.2, 0.9, 0.3], [0, 0.9, 0.8, 0]]
+        single_predict = [0, 0.2, 0.9, 0.3]
+       
+        labels = [[0, 1, 0, 0], [0, 0, 1, 0]]
+        sparse_labels = [2, 2]
+        single_label = 2
+
+        # test funtionality of one-hot label
+        top1.update(predicts, labels)
+        top2.update(predicts, labels)
+        top3.update(predicts, labels)
+        self.assertEqual(top1.result(), 0.0)
+        self.assertEqual(top2.result(), 0.5)
+        self.assertEqual(top3.result(), 1)
+
+        # test funtionality of sparse label
+        top1.update(predicts, sparse_labels)
+        top2.update(predicts, sparse_labels)
+        top3.update(predicts, sparse_labels)
+        self.assertEqual(top1.result(), 0.25)
+        self.assertEqual(top2.result(), 0.75)
+        self.assertEqual(top3.result(), 1)
+
+        # test funtionality of single label
+        top1.update(single_predict, single_label)
+        top2.update(single_predict, single_label)
+        top3.update(single_predict, single_label)
+        self.assertEqual(top1.result(), 0.4)
+        self.assertEqual(top2.result(), 0.8)
+        self.assertEqual(top3.result(), 1)
+        
+
     def test_pytorch_accuracy(self):
         metrics = METRICS('pytorch')
         acc = metrics['Accuracy']()
