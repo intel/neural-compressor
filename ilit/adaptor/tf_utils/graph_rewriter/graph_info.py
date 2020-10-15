e54e856b5644eea5ac38ef1f888802fc59f902c7
Zhang, Guoming <guoming.zhang@intel.com>
2020-10-15 15:13:59 +0800

    Add the missing comma that blocks the tpe ut.

--- ilit/adaptor/tf_utils/graph_rewriter/graph_info.py
+++ ilit/adaptor/tf_utils/graph_rewriter/graph_info.py
@@ -6,10 +6,10 @@
 class TFLowbitPrecisionPatterns(object):
     patterns = {
         "2.1.0": [[["ConcatV2"]], [["MaxPool", "AvgPool"]], [["MatMul"], ("BiasAdd")],
-                  [["Conv2D", "DepthwiseConv2dNative"], ("BiasAdd"), ("Add", "AddN", "AddV2"),
+                  [["Conv2D", "DepthwiseConv2dNative"], ("BiasAdd",), ("Add", "AddN", "AddV2"),
                    ("Relu", "Relu6")]],
         "default": [[["ConcatV2"]], [["MaxPool", "AvgPool"]], [["MatMul"], ("BiasAdd")],
-                    [["Conv2D", "DepthwiseConv2dNative"], ("BiasAdd"), ("Add", "AddN", "AddV2"),
+                    [["Conv2D", "DepthwiseConv2dNative"], ("BiasAdd",), ("Add", "AddN", "AddV2"),
                      ("Relu", "Relu6")]],
     }
 
