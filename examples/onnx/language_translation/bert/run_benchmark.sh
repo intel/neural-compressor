42921a6d16acd4a0a1b181c3b08736fdb857eb0e
Meng, Hengyu <hengyu.meng@intel.com>
2020-12-07 16:00:01 -0800

    update bash scripts for ort-support on linux

--- examples/onnx/language_translation/bert/run_benchmark.sh
+++ examples/onnx/language_translation/bert/run_benchmark.sh
@@ -24,6 +24,9 @@ function init_params {
       --config=*)
           bert_yaml=$(echo $var |cut -f2 -d=)
       ;;
+      --output_model=*)
+          output_model=$(echo $var |cut -f2 -d=)
+      ;;
     esac
   done
 
@@ -32,10 +35,10 @@ function init_params {
 # run_benchmark
 function run_benchmark {
     if [ "${topology}" = "bert_base_MRPC" ];then
-      task_name='MRPC'
+      task_name='mrpc'
       model_name_or_path='bert-base-uncased'
-
-    python main.py --model_path ${input_model} --data_dir ${data_dir} --task_name ${task_name} --input_dir ${model_name_or_path} --benchmark --config ${bert_yaml}
+    fi
+    python bert_base.py --model_path ${input_model} --data_dir ${data_dir} --task_name ${task_name} --input_dir ${model_name_or_path} --benchmark --config ${bert_yaml} --output_model ${output_model}
 }
 
 main "$@"
--- examples/onnx/language_translation/bert/run_tuning.sh
+++ examples/onnx/language_translation/bert/run_tuning.sh
@@ -24,6 +24,9 @@ function init_params {
       --config=*)
           bert_yaml=$(echo $var |cut -f2 -d=)
       ;;
+      --output_model=*)
+          output_model=$(echo $var |cut -f2 -d=)
+      ;;
     esac
   done
 
@@ -32,10 +35,11 @@ function init_params {
 # run_benchmark
 function run_benchmark {
     if [ "${topology}" = "bert_base_MRPC" ];then
-      task_name='MRPC'
+      task_name='mrpc'
       model_name_or_path='bert-base-uncased'
+    fi 
 
-    python main.py --model_path ${input_model} --data_dir ${data_dir} --task_name ${task_name} --input_dir ${model_name_or_path} --tune --config ${bert_yaml}
+    python bert_base.py --model_path ${input_model} --data_dir ${data_dir} --task_name ${task_name} --input_dir ${model_name_or_path} --tune --config ${bert_yaml} --output_model ${output_model}
 }
 
 main "$@"
