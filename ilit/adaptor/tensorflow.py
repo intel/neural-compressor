859e25797894ecf3c9f8d052e6caa099709477df
Wang, Chuanqi <chuanqi.wang@intel.com>
2020-12-11 18:54:48 +0800

    fix pylint

--- ilit/adaptor/tensorflow.py
+++ ilit/adaptor/tensorflow.py
@@ -165,14 +165,16 @@ class TensorFlowAdaptor(Adaptor):
             for node in graph_def.node:
                 if node.op in inspect_node_types:
                     fp32_inspect_node_name.append(node.name)
-                # Tensor dump supported quantized op including, 
-                # Requantize, QuantizedConv2DAndRequantize, QuantizedConv2DAndReluAndRequantize,
-                # QuantizedConv2DWithBiasAndRequantize, 
+                # Tensor dump supported quantized op including,
+                # Requantize, QuantizedConv2DAndRequantize,
+                # QuantizedConv2DAndReluAndRequantize,
+                # QuantizedConv2DWithBiasAndRequantize,
                 # QuantizedConv2DWithBiasAndReluAndRequantize,
-                # QuantizedConv2DWithBiasSignedSumAndReluAndRequantize, 
+                # QuantizedConv2DWithBiasSignedSumAndReluAndRequantize,
                 # QuantizedConv2DWithBiasSumAndReluAndRequantize,
-                # QuantizedDepthwiseConv2DWithBiasAndReluAndRequantize, 
-                # QuantizedMatMulWithBiasAndReluAndRequantize, QuantizedMatMulWithBiasAndRequantize
+                # QuantizedDepthwiseConv2DWithBiasAndReluAndRequantize,
+                # QuantizedMatMulWithBiasAndReluAndRequantize,
+                # QuantizedMatMulWithBiasAndRequantize
                 elif node.op.find("Requantize") != -1:
                     out_min = -2
                     out_max = -1
