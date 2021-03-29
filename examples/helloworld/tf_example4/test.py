80d333da90f6d1459912e2043fac285e84f8bd17
Myrcha, Bartosz <bartosz.myrcha@intel.com>
2021-03-29 07:36:23 -0700

    Update helloworld TF example 4

--- examples/helloworld/tf_example4/test.py
+++ examples/helloworld/tf_example4/test.py
@@ -11,7 +11,7 @@ def main():
     import lpot
     quantizer = lpot.Quantization('./conf.yaml')
     dataset = quantizer.dataset('dummy', shape=(100, 100, 100, 3), label=True)
-    quantizer.model = common.Model('./model/public/rfcn-resnet101-coco-tf/model/public/rfcn-resnet101-coco-tf/rfcn_resnet101_coco_2018_01_28/')
+    quantizer.model = common.Model('./model/public/rfcn-resnet101-coco-tf/rfcn_resnet101_coco_2018_01_28/')
     quantizer.calib_dataloader = common.DataLoader(dataset)
     quantized_model = quantizer()
 
