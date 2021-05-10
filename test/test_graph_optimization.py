Fix the graph optimization issue that setting precision to bf16 led c… (#16)
* Fix the graph optimization issue that setting precision to bf16 led crash on non-avx512-bf16 enabled platform.

* Enhance the graph optimization by sorting the specified precisions.

* Update the doc.

Co-authored-by: Tian, Feng <feng.tian@intel.com>

test/test_graph_optimization.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 
 from tensorflow.python.framework import graph_util
 from lpot.adaptor.tf_utils.util import disable_random
-
+from lpot.utils.utility import CpuInfo
 
 def build_fake_yaml():
     fake_yaml = '''
@@ -69,6 +69,59 @@ def build_fake_yaml_3():
         yaml.dump(y, f)
     f.close()
 
+class TestGraphOptimizationOnNonBF16Host(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        build_fake_yaml()
+
+    @classmethod
+    def tearDownClass(self):
+        os.remove('fake_yaml.yaml')
+
+    @disable_random()
+    def test_bf16_cfg_on_non_bf16_enabled_host(self):
+        x = tf.compat.v1.placeholder(tf.float32, [1, 300, 300, 16], name="input")
+        top_relu = tf.nn.relu(x)
+        paddings = tf.constant([[0, 0], [1, 1], [1, 1], [0, 0]])
+        x_pad = tf.pad(top_relu, paddings, "CONSTANT")
+        conv_weights = tf.compat.v1.get_variable("weight", [3, 3, 16, 16],
+                                                 initializer=tf.compat.v1.random_normal_initializer())
+        conv_weights_2 = tf.compat.v1.get_variable("weight_2", [3, 8, 16, 16],
+                                                   initializer=tf.compat.v1.random_normal_initializer())
+        conv = tf.nn.conv2d(x_pad, conv_weights, strides=[1, 2, 2, 1], padding="VALID")
+        relu = tf.nn.relu(conv)
+
+        max_pool = tf.nn.max_pool(relu, ksize=1, strides=[1, 2, 2, 1], padding="SAME")
+        conv_bias = tf.compat.v1.get_variable("bias", [16],
+                                              initializer=tf.compat.v1.random_normal_initializer())
+        conv_1 = tf.nn.conv2d(max_pool, conv_weights_2, strides=[
+                              1, 2, 2, 1], padding="VALID", name='conv1_3')
+        conv_bias = tf.math.add(conv_1, conv_bias)
+        relu6 = tf.nn.relu6(conv_bias, name='op_to_store')
+        out_name = relu6.name.split(':')[0]
+        with tf.compat.v1.Session() as sess:
+            sess.run(tf.compat.v1.global_variables_initializer())
+            output_graph_def = graph_util.convert_variables_to_constants(
+                sess=sess,
+                input_graph_def=sess.graph_def,
+                output_node_names=[out_name])
+            from lpot.experimental import Graph_Optimization, common
+            graph_optimizer = Graph_Optimization('fake_yaml.yaml')
+            dataset = graph_optimizer.dataset('dummy', shape=(100, 300, 300, 16), label=True)
+            graph_optimizer.eval_dataloader = common.DataLoader(dataset)
+            graph_optimizer.model = output_graph_def
+            output_graph = graph_optimizer()
+            found_cast_op = False
+
+            for i in output_graph.graph_def.node:
+                if i.op == 'Cast':
+                    found_cast_op = True
+                    break
+
+            if CpuInfo().bf16:
+                self.assertEqual(found_cast_op, True)
+            else:
+                self.assertEqual(found_cast_op, False)
 
 class TestGraphOptimization(unittest.TestCase):
     @classmethod