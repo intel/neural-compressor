6694d7c26dc45528df927cae985bb582ecf0c661
Deb Taylor <deb.taylor@intel.com>
2022-02-22 18:39:28 -0500

    Back to rtds theme
    Signed-off-by: Deb Taylor <deb.taylor@intel.com>

--- conf.py
+++ conf.py
@@ -84,7 +84,8 @@ pygments_style = 'sphinx'
 # html_theme = "alabaster"
 # html_theme = 'sphinx_book_theme'
 # html_theme = 'sphinx_rtd_theme'
-html_theme = 'pydata_sphinx_theme'
+# html_theme = 'pydata_sphinx_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
