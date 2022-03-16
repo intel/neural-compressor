79497b23dae4b1188649284f5e9292f51dab837c
Neo Zhang Jianyu <jianyu.zhang@intel.com>
2022-03-16 17:50:14 +0800

    add pytorch alexnet mnist example (#777)

---
+++ docs/tutorial/pytorch/alexnet_mnist/pip_set_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ENV_NAME=env_inc
+deactivate
+rm -rf $ENV_NAME
+python -m venv $ENV_NAME
+source $ENV_NAME/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+
