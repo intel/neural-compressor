9ad7d675cad6f4addac7be66af409bee9266ec2c
VincyZhang <wenxin.zhang@intel.com>
2022-05-13 17:59:21 +0800

    modify helloworld example7 (#911)

--- examples/helloworld/tf_example7/README.md
+++ examples/helloworld/tf_example7/README.md
@@ -13,8 +13,13 @@ python train.py
 ```
 
 ### 3. Run Command
+* Run quantization
 ```shell
-python test.py
+python test.py --tune
+``` 
+* Run benchmark
+```shell
+python test.py --benchmark
 ``` 
 
 ### 4. Introduction  
@@ -25,20 +30,21 @@ python test.py
         'dataset': {'dummy_v2': {'input_shape': [28, 28]}}
     }
     conf.evaluation.performance.dataloader = dataloader
-    
     conf.quantization.calibration.dataloader = dataloader
     conf.evaluation.accuracy.dataloader = dataloader
     conf.tuning.accuracy_criterion.absolute = 0.9
 ```
 * Run quantization and benchmark. 
 ```python
-    from neural_compressor.experimental import Quantization, Benchmark, common
+    from neural_compressor.experimental import Quantization, common
+    quantizer = Quantization(conf)
+    quantizer.model = common.Model("../models/frozen_graph.pb")
+    quantizer.fit()
+    
+    from neural_compressor.experimental import Benchmark
     evaluator = Benchmark(conf)
     evaluator.model = common.Model("../models/frozen_graph.pb")
     evaluator('performance')
     
-    quantizer = Quantization(conf)
-    quantizer.model = common.Model("../models/frozen_graph.pb")
-    quantizer.fit()
 ```
 
--- examples/helloworld/tf_example7/test.py
+++ examples/helloworld/tf_example7/test.py
@@ -1,24 +1,33 @@
 import tensorflow as tf
+from argparse import ArgumentParser
+from neural_compressor import conf
+from neural_compressor.experimental import common
 tf.compat.v1.disable_eager_execution()
 
-from neural_compressor import conf
 def main():
-
-    from neural_compressor.experimental import Quantization, Benchmark, common
+    arg_parser = ArgumentParser(description='Parse args')
+    arg_parser.add_argument('--benchmark', action='store_true', help='run benchmark')
+    arg_parser.add_argument('--tune', action='store_true', help='run tuning')
+    args = arg_parser.parse_args()
+    
     dataloader = {
         'dataset': {'dummy_v2': {'input_shape': [28, 28]}}
     }
-    conf.evaluation.performance.dataloader = dataloader
-    evaluator = Benchmark(conf)
-    evaluator.model = common.Model("../models/frozen_graph.pb")
-    evaluator('performance')
-
     conf.quantization.calibration.dataloader = dataloader
     conf.evaluation.accuracy.dataloader = dataloader
     conf.tuning.accuracy_criterion.absolute = 0.9
-    quantizer = Quantization(conf)
-    quantizer.model = common.Model("../models/frozen_graph.pb")
-    quantizer.fit()
+    conf.evaluation.performance.dataloader = dataloader
+    if args.tune:
+        from neural_compressor.experimental import Quantization
+        quantizer = Quantization(conf)
+        quantizer.model = common.Model("../models/frozen_graph.pb")
+        quantizer.fit()
+
+    if args.benchmark:
+        from neural_compressor.experimental import Benchmark
+        evaluator = Benchmark(conf)
+        evaluator.model = common.Model("../models/frozen_graph.pb")
+        evaluator('performance')
       
 if __name__ == "__main__":
 
