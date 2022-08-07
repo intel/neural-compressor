5bd154a41e8a5b00946821c403161a3e857f6262
chen,suyue <suyue.chen@intel.com>
2022-08-08 00:11:51 +0800

    update

--- .azure-pipelines/scripts/ut/run_basic_adaptor.sh
+++ .azure-pipelines/scripts/ut/run_basic_adaptor.sh
@@ -3,11 +3,19 @@ set -x
 
 python -c "import neural_compressor as nc;print(nc.version.__version__)"
 echo "run basic adaptor"
-env | sort
-echo "set up UT env..."
-bash env_setup.sh
 
-cd adaptor
+env | sort
+echo ${tensorflow_version}
+echo ${pytorch_version}
+export onnx_version='1.9.0'
+export onnxruntime_version='1.10.0'
+export mxnet_version='1.7.0'
+
+
+echo "set up UT env..."
+bash /neural-compressor/.azure-pipelines/scripts/ut/env_setup.sh
+
+cd /neural-compressor/test/adaptor || exit 1
 find . -name "test*.py" | sed 's,\.\/,python ,g' | sed 's/$/ --verbose/'  > run.sh
 ut_log_name=${WORKSPACE}/ut_tf_${tensorflow_version}_pt_${pytorch_version}.log
 
