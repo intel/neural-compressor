9827f74a5c3aedbc6ddc80d6ce4cc20b59d2f860
chensuyue <suyue.chen@intel.com>
2022-08-09 23:47:15 +0800

    bug fix

--- .azure-pipelines/scripts/install_nc_full.sh
+++ .azure-pipelines/scripts/install_nc_full.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /neural_compressor
+cd /neural-compressor
 python -m pip install --no-cache-dir -r requirements.txt
 python setup.py --full sdist bdist_wheel
 pip install dist/neural_compressor-*.whl
