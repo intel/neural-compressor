c9f2025fcb5c0633073cb976b12d7b064a9f4de4
chen, suyue <suyue.chen@intel.com>
2022-08-10 16:54:36 +0800

    Suyue/ut update (#16)

--- .azure-pipelines/scripts/install_nc.sh
+++ .azure-pipelines/scripts/install_nc.sh
@@ -3,5 +3,5 @@
 cd /neural-compressor
 python -m pip install --no-cache-dir -r requirements.txt
 python setup.py sdist bdist_wheel
-pip install dist/neural_compressor-*.whl
+pip install dist/neural_compressor*.whl
 pip list
