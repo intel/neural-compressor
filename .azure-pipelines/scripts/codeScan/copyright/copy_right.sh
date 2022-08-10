60a5cfc0a62cac35b489df51e7d1883d5c5e49f4
Yue, Wenjiao <wenjiao.yue@intel.com>
2022-08-10 13:35:56 +0800

    test

--- .azure-pipelines/scripts/codeScan/copyright/copy_right.sh
+++ .azure-pipelines/scripts/codeScan/copyright/copy_right.sh
@@ -12,7 +12,8 @@
     git --no-pager diff --name-only $(git show-ref -s remotes/origin/master) /neural_compressor/neural_compressor > /diff.log
     # git --no-pager diff --name-only $(git show-ref -s remotes/origin$2) ./$1 > $3/copyright/diff.log
 
-    files=$(cat $3/copyright/diff.log | awk '!a[$0]++')
+    files=$(cat /diff.log | awk '!a[$0]++')
+    # files=$(cat $3/copyright/diff.log | awk '!a[$0]++')
 
     for file in ${files}
 
@@ -20,8 +21,8 @@
         if [[ "${supported_extensions[@]}" =~ "${file##*.}" ]]; then
             echo "Checking license in 1 ${file}"
             if [ $(grep -E -c "Copyright \\(c\\) ([0-9]{4})(-[0-9]{4})? Intel Corporation" ${file}) = 0 ]; then
-                echo ${file} >>  $3/copyright/copyright_issue_summary.log
-                cat $3/copyright/copyright_issue_summary.log
+                echo ${file} >>  /copyright_issue_summary.log
+                cat /copyright_issue_summary.log
             fi
         else
             echo "Skipping ${file}"
@@ -29,7 +30,7 @@
     done
 
 
-    ls $3/copyright/copyright_issue_summary.log
+    ls /copyright_issue_summary.log
     exit_code=$?
     if [ ${exit_code} -e 0 ] ; then
         echo "------------------Check <copyright_issue_summary.log> for wrong file list !!!!!!!!!!!!!!!!!!!!!!!"; exit 1
