3ee9be71975e979dfc669cd29d17f32c007bd446
Yue, Wenjiao <wenjiao.yue@intel.com>
2022-08-11 15:01:45 +0800

    test

--- .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
+++ .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
@@ -5,11 +5,11 @@ apt-get install aspell -y
 apt-get install aspell-en -y
 
 
-sed -i "s|\${VAL_REPO}|${TARGET_PATH}|g" ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}/pyspelling_conf.yaml
-sed -i "s|\${LPOT_REPO}|.|g" ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}/pyspelling_conf.yaml
+sed -i "s|\${VAL_REPO}|${TARGET_PATH}|g" ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}pyspelling_conf.yaml
+sed -i "s|\${LPOT_REPO}|.|g" ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}pyspelling_conf.yaml
 echo "Modified config:"
-cat ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}/pyspelling_conf.yaml
-pyspelling -c ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}/pyspelling_conf.yaml > /lpot_pyspelling.log
+cat ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}pyspelling_conf.yaml
+pyspelling -c ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}pyspelling_conf.yaml > /lpot_pyspelling.log
 exit_code=$?
 if [ ${exit_code} -ne 0 ] ; then
     echo "Pyspelling exited with non-zero exit code."; exit 1
