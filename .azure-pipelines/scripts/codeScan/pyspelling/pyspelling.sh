ad64b2bce1af2adec0cdeb66e31855fd13e16e50
Yue, Wenjiao <wenjiao.yue@intel.com>
2022-08-11 16:10:52 +0800

    test

--- .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
+++ .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
@@ -6,11 +6,11 @@ apt-get install aspell-en -y
 
 
 
-sed -i "s|\${VAL_REPO}|$1|g" $1/.azure-pipelines/scripts/codeScan/pyspelling_conf.yaml
-sed -i "s|\${LPOT_REPO}|.|g" $1/.azure-pipelines/scripts/codeScan/pyspelling_conf.yaml
+sed -i "s|\${VAL_REPO}|$1|g" /neural_compressor/.azure-pipelines/scripts/codeScan/pyspelling/pyspelling_conf.yaml
+sed -i "s|\${LPOT_REPO}|.|g" /neural_compressor/.azure-pipelines/scripts/codeScan/pyspelling/pyspelling_conf.yaml
 echo "Modified config:"
-cat $1/.azure-pipelines/scripts/codeScan/pyspelling_conf.yaml
-pyspelling -c $1/.azure-pipelines/scripts/codeScan/pyspelling_conf.yaml > /lpot_pyspelling.log
+cat /neural_compressor/.azure-pipelines/scripts/codeScan/pyspelling/pyspelling_conf.yaml
+pyspelling -c /neural_compressor/.azure-pipelines/scripts/codeScan/pyspelling/pyspelling_conf.yaml > /lpot_pyspelling.log
 exit_code=$?
 if [ ${exit_code} -ne 0 ] ; then
     echo "Pyspelling exited with non-zero exit code."; exit 1
