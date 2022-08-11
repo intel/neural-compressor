9e1f33c8b1b1de7df408a832e217f6b1ca4d5d58
Yue, Wenjiao <wenjiao.yue@intel.com>
2022-08-11 16:00:29 +0800

    test

--- .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
+++ .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
@@ -4,17 +4,13 @@ pip install -r requirements.txt
 apt-get install aspell -y
 apt-get install aspell-en -y
 
-echo "---------------------"
-$1
-echo "---------------------"
-$2
 
 
-sed -i "s|\${VAL_REPO}|$1|g" $1/$2/pyspelling_conf.yaml
-sed -i "s|\${LPOT_REPO}|.|g" $1/$2/pyspelling_conf.yaml
+sed -i "s|\${VAL_REPO}|$1|g" $1/pyspelling_conf.yaml
+sed -i "s|\${LPOT_REPO}|.|g" $1/pyspelling_conf.yaml
 echo "Modified config:"
-cat $1/$2/pyspelling_conf.yaml
-pyspelling -c $1/$2/pyspelling_conf.yaml > /lpot_pyspelling.log
+cat $1/pyspelling_conf.yaml
+pyspelling -c $1/pyspelling_conf.yaml > /lpot_pyspelling.log
 exit_code=$?
 if [ ${exit_code} -ne 0 ] ; then
     echo "Pyspelling exited with non-zero exit code."; exit 1
