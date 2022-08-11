7043b982b12af04f2ff8b0c106319d996f136f5c
Yue, Wenjiao <wenjiao.yue@intel.com>
2022-08-11 18:51:14 +0800

    test

--- .azure-pipelines/scripts/codeScan/bandit/bandit.sh
+++ .azure-pipelines/scripts/codeScan/bandit/bandit.sh
@@ -1,12 +1,8 @@
 pip install bandit
 
-echo "--------------------"
-$1
-echo "--------------------"
 
 python -m bandit -r -lll -iii $1/neural_compressor >  /lpot-bandit.log
 
-exit 1
 exit_code=$?
 if [ ${exit_code} -ne 0 ] ; then
     echo "Bandit exited with non-zero exit code."; exit 1
