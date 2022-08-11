ad0fa292ddb6f52f8ab9c378a84f1f5e1dee4565
Yue, Wenjiao <wenjiao.yue@intel.com>
2022-08-11 15:17:07 +0800

    test

--- .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
+++ .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
@@ -12,11 +12,11 @@ echo "---------------------"
 cd ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}
 echo "---------------------------------------"
 
-sed -i "s|\${VAL_REPO}|${TARGET_PATH}|g" ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}pyspelling_conf.yaml
-sed -i "s|\${LPOT_REPO}|.|g" ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}pyspelling_conf.yaml
+sed -i "s|\${VAL_REPO}|${TARGET_PATH}|g" ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}/pyspelling_conf.yaml
+sed -i "s|\${LPOT_REPO}|.|g" ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}/pyspelling_conf.yaml
 echo "Modified config:"
-cat ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}pyspelling_conf.yaml
-pyspelling -c ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}pyspelling_conf.yaml > /lpot_pyspelling.log
+cat ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}/pyspelling_conf.yaml
+pyspelling -c ${TARGET_PATH}/${PYSPELLING_CONTAINER_NAME}/pyspelling_conf.yaml > /lpot_pyspelling.log
 exit_code=$?
 if [ ${exit_code} -ne 0 ] ; then
     echo "Pyspelling exited with non-zero exit code."; exit 1
