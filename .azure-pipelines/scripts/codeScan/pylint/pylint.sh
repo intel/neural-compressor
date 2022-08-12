34e50d8e82976dedeb9fc0acb0892ae42c9c2edb
Yue, Wenjiao <wenjiao.yue@intel.com>
2022-08-12 11:14:31 +0800

    test

--- .azure-pipelines/scripts/codeScan/pylint/pylint.sh
+++ .azure-pipelines/scripts/codeScan/pylint/pylint.sh
@@ -1,5 +1,5 @@
-# $1: $(VAL_PATH) 
-# $2: $(target_path) 
+set -ex
+pylint_log_dir="/neural-compressor/.azure-pipelines/scripts/codeScan/scanLog"
 
 pip install pylint==2.12.1
 pip install flask==2.1.3
@@ -16,7 +16,7 @@ pip install onnxruntime_extensions
 
 
 
-python -m pylint -f json --disable=R,C,W,E1129 --enable=line-too-long --max-line-length=120 --extension-pkg-whitelist=numpy --ignored-classes=TensorProto,NodeProto --ignored-modules=tensorflow,torch,torch.quantization,torch.tensor,torchvision,mxnet,onnx,onnxruntime $1/$1 > /lpot-pylint.json
+python -m pylint -f json --disable=R,C,W,E1129 --enable=line-too-long --max-line-length=120 --extension-pkg-whitelist=numpy --ignored-classes=TensorProto,NodeProto --ignored-modules=tensorflow,torch,torch.quantization,torch.tensor,torchvision,mxnet,onnx,onnxruntime /neural-compressor/neural_compressor > $pylint_log_dir/lpot-pylint.json
 
 exit_code=$?
 if [ ${exit_code} -ne 0 ] ; then
