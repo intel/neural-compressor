c9f0461938d8559de02434824edb130d1b73668a
WenjiaoYue <108783334+WenjiaoYue@users.noreply.github.com>
2022-08-24 11:20:09 +0800

    Code scan optimization (#28)

--- .azure-pipelines/scripts/codeScan/bandit/bandit.sh
+++ .azure-pipelines/scripts/codeScan/bandit/bandit.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
-set -ex
+set -e
 
 mkdir -p /neural-compressor/.azure-pipelines/scripts/codeScan/scanLog
-
 bandit_log_dir="/neural-compressor/.azure-pipelines/scripts/codeScan/scanLog"
-pip install bandit
 
+# error
+RED="echo -en \\E[1;31m"
+# succeed
+PURPLE="echo -en \\E[1;35m"
+# succeed hint
+LIGHT_PURPLE="echo -en \\E[35m"
+# log output hint
+YELLOW="echo -en \\E[1;33m"
+# close 
+RESET="echo -en \\E[0m \\n"
 
 python -m bandit -r -lll -iii /neural-compressor/neural_compressor >  $bandit_log_dir/lpot-bandit.log
 
+$YELLOW && echo " -----------------  Current log file output start --------------------------"
+cat  $bandit_log_dir/lpot-bandit.log
+$YELLOW && echo " -----------------   Current log file output end --------------------------" && $RESET
+
 exit_code=$?
 if [ ${exit_code} -ne 0 ] ; then
-    echo "Bandit exited with non-zero exit code."; exit 1
+    $RED && echo "Error!! Please Click on the artifact button to download and view Bandit error details. " && $RESET; exit 1
 fi
-exit 0
+$PURPLE && echo "Congratulations, Bandit check passed!" && $LIGHT_PURPLE && echo " You can click on the artifact button to see the log details." && $RESET; exit 0
 
--- .azure-pipelines/scripts/codeScan/pylint/pylint.sh
+++ .azure-pipelines/scripts/codeScan/pylint/pylint.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
-set -ex
+set -e
 
+source /neural-compressor/.azure-pipelines/scripts/change_color.sh
 mkdir -p /neural-compressor/.azure-pipelines/scripts/codeScan/scanLog
 pylint_log_dir="/neural-compressor/.azure-pipelines/scripts/codeScan/scanLog"
 
-pip install pylint==2.12.1
-pip install flask==2.1.3
-pip install google
-pip install horovod
-pip install ofa
-pip install fvcore
-pip install autograd
-pip install pymoo
-pip install tf_slim
-pip install transformers
 pip install -r /neural-compressor/requirements.txt
-pip install onnxruntime_extensions
-
 
 python -m pylint -f json --disable=R,C,W,E1129 --enable=line-too-long --max-line-length=120 --extension-pkg-whitelist=numpy --ignored-classes=TensorProto,NodeProto --ignored-modules=tensorflow,torch,torch.quantization,torch.tensor,torchvision,mxnet,onnx,onnxruntime /neural-compressor/neural_compressor > $pylint_log_dir/lpot-pylint.json
+# code-scan close 
+RESET="echo -en \\E[0m \\n"
+
+$BOLD_YELLOW && echo " -----------------  Current log file output start --------------------------"
+cat  $pylint_log_dir/lpot-pylint.json
+$BOLD_YELLOW && echo " -----------------  Current log file output end --------------------------" && $RESET
 
 exit_code=$?
 if [ ${exit_code} -ne 0 ] ; then
-    echo "PyLint exited with non-zero exit code."; exit 1
+    $BOLD_RED && echo "Error!! Please Click on the artifact button to download and view Pylint error details." && $RESET; exit 1
 fi
-exit 0
+$BOLD_PURPLE && echo "Congratulations, Pylint check passed!" && $LIGHT_PURPLE && echo " You can click on the artifact button to see the log details." && $RESET; exit 0
