1b6d74c5921870f1d93fc34948b7bac639f6c8af
Sun, Xuehao <xuehao.sun@intel.com>
2022-11-03 13:36:02 +0800

    update

--- .azure-pipelines/scripts/models/run_model_trigger_common.sh
+++ .azure-pipelines/scripts/models/run_model_trigger_common.sh
@@ -70,6 +70,15 @@ elif [ "${mode}" == "tuning" ]; then
         --input_model=${input_model} \
         --strategy=${strategy} \
         2>&1 | tee -a ${log_dir}/${model}/${framework}-${model}-tune.log
+    $BOLD_YELLOW && echo "====== check tuning status. ======" && $RESET
+    control_phrase="model which meet accuracy goal."
+    if [ $(grep "${control_phrase}" ${log_dir}/${model}/${framework}-${model}-tune.log | wc -l) == 0 ];then
+        $BOLD_RED && echo "====== tuning FAILED!! ======" && $RESET; exit 1
+    fi
+    if [ $(grep "${control_phrase}" ${log_dir}/${model}/${framework}-${model}-tune.log | grep "Not found" | wc -l) == 1 ];then
+        $BOLD_RED && echo "====== tuning FAILED!! ======" && $RESET; exit 1
+    fi
+    $BOLD_GREEN && echo "====== tuning SUCCEED!! ======" && $RESET
 elif [ "${mode}" == "fp32_benchmark" ]; then
     cd ${WORK_SOURCE_DIR}/${model_src_dir}
     $BOLD_YELLOW && echo "benchmark_cmd is ${benchmark_cmd}" && $RESET
