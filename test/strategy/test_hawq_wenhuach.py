581b21e56cf8baf0766d4a3c980a86cef3339ef7
wenhuach <dd>
2022-11-15 14:00:06 +0800

    try to test mes strategy, have bug now

--- test/strategy/test_hawq_wenhuach.py
+++ test/strategy/test_hawq_wenhuach.py
@@ -9,66 +9,130 @@ from torch.utils.data import DataLoader
 from neural_compressor.data import DATASETS
 from neural_compressor.experimental.data.dataloaders.pytorch_dataloader import PyTorchDataLoader
 from neural_compressor.adaptor.pytorch import TemplateAdaptor
-
+from neural_compressor.adaptor import FRAMEWORKS
+import shutil
 from neural_compressor.strategy.hawq import Hawq_top, fix_seed
 
 fix_seed(1)
 
+def build_ptq_yaml():
+    fake_yaml = '''
+        model:
+          name: imagenet
+          framework: pytorch
+        quantization: 
+          calibration:
+        evaluation:
+          accuracy:
+            metric:
+              topk: 1
+        tuning:
+          strategy:
+            name: mse
+          accuracy_criterion:
+            relative: -0.1
+          random_seed: 9527
+          exit_policy:
+            max_trials: 1
+          workspace:
+            path: saved
+        '''
+    with open('ptq_yaml.yaml', 'w', encoding="utf-8") as f:
+        f.write(fake_yaml)
 
-def build_hessian_trace():
-    hessian_trace_config_yaml = '''
-    loss:
-        CrossEntropyLoss
-    random_seed:
-        1
-    max_Iteration:
-        100
-    tolerance:
-        1e-3
-    enable_op_fuse:
-        True
-    max_cal_smaple:
-        100
-    quantize_mode:
-        ptq
-    '''
-    with open('./hessian_trace_config_yaml', 'w+', encoding="utf-8") as f:
-        f.write(hessian_trace_config_yaml)
+class TestPytorchAdaptor(unittest.TestCase):
+    framework_specific_info = {"device": "cpu",
+                               "approach": "post_training_static_quant",
+                               "random_seed": 1234,
+                               "q_dataloader": None,
+                               "workspace_path": None}
+    framework = "pytorch"
+    adaptor = FRAMEWORKS[framework](framework_specific_info)
+    model = torchvision.models.resnet18()
 
+    # model = torch.quantization.QuantWrapper(model)
 
-class Test_hessian_trace(unittest.TestCase):
-    # boot up test
     @classmethod
-    def setUpClass(cls) -> None:
-        build_hessian_trace()
-        cls.model = torchvision.models.resnet18()
+    def setUpClass(self):
+        build_ptq_yaml()
 
-    # shotdown test
+
     @classmethod
-    def tearDownClass(cls) -> None:
-        os.remove('./hessian_trace_config_yaml')
+    def tearDownClass(self):
+        os.remove('ptq_yaml.yaml')
+        shutil.rmtree('./saved', ignore_errors=True)
+        shutil.rmtree('runs', ignore_errors=True)
 
-    # one test case
-    def test_run_hessian_trace(cls):
-        """
-        hessian_trace_top
-        Inputs:
-            model:                      FP32 model
-            dataloader:                 imagenet
-        """
-
-        model = cls.model
-        datasets = DATASETS('pytorch')
-        dummy_dataset = datasets['dummy'](shape=(200, 3, 224, 224), low=0., high=1., label=True)
-        dummy_dataloader = PyTorchDataLoader(dummy_dataset)
-        # yaml_cpu='/home/bfang1/Projects/HAWQ_INC/frameworks.ai.lpot.intel-lpot/neural_compressor/adaptor/pytorch_cpu.yaml'
-        # hessian_cmp=hawq_metric.Hawq_top(model,'./hessian_trace_config_yaml',yaml_cpu,dummy_dataloader)
-        hessian_cmp = Hawq_top(model, yaml_cpu=None, yaml_trace=None, dataloader=dummy_dataloader)
-        tuning_init_config = hessian_cmp.get_init_config()
-        # print tuning init_config
-        for i in tuning_init_config:
-            print(i)
-
+    def test_run_hawq_one_trial(self):
+        from neural_compressor.experimental import Quantization, common
+        model = copy.deepcopy(self.model)
+        for fake_yaml in ['ptq_yaml.yaml']:
+            if fake_yaml == 'ptq_yaml.yaml':
+                model.eval()
+            quantizer = Quantization(fake_yaml)
+            dataset = quantizer.dataset('dummy', (1, 3, 224, 224), label=True)
+            quantizer.calib_dataloader = common.DataLoader(dataset)
+            quantizer.eval_dataloader = common.DataLoader(dataset)
+            quantizer.model = model
+            quantizer()
 
 if __name__ == "__main__":
     unittest.main()
+
+# def build_hessian_trace():
+#     hessian_trace_config_yaml = '''
+#     loss:
+#         CrossEntropyLoss
+#     random_seed:
+#         1
+#     max_Iteration:
+#         100
+#     tolerance:
+#         1e-3
+#     enable_op_fuse:
+#         True
+#     max_cal_smaple:
+#         100
+#     quantize_mode:
+#         ptq
+#     '''
+#     with open('./hessian_trace_config_yaml', 'w+', encoding="utf-8") as f:
+#         f.write(hessian_trace_config_yaml)
+#
+#
+# class Test_hessian_trace(unittest.TestCase):
+#     # boot up test
+#     @classmethod
+#     def setUpClass(cls) -> None:
+#         build_hessian_trace()
+#         cls.model = torchvision.models.resnet18()
+#
+#     # shotdown test
+#     @classmethod
+#     def tearDownClass(cls) -> None:
+#         os.remove('./hessian_trace_config_yaml')
+#
+#     # one test case
+#     def test_run_hessian_trace(cls):
+#         """
+#         hessian_trace_top
+#         Inputs:
+#             model:                      FP32 model
+#             dataloader:                 imagenet
+#         """
+#
+#         model = cls.model
+#         datasets = DATASETS('pytorch')
+#         dummy_dataset = datasets['dummy'](shape=(200, 3, 224, 224), low=0., high=1., label=True)
+#         dummy_dataloader = PyTorchDataLoader(dummy_dataset)
+#         # yaml_cpu='/home/bfang1/Projects/HAWQ_INC/frameworks.ai.lpot.intel-lpot/neural_compressor/adaptor/pytorch_cpu.yaml'
+#         # hessian_cmp=hawq_metric.Hawq_top(model,'./hessian_trace_config_yaml',yaml_cpu,dummy_dataloader)
+#         hessian_cmp = Hawq_top(model, yaml_cpu=None, yaml_trace=None, dataloader=dummy_dataloader)
+#         tuning_init_config = hessian_cmp.get_init_config()
+#         # print tuning init_config
+#         for i in tuning_init_config:
+#             print(i)
+
+
+# if __name__ == "__main__":
+#     unittest.main()
