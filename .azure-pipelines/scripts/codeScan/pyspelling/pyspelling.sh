f23a3709fec056051cacf683407502e7a7feeab5
chensuyue <suyue.chen@intel.com>
2022-11-23 16:40:20 +0800

    separate spell check and fix the check path
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
+++ .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
 source /neural-compressor/.azure-pipelines/scripts/change_color.sh
+RESET="echo -en \\E[0m \\n" # close color
 
-mkdir -p /neural-compressor/.azure-pipelines/scripts/codeScan/scanLog
-pyspelling_dir="/neural-compressor/.azure-pipelines/scripts/codeScan"
-pyspelling_log_dir="/neural-compressor/.azure-pipelines/scripts/codeScan/scanLog"
+pyspelling_dir="/neural-compressor/.azure-pipelines/scripts/codeScan/pyspelling"
+pyspelling_log_dir="$pyspelling_dir/scanLog"
+mkdir -p $pyspelling_log_dir
 
 pip install -r /neural-compressor/requirements.txt
 
-sed -i "s|\${VAL_REPO}|$pyspelling_dir|g" $pyspelling_dir/pyspelling/pyspelling_conf.yaml
-sed -i "s|\${LPOT_REPO}|/neural-compressor|g" $pyspelling_dir/pyspelling/pyspelling_conf.yaml
+sed -i "s|\${DICT_DIR}|$pyspelling_dir|g" $pyspelling_dir/pyspelling_conf.yaml
+sed -i "s|\${REPO_DIR}|/neural-compressor|g" $pyspelling_dir/pyspelling_conf.yaml
 
-pyspelling -c $pyspelling_dir/pyspelling/pyspelling_conf.yaml > $pyspelling_log_dir/lpot_pyspelling.log
+pyspelling -c $pyspelling_dir/pyspelling_conf.yaml > $pyspelling_log_dir/lpot_pyspelling.log
 exit_code=$?
-
-# code-scan close 
-RESET="echo -en \\E[0m \\n"
 
 $BOLD_YELLOW && echo "-------------------  Current log file output start --------------------------" && $RESET
 cat  $pyspelling_log_dir/lpot_pyspelling.log
@@ -23,7 +21,7 @@ $BOLD_YELLOW && echo "-------------------  Current log file output end ---------
 
 
 if [ ${exit_code} -ne 0 ] ; then
-    $BOLD_RED && echo "Error!! Please Click on the artifact button to download and view Pyspelling error details." && $RESET; exit 1
+    $BOLD_RED && echo "Error!! Please Click on the artifact button to download and view pyspelling error details." && $RESET; exit 1
 fi
 $BOLD_PURPLE && echo "Congratulations, Pyspelling check passed!" && $LIGHT_PURPLE && echo "You can click on the artifact button to see the log details." && $RESET; exit 0
 
