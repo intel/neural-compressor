9552f9a23dc07af155c323213fc4fc8821b4426b
mengniwa <mengni.wang@intel.com>
2022-11-24 10:27:21 +0800

    Add dataloader doc_string
    Signed-off-by: mengniwa <mengni.wang@intel.com>

--- neural_compressor/experimental/data/__init__.py
+++ neural_compressor/experimental/data/__init__.py
@@ -14,7 +14,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# ==============================================================================
+"""Built-in dataloaders, datasets, transforms, filters for multiple framework backends."""
 
 from .datasets import DATASETS, Dataset, IterableDataset, dataset_registry
 from .transforms import TRANSFORMS, BaseTransform, transform_registry
