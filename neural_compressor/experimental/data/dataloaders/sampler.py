94578c8d4ee9697c14c5cd68cec6be77d5e9c852
Wang, Mengni <mengni.wang@intel.com>
2022-11-25 09:17:52 +0800

    Add dataloader doc_string (#154)
    Signed-off-by: mengniwa <mengni.wang@intel.com>

--- neural_compressor/experimental/data/dataloaders/sampler.py
+++ neural_compressor/experimental/data/dataloaders/sampler.py
@@ -14,57 +14,70 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# ==============================================================================
+"""Definitions of the methods to sample data."""
 
 from abc import abstractmethod
 
 class Sampler(object):
-    """Base class for all Samplers. __iter__ is needed no matter whether you use IterableSampler
-       or Squential sampler, if you want implement your own sampler, make clear what the type is
-       your Dataset, if IterableDataset(method __iter__ implemented), try to use IterableSampler,
-       else if you have an IndexDataset(method __getitem__ implemented), your dataset should have
-       method __len__ implemented.
-
+    """Base class for all Samplers.
+    
+    __iter__ is needed no matter whether you use IterableSampler
+    or Squential sampler, if you want implement your own sampler, make clear what the type is
+    your Dataset, if IterableDataset(method __iter__ implemented), try to use IterableSampler,
+    else if you have an IndexDataset(method __getitem__ implemented), your dataset should have
+    method __len__ implemented.
     """
 
     def __init__(self, data_source):
+        """Initialize Sampler."""
         pass
 
     @abstractmethod
     def __iter__(self):
+        """Convert dataloder to an iterator."""
         raise NotImplementedError
 
 
 class IterableSampler(Sampler):
-    """Interally samples elements, used for datasets retrieved element by interator.
-       yield None to act as a placeholder for each iteration
-
-    Args:
-        dataset (Dataset): set to None
+    """Interally samples elements.
+    
+    Used for datasets retrieved element by interator. Yield None to act as a placeholder for each iteration.
     """
 
     def __init__(self, dataset):
+        """Initialize IterableSampler.
+
+        Args:
+            dataset (object): dataset object from which to get data
+        """
         super(IterableSampler, self).__init__(None)
         self.whole_dataset = dataset
 
     def __iter__(self):
+        """Yield data in iterative order."""
         while True:
             yield None
 
     def __len__(self):
+        """Return the length of dataset."""
         raise NotImplementedError("'__len__' for IterableDataset object has not defined")
 
 class SequentialSampler(Sampler):
-    """Sequentially samples elements, used for datasets retrieved element by index.
-
-    Args:
-        dataset (Dataset): index dataset(implement method __len__) for sampling
-    """
+    """Sequentially samples elements, used for datasets retrieved element by index."""
 
     def __init__(self, dataset, distributed):
+        """Initialize SequentialSampler.
+
+        Args:
+            dataset (object): dataset object from which to get data
+            distributed (bool): whether the dataloader is distributed
+        """
         self.whole_dataset = dataset
         self.distributed = distributed
 
     def __iter__(self):
+        """Yield data in iterative order."""
         self.process_rank = 0 # The default rank is 0, which represents the main process
         self.process_size = 1 # By default, process_size=1, only the main process is running
         if self.distributed:
@@ -81,21 +94,20 @@ class SequentialSampler(Sampler):
         return iter(range(self.process_rank, len(self.whole_dataset), self.process_size))
 
     def __len__(self):
+        """Return the length of dataset."""
         return len(self.whole_dataset)
 
 class BatchSampler(Sampler):
-    """yield a mini-batch of indices for SquentialSampler and batch size length of None list for
-       IterableSampler.
-
-    Args:
-        sampler (Sampler): sampler used for generating batches.
-        batch_size (int): Size of mini-batch.
-        drop_last (bool): BatchSampler will drop the last batch if drop_last is True, else
-                          will return the last batch whose size will be less than batch_size
-
-    """
+    """Yield a batch of indices and number of batches."""
 
     def __init__(self, sampler, batch_size, drop_last=True):
+        """Initialize BatchSampler.
+
+        Args:
+            sampler (Sampler): sampler used for generating batches
+            batch_size (int): size of batch
+            drop_last (bool, optional): whether to drop the last batch if it is incomplete. Defaults to True.
+        """
         if isinstance(drop_last, bool):
             self.drop_last = drop_last
         else:
@@ -106,6 +118,7 @@ class BatchSampler(Sampler):
         self.drop_last = drop_last
 
     def __iter__(self):
+        """Yield data in iterative order."""
         batch = []
         for idx in self.sampler:
             batch.append(idx)
@@ -116,6 +129,7 @@ class BatchSampler(Sampler):
             yield batch
 
     def __len__(self):
+        """Return the number of batches."""
         if self.drop_last:
             return len(self.sampler) // self.batch_size
         else:
