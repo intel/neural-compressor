1679a054791c0a2ac59e5cca2921fb36fd1d1913
Ray <106061964+yiliu30@users.noreply.github.com>
2022-11-29 15:33:52 +0800

    Adjust the hyperopt and sigopt as extras requirements (#173)
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- setup.py
+++ setup.py
@@ -36,8 +36,8 @@ ux_package_data = {
 
 # define install requirements
 install_requires_list = [
-        'numpy', 'pyyaml', 'scikit-learn', 'schema', 'py-cpuinfo', 'hyperopt', 'pandas', 'pycocotools',
-        'opencv-python', 'requests', 'psutil', 'Pillow', 'sigopt', 'prettytable', 'cryptography', 'Cython',
+        'numpy', 'pyyaml', 'scikit-learn', 'schema', 'py-cpuinfo', 'pandas', 'pycocotools',
+        'opencv-python', 'requests', 'psutil', 'Pillow', 'prettytable', 'cryptography', 'Cython',
         'deprecated']
 ux_install_requires_list = [
         'Flask-Cors', 'Flask-SocketIO', 'Flask', 'gevent-websocket', 'gevent','sqlalchemy==1.4.27', 'alembic==1.7.7']
