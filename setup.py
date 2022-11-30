79db56f65b0a0bd0267173e32b280c068c4e9bad
chen, suyue <suyue.chen@intel.com>
2022-11-30 16:18:28 +0800

    update installation dependency (#185)

--- setup.py
+++ setup.py
@@ -37,10 +37,10 @@ ux_package_data = {
 # define install requirements
 install_requires_list = [
         'numpy', 'pyyaml', 'scikit-learn', 'schema', 'py-cpuinfo', 'pandas', 'pycocotools',
-        'opencv-python', 'requests', 'psutil', 'Pillow', 'prettytable', 'cryptography', 'Cython',
-        'deprecated']
+        'opencv-python', 'requests', 'psutil', 'Pillow', 'prettytable', 'deprecated']
 ux_install_requires_list = [
-        'Flask-Cors', 'Flask-SocketIO', 'Flask', 'gevent-websocket', 'gevent','sqlalchemy==1.4.27', 'alembic==1.7.7']
+        'Flask-Cors', 'Flask-SocketIO', 'Flask', 'gevent-websocket', 'gevent','sqlalchemy==1.4.27',
+        'alembic==1.7.7', 'cryptography']
 
 # define scripts
 scripts_list = []
