5174c8027bbd79b98fdf8ac9889fe6e7eb6e921e
yiliu30 <yi4.liu@intel.com>
2022-12-01 09:04:25 +0800

    remove hard code for baseline
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- neural_compressor/strategy/strategy.py
+++ neural_compressor/strategy/strategy.py
@@ -219,8 +219,7 @@ class TuneStrategy(object):
         if self.baseline is None:
             logger.info("Get FP32 model baseline.")
             self._fp32_model = self.model
-            ##self.baseline = self._evaluate(self.model)
-            self.baseline = [0.698,[700]]
+            self.baseline = self._evaluate(self.model)
             # record the FP32 baseline
             self._add_tuning_history()
         self.show_baseline_info()
