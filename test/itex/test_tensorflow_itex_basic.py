b7b475d40013cd324e516b6380025d16bff48113
Lv, Liang1 <liang1.lv@intel.com>
2022-12-01 22:34:21 +0800

    Disable multi instance for ITEX GPU benchmark
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>

--- test/itex/test_tensorflow_itex_basic.py
+++ test/itex/test_tensorflow_itex_basic.py
@@ -11,7 +11,7 @@ from neural_compressor.adaptor.tf_utils.graph_rewriter.generic.strip_unused_node
 from neural_compressor.adaptor.tf_utils.graph_rewriter.generic.fold_batch_norm import FoldBatchNormNodesOptimizer
 from neural_compressor.adaptor.tensorflow import TensorflowQuery
 from neural_compressor.adaptor.tf_utils.util import disable_random
-from neural_compressor.experimental import Quantization, common
+from neural_compressor.experimental import Quantization, Benchmark, common
 from neural_compressor.utils.utility import CpuInfo
 from neural_compressor.adaptor.tf_utils.util import version1_lt_version2, version1_gte_version2
 
@@ -216,6 +216,47 @@ class TestItexEnabling(unittest.TestCase):
                 if i.op == 'Reshape':
                     reshape_counter += 1
             self.assertEqual(reshape_counter, 2)
+
+    @disable_random()
+    @unittest.skipIf(version1_lt_version2(tf.version.VERSION, '2.8.0'), "Only supports tf greater 2.7.0")
+    def test_itex_benchmark_gpu(self):
+        x = tf.compat.v1.placeholder(tf.float32, [1, 56, 56, 16], name="input")
+        top_relu = tf.nn.relu(x)
+        paddings = tf.constant([[0, 0], [1, 1], [1, 1], [0, 0]])
+        x_pad = tf.pad(top_relu, paddings, "CONSTANT")
+        conv_weights = tf.compat.v1.get_variable("weight", [3, 3, 16, 16],
+                                                 initializer=tf.compat.v1.random_normal_initializer())
+        conv = tf.nn.conv2d(x_pad, conv_weights, strides=[1, 2, 2, 1], padding="VALID")
+        normed = tf.compat.v1.layers.batch_normalization(conv)
+        conv_weights2 = tf.compat.v1.get_variable("weight2", [3, 3, 16, 16],
+                                                  initializer=tf.compat.v1.random_normal_initializer())
+        conv2 = tf.nn.conv2d(top_relu, conv_weights2, strides=[1, 2, 2, 1], padding="SAME")
+        normed2 = tf.compat.v1.layers.batch_normalization(conv2)
+        add = tf.raw_ops.Add(x=normed, y=normed2, name='addv2')
+        relu = tf.nn.relu(add)
+        relu6 = tf.nn.relu6(relu, name='op_to_store')
+        out_name = relu6.name.split(':')[0]
+        with tf.compat.v1.Session() as sess:
+            sess.run(tf.compat.v1.global_variables_initializer())
+            output_graph_def = graph_util.convert_variables_to_constants(
+                sess=sess,
+                input_graph_def=sess.graph_def,
+                output_node_names=[out_name])
+
+            quantizer = Quantization('fake_yaml_2.yaml')
+            dataset = quantizer.dataset('dummy', shape=(100, 56, 56, 16), label=True)
+            quantizer.eval_dataloader = common.DataLoader(dataset)
+            quantizer.calib_dataloader = common.DataLoader(dataset)
+            quantizer.model = output_graph_def
+            output_graph = quantizer.fit()
+
+            evaluator = Benchmark('fake_yaml_2.yaml')
+            evaluator.b_dataloader = common.DataLoader(dataset)
+            evaluator.model = output_graph
+            evaluator('performance')
+
+            self.assertEqual(os.environ.get('NC_ENV_CONF'), 'True')
+
 
 if __name__ == '__main__':
     unittest.main()
