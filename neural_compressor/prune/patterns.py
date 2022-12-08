8be9637161d8fa0a49d741861c35e1f1df8d2128
wenhuach21 <wenhua.cheng@intel.com>
2022-12-08 22:46:03 +0800

    torch import issue
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>

--- neural_compressor/prune/patterns.py
+++ neural_compressor/prune/patterns.py
@@ -532,7 +532,7 @@ class PatternNxM(BasePattern):
             data = data.permute(0, 3, 1, 2)
         return data
 
-    def reshape_orig_to_pattern(self, data: torch.Tensor, key):
+    def reshape_orig_to_pattern(self, data, key):
         """Reshape the data(s1,s2) to [s1/N,N,s2,s2/M].
 
         Args:
