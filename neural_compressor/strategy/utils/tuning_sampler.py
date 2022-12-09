83018ef28170f8d2659dd30bb28738857e5c0dec
BiaoFangAIA <108742533+BiaoFangAIA@users.noreply.github.com>
2022-12-10 00:08:52 +0800

    Add hawq_v2 tuning strategy  (#230)
    Signed-off-by: yiliu30 <yi4.liu@intel.com>
    Co-authored-by: lvliang-intel <liang1.lv@intel.com>
    Co-authored-by: chen, suyue <suyue.chen@intel.com>
    Co-authored-by: xinhe <xin3.he@intel.com>
    Co-authored-by: Ray <106061964+yiliu30@users.noreply.github.com>

--- neural_compressor/strategy/utils/tuning_sampler.py
+++ neural_compressor/strategy/utils/tuning_sampler.py
@@ -254,16 +254,18 @@ class FallbackTuningSampler(TuningSampler):
                  tuning_order_lst: List[TuningOrder],
                  initial_op_tuning_cfg: Dict[tuple, Any],
                  op_dtypes: Dict[str, str],
-                 accumulate: bool
+                 accumulate: bool,
+                 skip_first: bool = True
                  ):
         super().__init__(tuning_space, tuning_order_lst, initial_op_tuning_cfg)
         self.op_dtypes = op_dtypes
         self.accumulate = accumulate
+        self.skip_first = skip_first
         pass
 
     def __iter__(self):
         new_tune_cfg = copy.deepcopy(self.initial_op_tuning_cfg)
-        skip_first = True
+        skip_first = self.skip_first
         for op_name_type, target_dtype in self.op_dtypes.items():
             if not self.accumulate:
                 new_tune_cfg = copy.deepcopy(self.initial_op_tuning_cfg)
