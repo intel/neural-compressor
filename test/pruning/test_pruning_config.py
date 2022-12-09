d5795df4b327f1e3b4751b8d9cbf1ea41aa2a803
wenhuach21 <wenhua.cheng@intel.com>
2022-12-10 01:36:18 +0800

    try to fix circular import issue
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>

--- test/pruning/test_pruning_config.py
+++ test/pruning/test_pruning_config.py
@@ -9,7 +9,7 @@ import torch.nn as nn
 from neural_compressor.data import DATASETS
 from neural_compressor.experimental.data.dataloaders.pytorch_dataloader import PyTorchDataLoader
 from neural_compressor.pruning import Pruning
-from neural_compressor.config import WeightPruningConfig
+from neural_compressor.pruner.utils import WeightPruningConfig
 
 class TestPytorchPruning(unittest.TestCase):
     model = torchvision.models.resnet18()
