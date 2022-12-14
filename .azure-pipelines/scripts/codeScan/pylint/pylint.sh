ca1080abaa1e68e4c65d25ebde92163a6b28c9c6
Sun, Xuehao <xuehao.sun@intel.com>
2022-12-14 13:24:58 +0800

    update
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/codeScan/pylint/pylint.sh
+++ .azure-pipelines/scripts/codeScan/pylint/pylint.sh
@@ -6,19 +6,23 @@ RESET="echo -en \\E[0m \\n" # close color
 log_dir="/neural-compressor/.azure-pipelines/scripts/codeScan/scanLog"
 mkdir -p $log_dir
 
+apt-get install -y --no-install-recommends --fix-missing \
+        autoconf \
+        build-essential
+
 pip install -r /neural-compressor/requirements.txt
 
-pip install torch==1.12.0
-pip install horovod \
-    google\
-    autograd\
-    ofa\
-    fvcore\
-    pymoo\
-    onnxruntime_extensions\
-    tf_slim\
-    transformers\
-    flask==2.1.3
+pip install torch==1.12.0 \
+            horovod \
+            google \
+            autograd \
+            ofa \
+            fvcore \
+            pymoo \
+            onnxruntime_extensions \
+            tf_slim \
+            transformers \
+            flask==2.1.3
 
 python -m pylint -f json --disable=R,C,W,E1129 --enable=line-too-long --max-line-length=120 --extension-pkg-whitelist=numpy --ignored-classes=TensorProto,NodeProto \
 --ignored-modules=tensorflow,torch,torch.quantization,torch.tensor,torchvision,fairseq,mxnet,onnx,onnxruntime,intel_extension_for_pytorch /neural-compressor/neural_compressor \
