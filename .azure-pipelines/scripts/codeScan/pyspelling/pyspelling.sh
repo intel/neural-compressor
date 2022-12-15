88101e5bf92a1f9dfdae8d10fcc0acb9fb15298f
XuehaoSun <109065026+XuehaoSun@users.noreply.github.com>
2022-12-15 21:04:58 +0800

    Azure pipeline use Microsoft hosted agent (#277)
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
+++ .azure-pipelines/scripts/codeScan/pyspelling/pyspelling.sh
@@ -12,16 +12,16 @@ pip install -r /neural-compressor/requirements.txt
 sed -i "s|\${DICT_DIR}|$work_dir|g" $work_dir/pyspelling_conf.yaml
 sed -i "s|\${REPO_DIR}|/neural-compressor|g" $work_dir/pyspelling_conf.yaml
 
-pyspelling -c $work_dir/pyspelling_conf.yaml > $log_dir/pyspelling.log
+pyspelling -c $work_dir/pyspelling_conf.yaml >$log_dir/pyspelling.log
 exit_code=$?
 
 $BOLD_YELLOW && echo "-------------------  Current log file output start --------------------------" && $RESET
-cat  $log_dir/pyspelling.log
+cat $log_dir/pyspelling.log
 $BOLD_YELLOW && echo "-------------------  Current log file output end ----------------------------" && $RESET
 
-
-if [ ${exit_code} -ne 0 ] ; then
-    $BOLD_RED && echo "Error!! Please Click on the artifact button to download and view pyspelling error details." && $RESET; exit 1
+if [ ${exit_code} -ne 0 ]; then
+    $BOLD_RED && echo "Error!! Please Click on the artifact button to download and view pyspelling error details." && $RESET
+    exit 1
 fi
-$BOLD_PURPLE && echo "Congratulations, Pyspelling check passed!" && $LIGHT_PURPLE && echo "You can click on the artifact button to see the log details." && $RESET; exit 0
-
+$BOLD_PURPLE && echo "Congratulations, Pyspelling check passed!" && $LIGHT_PURPLE && echo "You can click on the artifact button to see the log details." && $RESET
+exit 0
