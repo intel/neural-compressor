f580ee20a6d4d6e257a2540e0b7f269489378a19
kaiyaointel <kai.yao@intel.com>
2022-12-15 12:47:37 +0800

    hide remove patch in overwrite=True

--- neural_coder/interface.py
+++ neural_coder/interface.py
@@ -448,7 +448,7 @@ def enable(
             sp_overwrite = subprocess.Popen(
                 "patch -d/ -p0 < " + abs_patch_path, env=os.environ, shell=True, stdout=subprocess.PIPE)  # nosec
             sp_overwrite.wait()
-            os.remove(abs_patch_path)  # remove patch after overwrite
+            # os.remove(abs_patch_path)  # remove patch after overwrite
 
         if patch_imports:
             whole_patch_import_modules = ""
