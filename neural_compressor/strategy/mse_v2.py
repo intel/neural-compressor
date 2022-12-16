87734168b98d5c442f0103c8e6f85853fd48f6a7
intel-zhangyi <yi5.zhang@intel.com>
2022-12-16 12:40:04 +0800

    doc(mse_v2): add docstring to mse_v2 strategy.
    Signed-off-by: intel-zhangyi <yi5.zhang@intel.com>

--- neural_compressor/strategy/mse_v2.py
+++ neural_compressor/strategy/mse_v2.py
@@ -15,6 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""MSE_v2 tuning strategy."""
+
 import copy
 from copy import deepcopy
 import numpy as np
@@ -30,57 +32,32 @@ from .utils.helper import tuning_record_msg
 
 @strategy_registry
 class MSE_V2TuneStrategy(TuneStrategy):
-    """The tuning strategy using MSE policy in tuning space.
+    """The mse_v2 tuning strategy which tunes the low precision model with below order.
 
-       This MSE policy runs fp32 model and int8 model seperately to get all activation tensors,
-       and then compares those tensors by MSE algorithm to order all ops with MSE distance for
-       deciding the impact of each op to final accuracy.
-       It will be used to define opwise tuningspace by priority.
-
-    Args:
-        model (object):                        The FP32 model specified for low precision tuning.
-        conf (Class):                          The Conf class instance initialized from user yaml
-                                               config file.
-        q_dataloader (generator):              Data loader for calibration, mandatory for
-                                               post-training quantization.
-                                               It is iterable and should yield a tuple (input,
-                                               label) for calibration dataset containing label,
-                                               or yield (input, _) for label-free calibration
-                                               dataset. The input could be a object, list, tuple or
-                                               dict, depending on user implementation, as well as
-                                               it can be taken as model input.
-        q_func (function, optional):           Reserved for future use.
-        eval_dataloader (generator, optional): Data loader for evaluation. It is iterable
-                                               and should yield a tuple of (input, label).
-                                               The input could be a object, list, tuple or dict,
-                                               depending on user implementation, as well as it can
-                                               be taken as model input. The label should be able
-                                               to take as input of supported metrics. If this
-                                               parameter is not None, user needs to specify
-                                               pre-defined evaluation metrics through configuration
-                                               file and should set "eval_func" parameter as None.
-                                               Tuner will combine model, eval_dataloader and
-                                               pre-defined metrics to run evaluation process.
-        eval_func (function, optional):        The evaluation function provided by user.
-                                               This function takes model as parameter, and
-                                               evaluation dataset and metrics should be
-                                               encapsulated in this function implementation and
-                                               outputs a higher-is-better accuracy scalar value.
-
-                                               The pseudo code should be something like:
-
-                                               def eval_func(model):
-                                                    input, label = dataloader()
-                                                    output = model(input)
-                                                    accuracy = metric(output, label)
-                                                    return accuracy
-        dicts (dict, optional):                The dict containing resume information.
-                                               Defaults to None.
-
+    1. Several times of model-wise tuning for all quantizable operators.
+    2. Try fallback the quantized ops one by one, calculate and sort by the MSE of the 
+       quantizable tensors nearest to the output, and then select the operator with lowest
+       MSE to fallback.
+    3. Try quantize the fallback operators in stage 2 back further for better performance.
+    
+    Note that, only tensorflow framework and pytorch FX backend is currently supported for mse_v2
+    tuning strategy.
     """
 
     def __init__(self, model, conf, q_dataloader, q_func=None,
                  eval_dataloader=None, eval_func=None, dicts=None, q_hooks=None):
+        """Construct an mse_v2 tuning strategy.
+        
+        Args:
+        model (object): The FP32 model specified for low precision tuning.
+        conf (Conf | Config): The configurations for tuning, quantization, evaluation etc.
+        q_dataloader (generator): Data loader for calibration, mandatory for post-training quantization.
+        q_func (function, optional): Training function for quantization aware training.
+        eval_dataloader (generator, optional): Data loader for evaluation.
+        eval_func (function, optional): The evaluation function provided by user. 
+        dicts (dict, optional): The dict containing resume information. Defaults to None.
+        """
+        
         self.ordered_ops = None
         super(
             MSE_V2TuneStrategy,
