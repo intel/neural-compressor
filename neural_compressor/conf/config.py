06415e7348cc556b35a44b42e4201acd8b2cd7bb
intel-zhangyi <yi5.zhang@intel.com>
2022-12-20 14:06:33 +0800

    fix(strategy): rename optimize_level to quant_level.
    Signed-off-by: intel-zhangyi <yi5.zhang@intel.com>

--- neural_compressor/conf/config.py
+++ neural_compressor/conf/config.py
@@ -806,7 +806,7 @@ schema = Schema({
                                                       'pre_post_process_quantization': True},
                                       'model_wise': {'weight': {'bit': [7.0]},
                                                      'activation': {}},
-                                      'optimization_level': 1,
+                                      'quant_level': 1,
                                       }): {
         Optional('approach', default='post_training_static_quant'): And(
             str,
@@ -900,10 +900,10 @@ schema = Schema({
         Optional('op_wise', default=None): {
             str: ops_schema
         },
-        Optional('optimization_level', default=1): And(int, lambda level: level in [0, 1]),
+        Optional('quant_level', default=1): And(int, lambda level: level in [0, 1]),
     },
     Optional('use_bf16', default=True): bool,
-    Optional('optimization_level', default=1): And(int, lambda level: level in [0, 1]),
+    Optional('quant_level', default=1): And(int, lambda level: level in [0, 1]),
     Optional('graph_optimization'): graph_optimization_schema,
     Optional('mixed_precision'): mixed_precision_schema,
 
@@ -1178,7 +1178,7 @@ quantization_default_schema = Schema({
                                                      'activation': {}},
                                     }): dict,
     Optional('use_bf16', default=False): bool,
-    Optional('optimization_level', default=1): int,
+    Optional('quant_level', default=1): int,
     Optional('tuning', default={
         'strategy': {'name': 'basic'},
         'accuracy_criterion': {'relative': 0.01, 'higher_is_better': True},
@@ -1415,7 +1415,7 @@ class Conf(object):
                 'tuning.exit_policy.max_trials': pythonic_config.quantization.max_trials,
                 'tuning.exit_policy.performance_only': pythonic_config.quantization.performance_only,
                 'use_bf16': pythonic_config.quantization.use_bf16,
-                'quantization.optimization_level': pythonic_config.quantization.optimization_level,
+                'quantization.quant_level': pythonic_config.quantization.quant_level,
                 'reduce_range': pythonic_config.quantization.reduce_range
             })
             if pythonic_config.quantization.strategy_kwargs:
