b0ad837b995b7762304a8c2675051ddf66b4fcbc
zehao-intel <zehao.huang@intel.com>
2022-12-20 14:04:22 +0800

    fix config
    Signed-off-by: zehao-intel <zehao.huang@intel.com>

--- examples/tensorflow/nlp/distilbert_base/quantization/ptq/run_inference.py
+++ examples/tensorflow/nlp/distilbert_base/quantization/ptq/run_inference.py
@@ -286,7 +286,8 @@ class Distilbert_base(object):
             from neural_compressor.benchmark import fit
             from neural_compressor.config import BenchmarkConfig
             if ARGS.mode == 'performance':
-                fit(graph, conf=BenchmarkConfig(cores_per_instance=28, num_of_instance=1), b_func=self.eval_func)
+                conf = BenchmarkConfig(cores_per_instance=28, num_of_instance=1)
+                fit(graph, conf, b_func=self.eval_func)
             elif ARGS.mode == 'accuracy':
                 self.eval_func(graph)
 
