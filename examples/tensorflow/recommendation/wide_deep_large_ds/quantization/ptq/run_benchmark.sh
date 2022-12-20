082d6823e240175ba4365eb719621bb126b9db38
lvliang-intel <liang1.lv@intel.com>
2022-12-20 13:31:14 +0800

    INC New API TF oob, wide_deep_large_ds, 3dunet-mlperf examples (#211)
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>
    Co-authored-by: chensuyue <suyue.chen@intel.com>

--- examples/tensorflow/recommendation/wide_deep_large_ds/quantization/ptq/run_benchmark.sh
+++ examples/tensorflow/recommendation/wide_deep_large_ds/quantization/ptq/run_benchmark.sh
@@ -15,9 +15,6 @@ function init_params {
   for var in "$@"
   do
     case $var in
-      --topology=*)
-          topology=$(echo $var |cut -f2 -d=)
-      ;;
       --dataset_location=*)
           dataset_location=$(echo $var |cut -f2 -d=)
       ;;
@@ -30,13 +27,6 @@ function init_params {
       --batch_size=*)
           batch_size=$(echo $var |cut -f2 -d=)
       ;;
-      --iters=*)
-          iters=$(echo ${var} |cut -f2 -d=)
-      ;;
-      *)
-          echo "Error: No such parameter: ${var}"
-          exit 1
-      ;;
     esac
   done
 
@@ -45,8 +35,8 @@ function init_params {
 function define_mode {
     if [[ ${mode} == "accuracy" ]]; then
       mode_cmd=" --benchmark --accuracy_only"
-    elif [[ ${mode} == "benchmark" ]]; then
-      mode_cmd=" --steps ${iters} --benchmark"
+    elif [[ ${mode} == "performance" ]]; then
+      mode_cmd=" --benchmark"
     else
       echo "Error: No such mode: ${mode}"
       exit 1
