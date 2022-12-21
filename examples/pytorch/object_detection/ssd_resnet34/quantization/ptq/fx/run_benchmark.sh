b9557db7e8ec1a84fcced5385fcba43280dea5c1
spycsh <sihan.chen@intel.com>
2022-12-21 14:28:34 +0800

    fix shell command

--- examples/pytorch/object_detection/ssd_resnet34/quantization/ptq/fx/run_benchmark.sh
+++ examples/pytorch/object_detection/ssd_resnet34/quantization/ptq/fx/run_benchmark.sh
@@ -51,7 +51,7 @@ function run_benchmark {
 
     if [[ ${mode} == "accuracy" ]]; then
         mode_cmd="--accuracy "
-        mode_cmd="--batch_size ${batch_size} "
+        mode_cmd="${mode_cmd} --batch_size ${batch_size} "
     elif [[ ${mode} == "performance" ]]; then
         mode_cmd="--benchmark "
     else
