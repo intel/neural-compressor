c6aa57f5f6f491a5dfd6880c551647ba7ef020c3
BiaoFangAIA <108742533+BiaoFangAIA@users.noreply.github.com>
2022-12-21 15:46:56 +0800

    Update hawq_v2.py

--- neural_compressor/strategy/hawq_v2.py
+++ neural_compressor/strategy/hawq_v2.py
@@ -109,7 +109,7 @@ class HAWQ_V2TuneStrategy(TuneStrategy):
             yield op_tuning_cfg
         # Start compute the hessian trace
         logger.info(f"**************  Start compute the hessian trace  *****************")
-        target_dtype = "int8"
+        target_dtype = "fp32"
         hawq_v2_criterion = self.cfg.tuning.strategy.hawq_v2_loss
         # assert hawq_v2_criterion is not None, "HAWQ-V2 strategy needs model loss function to compute the gradient, \
         #     Please assign it by strategy_kwargs({'hawq_v2_loss': hawq_v2_loss})."
