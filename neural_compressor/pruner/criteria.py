fix gradient based criterion bug
Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>

neural_compressor/pruner/criteria.py
@@ -113,7 +113,7 @@ class GradientCriterion(PruningCriterion):
         super(GradientCriterion, self).__init__(modules, config)
         assert self.config.end_step > 0, "please set end_step > 0 for gradient based criterion"
 
-    def on_after_optimizer_step(self):
+    def on_before_optimizer_step(self):
         """Calculate and store the pruning scores based on gradient criterion."""
         with torch.no_grad():
             for key in self.modules.keys():
@@ -143,7 +143,7 @@ class SnipCriterion(PruningCriterion):
         super(SnipCriterion, self).__init__(modules, config)
         assert self.config.end_step > 0, "please set end_step > 0 for gradient based criterion"
 
-    def on_after_optimizer_step(self):
+    def on_before_optimizer_step(self):
         """Calculate and store the pruning scores based on snip criterion."""
         ##self.mask_weights()
         with torch.no_grad():
@@ -180,7 +180,7 @@ class SnipMomentumCriterion(PruningCriterion):
         self.alpha = 0.9
         self.beta = 1.0
 
-    def on_after_optimizer_step(self):
+    def on_before_optimizer_step(self):
         """Calculate and store the pruning scores based on snip_momentum criterion."""
         with torch.no_grad():
             for key in self.modules.keys():