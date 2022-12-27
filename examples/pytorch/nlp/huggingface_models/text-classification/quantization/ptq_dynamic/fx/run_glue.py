7e9b9f145fd7f44fc35ddcd6517585ba4ce0ee25
changwa1 <chang1.wang@intel.com>
2022-12-27 15:23:23 +0800

    fix nlp release bugs
    Signed-off-by: changwa1 <chang1.wang@intel.com>

--- examples/pytorch/nlp/huggingface_models/text-classification/quantization/ptq_dynamic/fx/run_glue.py
+++ examples/pytorch/nlp/huggingface_models/text-classification/quantization/ptq_dynamic/fx/run_glue.py
@@ -501,7 +501,7 @@ def main():
         from neural_compressor.quantization import fit
         from neural_compressor.config import PostTrainingQuantConfig, TuningCriterion
         tuning_criterion = TuningCriterion(max_trials=600)
-        conf = PostTrainingQuantConfig(approach="dynamic", backend="pytorch", tuning_criterion=tuning_criterion)
+        conf = PostTrainingQuantConfig(approach="dynamic", tuning_criterion=tuning_criterion)
         q_model = fit(model, conf=conf, eval_func=eval_func)
         from neural_compressor.utils.load_huggingface import save_for_huggingface_upstream
         save_for_huggingface_upstream(q_model, tokenizer, training_args.output_dir)
