18944f8a6e2788c1dbfa6bad36e297237e9193c7
Xin He <xin3.he@intel.com>
2022-12-27 14:03:42 +0800

    fix bug
    Signed-off-by: Xin He <xin3.he@intel.com>

--- examples/pytorch/object_detection/yolo_v3/quantization/ptq/eager/test.py
+++ examples/pytorch/object_detection/yolo_v3/quantization/ptq/eager/test.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
         from neural_compressor.experimental import Quantization, common
         dataset = ListDataset(valid_path, img_size=opt.img_size, augment=False, multiscale=False)
         dataloader = torch.utils.data.DataLoader(
-            dataset, batch_size=opt.batch_size, shuffle=False, num_workers=1, collate_fn=dataset.collate_fn
+            dataset, batch_size=opt.batch_size, shuffle=True, num_workers=1, collate_fn=dataset.collate_fn
         )
         nc_dataloader = yolo_dataLoader(dataloader)
         quantizer = Quantization("./conf.yaml")
