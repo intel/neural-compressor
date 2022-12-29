081e914cd17d0bdb6f2bda38ac68ec66869a20f8
changwa1 <chang1.wang@intel.com>
2022-12-29 11:11:36 +0800

    fix accuracy mode
    Signed-off-by: changwa1 <chang1.wang@intel.com>

--- examples/pytorch/object_detection/ssd_resnet34/quantization/ptq/fx/python/main.py
+++ examples/pytorch/object_detection/ssd_resnet34/quantization/ptq/fx/python/main.py
@@ -614,12 +614,9 @@ def main():
         import torchvision.transforms as transforms
         import torch
         from neural_compressor.experimental.data.datasets.coco_dataset import COCONpy
-        dataset = COCONpy(root='./', npy_dir='preprocessed/coco-1200-pt/NCHW/val2017/',
-            anno_dir='convert_dataset/annotations/instances_val2017.json')
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size)
         int8_model = load(os.path.abspath(os.path.expanduser(args.tuned_checkpoint)),
                           raw_model,
-                          dataloader=dataloader)
+                          dataloader=None)
         if args.accuracy:
             eval_func(int8_model)
         elif args.benchmark:
