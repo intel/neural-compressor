60f427cde1d975f3160e8bb84a43053fb5a0ff6f
ZhangJianyu <zhang.jianyu@outlook.com>
2022-12-29 11:06:14 +0800

    fix err, use /version.html

--- build_docs/update_html.py
+++ build_docs/update_html.py
@@ -2,13 +2,13 @@ import os, sys
 import glob
 
 
-def oupdate_version_link(version, folder_name, index_file):
+def update_version_link(version, folder_name, index_file):
     index_buf = ""
     with open(index_file, "r") as f:
         index_buf = f.read()
         key_str='  <div class="version">\n                {}\n              </div>'.format(version)
         version_list = '''<div class="version">
-              <a href="../versions.html">{}▼</a>
+              <a href="/versions.html">{}▼</a>
               <p>Click link above to switch version</p>
             </div>'''.format(folder_name)
         #print(index_buf.find(key_str))
