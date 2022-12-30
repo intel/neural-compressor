7a39c862bf37a7b890824621dafedef8b4161c33
lvliang-intel <liang1.lv@intel.com>
2022-12-30 13:35:37 +0800

    Fix OOB centernet_hg104 benchmark issue (#369)
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>

--- examples/tensorflow/oob_models/quantization/ptq/tf_benchmark.py
+++ examples/tensorflow/oob_models/quantization/ptq/tf_benchmark.py
@@ -45,6 +45,11 @@ def metrics_generator(array, tolerance):
     return max_diff, mean_diff, median_diff, success_rate
 
 def initialize_graph(model_details, args, od_graph_def):
+    if args.use_nc and not od_graph_def.node:
+        from neural_compressor.model.model import Model
+        model = Model(os.path.join(os.getcwd(), model_details['model_dir']))
+        od_graph_def = model.graph_def
+
     graph = tf_v1.Graph()
     with graph.as_default():
         input_variables = {
@@ -56,11 +61,6 @@ def initialize_graph(model_details, args, od_graph_def):
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 od_graph_def = delete_assign(od_graph_def)
-
-        elif args.use_nc and not od_graph_def.node:
-            from neural_compressor.model.model import Model
-            model = Model(os.path.join(os.getcwd(), model_details['model_dir']))
-            od_graph_def = model.graph_def
 
         # optimize for inference
         if not args.disable_optimize:
