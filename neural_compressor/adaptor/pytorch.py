36c17bcc390820da605cea3610d0243da59da6cc
Chang Wang <chang1.wang@intel.com>
2023-01-11 22:38:40 +0800

    fix name (#1555)

--- neural_compressor/adaptor/pytorch.py
+++ neural_compressor/adaptor/pytorch.py
@@ -3314,7 +3314,7 @@ class PyTorch_FXAdaptor(TemplateAdaptor):
                     sub_name = prefix + '--' + node.target
                 else:
                     sub_name = node.target
-                if not hasattr(model, sub_name):
+                if not hasattr(model, node.target):
                     continue
                 if 'scale' in node.target:
                     tune_cfg['get_attr'][sub_name] = float(getattr(model, node.target))
