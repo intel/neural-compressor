a47957df2d5cff986e8bf14884a170543754f9cc
Xin He <xin3.he@intel.com>
2023-02-02 12:40:34 +0800

    add percentile observer
    Signed-off-by: Xin He <xin3.he@intel.com>

--- neural_compressor/adaptor/torch_utils/observer.py
+++ neural_compressor/adaptor/torch_utils/observer.py
@@ -221,3 +221,140 @@ class FP8HistogramObserver(HistogramObserver):
             self.max_val.detach_().resize_(combined_max.shape)
             self.max_val.copy_(combined_max)
         return x_orig
+
+
+class FP8PercentileObserver(HistogramObserver):
+    def __init__(
+        self,
+        bins: int = 2048,
+        upsample_rate: int = 128,
+        dtype: torch.dtype = torch.quint8,
+        qscheme=torch.per_tensor_affine,
+        reduce_range=False,
+        quant_min=None,
+        quant_max=None,
+        factory_kwargs=None,
+        eps=torch.finfo(torch.float32).eps,
+        percentile=0.999,
+    ) -> None:
+        # bins: The number of bins used for histogram calculation.
+        super(FP8PercentileObserver, self).__init__(
+            bins=bins,
+            upsample_rate=upsample_rate,
+            dtype=dtype,
+            qscheme=qscheme,
+            reduce_range=reduce_range,
+            quant_min=quant_min,
+            quant_max=quant_max,
+            factory_kwargs=factory_kwargs,
+            eps=eps,
+        )
+        self.percentile = percentile
+
+    def _non_linear_param_search(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        r"""Non-linear parameter search.
+
+        An approximation for L2 error minimization for selecting min/max.
+        By selecting new min/max, we filter out outliers in input distribution.
+        This follows the implementation of NormMinimization::NonlinearQuantizationParamsSearch in
+        caffe2/quantization/server/norm_minimization.cc
+        """
+        assert self.histogram.size()[0] == self.bins, "bins mistmatch"
+        bin_width = (self.max_val - self.min_val) / self.bins
+
+        # cumulative sum
+        total = torch.sum(self.histogram).item()
+        cSum = torch.cumsum(self.histogram, dim=0)
+
+        alpha = 0.0  # lower bound
+        beta = 1.0  # upper bound
+        start_bin = 0
+        end_bin = self.bins - 1
+
+        # Find the next step
+        next_alpha = alpha
+        next_beta = beta * self.percentile
+
+        # find the right bins between the quantile bounds
+        # keep the left bins at zero due to fp8 symmetry
+        l = 0
+        r = end_bin
+        while r > start_bin and cSum[r] > next_beta * total:
+            r = r - 1
+            print(r)
+
+        # decide the next move
+        next_start_bin = start_bin
+        next_end_bin = end_bin
+        if (l - start_bin) <= (end_bin - r):
+            # move the end bin
+            next_end_bin = r
+            beta = next_beta
+
+        start_bin = next_start_bin
+        end_bin = next_end_bin
+
+        new_min = self.min_val + bin_width * start_bin
+        new_max = self.min_val + bin_width * (end_bin + 1)
+        return new_min, new_max
+
+    def forward(self, x_orig: torch.Tensor) -> torch.Tensor:
+        if x_orig.numel() == 0:
+            return x_orig
+        x = x_orig.detach()
+        # use abs due to fp8 symmetry 
+        x = torch.abs(x)
+        min_val = self.min_val
+        max_val = self.max_val
+        same_values = min_val.item() == max_val.item()
+        is_uninitialized = min_val == float("inf") and max_val == float("-inf")
+        if is_uninitialized or same_values:
+            min_val, max_val = torch.aminmax(x)
+            self.min_val.resize_(min_val.shape)
+            self.min_val.copy_(min_val)
+            self.max_val.resize_(max_val.shape)
+            self.max_val.copy_(max_val)
+            assert (
+                min_val.numel() == 1 and max_val.numel() == 1
+            ), "histogram min/max values must be scalar."
+            torch.histc(
+                x, self.bins, min=int(min_val), max=int(max_val), out=self.histogram
+            )
+        else:
+            new_min, new_max = torch.aminmax(x)
+            combined_min = torch.min(new_min, min_val)
+            combined_max = torch.max(new_max, max_val)
+            # combine the existing histogram and new histogram into 1 histogram
+            # We do this by first upsampling the histogram to a dense grid
+            # and then downsampling the histogram efficiently
+            (
+                combined_min,
+                combined_max,
+                downsample_rate,
+                start_idx,
+            ) = self._adjust_min_max(combined_min, combined_max, self.upsample_rate)
+            assert (
+                combined_min.numel() == 1 and combined_max.numel() == 1
+            ), "histogram min/max values must be scalar."
+            combined_histogram = torch.histc(
+                x, self.bins, min=int(combined_min), max=int(combined_max)
+            )
+            if combined_min == min_val and combined_max == max_val:
+                combined_histogram += self.histogram
+            else:
+                combined_histogram = self._combine_histograms(
+                    combined_histogram,
+                    self.histogram,
+                    self.upsample_rate,
+                    downsample_rate,
+                    start_idx,
+                    self.bins,
+                )
+
+            self.histogram.detach_().resize_(combined_histogram.shape)
+            self.histogram.copy_(combined_histogram)
+            self.min_val.detach_().resize_(combined_min.shape)
+            self.min_val.copy_(combined_min)
+            self.max_val.detach_().resize_(combined_max.shape)
+            self.max_val.copy_(combined_max)
+        return x_orig
