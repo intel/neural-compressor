bd5fe10764c5c64767f1524440bb73102f716e27
Kaihui-intel <kaihui.tang@intel.com>
2023-02-03 14:49:11 +0800

    fix fake eval in UT
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- test/strategy/test_distributed_tuning.py
+++ test/strategy/test_distributed_tuning.py
@@ -80,20 +80,26 @@ class TestDistributedTuning(unittest.TestCase):
     def test_pt_stage_1_met(self):
         logger.info("*** Test: distributed tuning testing test_pt_stage_1_met start.")
 
+        num_processes = 3
+        logger.info(f"*** Test: distributed tuning testing test_pt_stage_1_met start. NP: {num_processes} (load acc and perf from local).")
+
         # model
         resnet18 = torchvision.models.resnet18()
 
         # fake evaluation function
-        acc_lst =  [2.0, 1.0, 2.1, 2.2, 2.3, 2.0]
-        perf_lst = [2.0, 1.5, 1.0, 0.5, 0.1, 4.0]
+        num_baseline = num_processes # TODO, replace num_baseline with 1 when evaluating baseline only once.
+        acc_lst =  [2.0] * num_baseline + [1.0, 2.1, 2.2, 2.3, 2.0] #the tuning result (2.1)
+        perf_lst = [2.0] * num_baseline + [2.5, 2.0, 1.5, 1.1, 5.0] 
 
-        self.test_pt_stage_1_met_index = -1
+        # make sure this path can be accessed by all nodes
+        acc_perf_data_file_path = 'test_pt_stage_1_met.json' 
+        save_acc_perf_to_local(acc_lst, perf_lst, acc_perf_data_file_path)
+
         def _fake_eval(model):
-            self.test_pt_stage_1_met_index += 1
-            print("perf,acc index: {}".format(self.test_pt_stage_1_met_index))
-            perf = perf_lst[self.test_pt_stage_1_met_index]
+            acc, perf = next_acc_and_perf(acc_perf_data_file_path)
+            logger.info(f"Current evaluate result: acc {acc}, perf: {perf}.")
             time.sleep(perf)
-            return acc_lst[self.test_pt_stage_1_met_index]
+            return acc
 
         # dataset and dataloader
         dataset = Datasets("pytorch")["dummy"](((100, 3, 3, 1)))
@@ -113,27 +119,33 @@ class TestDistributedTuning(unittest.TestCase):
     def test_pt_stage_3_fp32_met(self):
         logger.info("*** Test: distributed tuning testing test_pt_stage_3_fp32_met start.")
 
+        num_processes = 3
+        logger.info(f"*** Test: distributed tuning testing test_pt_stage_1_met start. NP: {num_processes} (load acc and perf from local).")
+
         # model
         resnet18 = torchvision.models.resnet18()
 
         # fake evaluation function
-        acc_lst =  [2.0] + [1.0] * 16 + [2.0]
-        perf_lst = [2.0] + [1.0] * 16 + [1.0]
+        num_baseline = num_processes # TODO, replace num_baseline with 1 when evaluating baseline only once.
+        acc_lst =  [2.0] * num_baseline + [1.0] * 16 + [2.0]
+        perf_lst = [2.0] * num_baseline + [1.0] * 16 + [1.0]
 
-        self.test_pt_stage_3_fp32_met_index = -1
+        # make sure this path can be accessed by all nodes
+        acc_perf_data_file_path = 'test_pt_stage_3_fp32_met.json' 
+        save_acc_perf_to_local(acc_lst, perf_lst, acc_perf_data_file_path)
+
         def _fake_eval(model):
-            self.test_pt_stage_3_fp32_met_index += 1
-            print("perf,acc index: {}".format(self.test_pt_stage_3_fp32_met_index))
-            perf = perf_lst[self.test_pt_stage_3_fp32_met_index]
+            acc, perf = next_acc_and_perf(acc_perf_data_file_path)
+            logger.info(f"Current evaluate result: acc {acc}, perf: {perf}.")
             time.sleep(perf)
-            return acc_lst[self.test_pt_stage_3_fp32_met_index]
+            return acc
 
         # dataset and dataloader
         dataset = Datasets("pytorch")["dummy"](((100, 3, 3, 1)))
         dataloader = DATALOADERS["pytorch"](dataset)
 
         # tuning and accuracy criterion
-        conf = PostTrainingQuantConfig(use_distributed_tuning=True)
+        conf = PostTrainingQuantConfig(use_distributed_tuning=Fals)
         # fit
         q_model = fit(model=resnet18,
                       conf=conf,
@@ -146,20 +158,26 @@ class TestDistributedTuning(unittest.TestCase):
     def test_pt_stage_4_fp32_met(self):
         logger.info("*** Test: distributed tuning testing test_pt_stage_3_met start.")
 
+        num_processes = 3
+        logger.info(f"*** Test: distributed tuning testing test_pt_stage_1_met start. NP: {num_processes} (load acc and perf from local).")
+
         # model
         resnet18 = torchvision.models.resnet18()
 
         # fake evaluation function
-        acc_lst =  [2.0] + [1.0] * 37 + [2.0]
-        perf_lst = [2.0] + [1.0] * 37 + [1.0]
+        num_baseline = num_processes # TODO, replace num_baseline with 1 when evaluating baseline only once.
+        acc_lst =  [2.0] * num_baseline + [1.0] * 37 + [2.0]
+        perf_lst = [2.0] * num_baseline + [1.0] * 37 + [1.0]
 
-        self.test_pt_stage_3_met_index = -1
+        # make sure this path can be accessed by all nodes
+        acc_perf_data_file_path = 'test_pt_stage_stage_4_fp32_met.json' 
+        save_acc_perf_to_local(acc_lst, perf_lst, acc_perf_data_file_path)
+
         def _fake_eval(model):
-            self.test_pt_stage_3_met_index += 1
-            print("perf,acc index: {}".format(self.test_pt_stage_3_met_index))
-            perf = perf_lst[self.test_pt_stage_3_met_index]
+            acc, perf = next_acc_and_perf(acc_perf_data_file_path)
+            logger.info(f"Current evaluate result: acc {acc}, perf: {perf}.")
             time.sleep(perf)
-            return acc_lst[self.test_pt_stage_3_met_index]
+            return acc
 
         # dataset and dataloader
         dataset = Datasets("pytorch")["dummy"](((100, 3, 3, 1)))
@@ -178,21 +196,26 @@ class TestDistributedTuning(unittest.TestCase):
 
     def test_pt_stage_not_met(self):
         logger.info("*** Test: distributed tuning testing test_pt_stage_not_met start.")
+        num_processes = 3
+        logger.info(f"*** Test: distributed tuning testing test_pt_stage_1_met start. NP: {num_processes} (load acc and perf from local).")
 
         # model
         resnet18 = torchvision.models.resnet18()
 
         # fake evaluation function
-        acc_lst =  [2.0] + [1.0] * 57 + [2.0]
-        perf_lst = [2.0] + [1.0] * 57 + [1.0]
+        num_baseline = num_processes # TODO, replace num_baseline with 1 when evaluating baseline only once.
+        acc_lst =  [2.0] * num_baseline + [1.0] * 57
+        perf_lst = [2.0] * num_baseline + [1.0] * 57
 
-        self.test_pt_stage_not_met_index = -1
+        # make sure this path can be accessed by all nodes
+        acc_perf_data_file_path = 'test_pt_stage_not_met.json' 
+        save_acc_perf_to_local(acc_lst, perf_lst, acc_perf_data_file_path)
+
         def _fake_eval(model):
-            self.test_pt_stage_not_met_index += 1
-            print("perf,acc index: {}".format(self.test_pt_stage_not_met_index))
-            perf = perf_lst[self.test_pt_stage_not_met_index]
+            acc, perf = next_acc_and_perf(acc_perf_data_file_path)
+            logger.info(f"Current evaluate result: acc {acc}, perf: {perf}.")
             time.sleep(perf)
-            return acc_lst[self.test_pt_stage_not_met_index]
+            return acc
 
         # dataset and dataloader
         dataset = Datasets("pytorch")["dummy"](((100, 3, 3, 1)))
