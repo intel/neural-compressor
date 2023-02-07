ef655ae26802a6fb1cd9383c66c3e628e1f78373
Lv, Liang1 <liang1.lv@intel.com>
2023-02-07 13:19:44 +0800

    fix mobilenet_v1 benchmark issue
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>

--- examples/tensorflow/image_recognition/SavedModel/mobilenet_v1/quantization/ptq/main.py
+++ examples/tensorflow/image_recognition/SavedModel/mobilenet_v1/quantization/ptq/main.py
@@ -116,6 +116,7 @@ class eval_object_detection_optimized_graph(object):
             from neural_compressor.config import BenchmarkConfig
             if args.mode == 'performance':
                 conf = BenchmarkConfig(cores_per_instance=4, num_of_instance=7)
+                from neural_compressor.utils.create_obj_from_config import create_dataloader
                 dataloader_args = {
                     'batch_size': args.batch_size,
                     'dataset': {"ImageRecord": {'root': args.dataset_location}},
