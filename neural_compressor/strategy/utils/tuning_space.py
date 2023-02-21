fb7a9da5217411c2ccb26030dab32c0c5a85904d
yiliu30 <yi4.liu@intel.com>
2023-02-21 11:10:37 +0800

    fixed the merge
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- neural_compressor/strategy/utils/tuning_space.py
+++ neural_compressor/strategy/utils/tuning_space.py
@@ -22,7 +22,6 @@ import os
 import re
 from typing import Dict, Tuple
 from copy import deepcopy
-from enum import IntEnum
 from ...utils import logger
 from .util import OrderedDefaultDict
 from .tuning_structs import OpTuningConfig
@@ -119,7 +118,6 @@ class TuningSpace:
     """Not displayed in API Docs.
     
     1) capability -> internal format -> merge -> tuning space (tree)
-    2) capability -> merge -> internal format -> tuning space (tree)
     
     """
     
@@ -205,91 +203,70 @@ class TuningSpace:
                 tuning_item = TuningItem(name=name, options=options, item_type=name)
                 quant_mode_item.append(tuning_item)
 
-    def _merge_op_cfg(self, op_cap, op_user_cfg, fw_op_cap):
+    def _merge_op_cfg(self, cur_op_cap, op_user_cfg, fw_op_cap):
         """Merge the op cfg with user cfg.
         
-        dtype: ['int8', 'fp32'] -> ('static', ('int8', 'signed')) and ('precision', ('fp32'))
-        dtype: ['fp32'] -> ('precision', ('fp32'))
-        step1, For dtype, filter the invalid data type. Override the fwk data type if the valid data type is not empty.
-        step2. For tuning item, filter the invalid options, Override the options if the valid option is not empty.
-        Skip override if the valid data type or options is empty.
-        user_precision_set = {}
-        op_user_cfg:
-            {
-                'weight':{
-                    'dtype': ['int8', 'fp32'], 
-                    'scheme': ['sym'],
-                    'algorithm': ['minmax'],
-                    'granularity': ['per_tensor']
+        op_user_cfg:{
+            'activation':{
+                'dtype': ['fp32']
                 },
-                'activation': {
-                    'dtype': ['uint8'],
-                    'scheme': ['asym'],
-                    'algorithm': ['minmax'],
-                    'granularity': ['per_tensor']
-                    }
+            'weight':{
+                'dtype': ['fp32']
+                }
             }
+            
+        Step1. merge dtype, get the intersection between fw_op_cap and op_user_cfg.
+        Step2. merge method options.
+        
+        1) the intersection(fw_op_cap.dtype, op_user_cfg.data)) is None, use the cur_op_cap.
+        2) the intersection(fw_op_cap.dtype, op_user_cfg.data)) is not None, use the merge result.
+        
+        # if dtype and type intersection with precision set -> only keep the intersection precision
+        # and remove the quantization.
+        # else(no dtype, or no intersection) -> merge the method
+
+        Args:
+            cur_op_cap: current capability.
+            op_user_cfg: The user capability. 
+            fw_op_cap: The fwk capability(baseline).
+            
+        Returns:
+            Return the merged capability.
         """
         from .util import extract_data_type, reverted_data_type
         fw_op_cap = deepcopy(fw_op_cap)
+        new_op_cap = deepcopy(cur_op_cap)
         for att in ['activation', 'weight']:
-            if att in fw_op_cap and op_user_cfg.get(att, None) is not None:
+            if op_user_cfg.get(att, None) is not None:
                 user_dtype_lst = op_user_cfg[att]['dtype'] if op_user_cfg[att]['dtype'] is not None else []
                 # Merge the precision part.
-                fwk_att_precision_cap = fw_op_cap['precision'][att]
-                fwk_precision_lst = list(fwk_att_precision_cap.keys())
+                fwk_att_precision_cap = fw_op_cap['precision'].get(att, {})
+                fwk_precision_set = set(fwk_att_precision_cap.keys())
                 # The intersection of user cfg and fwk capability.
-                valid_precision_set = set(fwk_precision_lst).intersection(set(user_dtype_lst))
+                valid_precision_set = set(fwk_precision_set).intersection(set(user_dtype_lst))
                 if len(valid_precision_set) != 0:
-                    # TODO if dtype is ['int8'], no 'fp32'?
-                    for precision_name in fwk_precision_lst:
-                        if precision_name not in valid_precision_set:
-                            fwk_att_precision_cap.pop(precision_name, None)
-                # Merge the quantization part.
-                quant_modes_lst = list(fw_op_cap.keys())
-                quant_modes_lst.remove('precision')
-                for quant_mode in quant_modes_lst:
-                    data_type_cap = fw_op_cap[quant_mode][att]
-                    data_type_lst = list(data_type_cap.keys())
-                    fwk_data_type_lst = []
-                    for data_type in data_type_lst:
-                        for signed_flag in fw_op_cap[quant_mode][att][data_type].keys():
-                            fwk_data_type_lst.append(reverted_data_type(signed_flag, data_type))
-                    valid_quant_dtype_lst = set(fwk_data_type_lst).intersection(user_dtype_lst)
-                    if len(valid_quant_dtype_lst) != 0:
-                        # Filter the valid dtype
-                        # TODO if dtype is ['fp32']
-                        if len(valid_precision_set) == 0:
-                            for precision in fwk_precision_lst:
-                                fw_op_cap['precision'][att].pop(precision, None)
-                                if len(fw_op_cap['precision'][att]) == 0:
-                                    fw_op_cap['precision'].pop(att, None)
-                                if len(fw_op_cap['precision']) == 0:
-                                    fw_op_cap.pop('precision', None)
-                        for dtype in fwk_data_type_lst:
-                            if dtype not in valid_quant_dtype_lst:
-                                signed_flag, data_type = extract_data_type(dtype)
-                                fw_op_cap[quant_mode][att][data_type].pop(signed_flag, None)
-                            if len(fw_op_cap[quant_mode][att][data_type]) == 0:
-                                fw_op_cap[quant_mode][att].pop(data_type, None)
-                            if len(fw_op_cap[quant_mode][att]) == 0:
-                                fw_op_cap[quant_mode].pop(att, None)
-                    else:
-                        # No valid quant data type but have valid precision dtype
-                        if len(valid_precision_set) != 0:
-                            fw_op_cap.pop(quant_mode, None)
+                    logger.debug(f"only keep precision...")
+                    new_op_cap = dict(filter(lambda item: item[0] == 'precision', new_op_cap.items()))
+                    new_op_cap['precision'][att] = dict(filter(lambda item: item[0] in valid_precision_set,\
+                        fw_op_cap['precision'][att].items()))
+                else:                    
                     # Filter the valid options for tuning item
-                    if quant_mode in fw_op_cap:
-                        for data_type in fw_op_cap[quant_mode][att]:
-                            for signed_flag in fw_op_cap[quant_mode][att][data_type]:
+                    for quant_mode in new_op_cap:
+                        if quant_mode == 'precision': continue
+                        for data_type in new_op_cap[quant_mode][att]:
+                            for signed_flag in new_op_cap[quant_mode][att][data_type]:
+                                cur_items = new_op_cap[quant_mode][att][data_type][signed_flag]
                                 fwk_items = fw_op_cap[quant_mode][att][data_type][signed_flag]
-                                for item_name, item_options in op_user_cfg[att].items():
-                                    if item_name not in ['dtype', 'quant_mode'] and item_options:
-                                        options_intersection = set(fwk_items[item_name]).intersection(set(item_options))
+                                for method_name, method_options in op_user_cfg[att].items():
+                                    if method_name not in ['dtype', 'quant_mode'] and method_options:
+                                        # filter the method options
+                                        options_intersection = set(fwk_items[method_name]\
+                                            ).intersection(set(method_options))
+                                        # merge with fwk, if intersection -> use intersection
                                         if len(options_intersection) > 0:
-                                            fwk_items[item_name] = [option for option in fwk_items[item_name] if\
+                                            cur_items[method_name] = [option for option in fwk_items[method_name] if\
                                                 option in options_intersection]
-        return fw_op_cap
+        return new_op_cap
 
     def _merge_optype_wise_cfg(self, cap: Dict, optype_wise_usr_cfg: Dict, fw_cap: Dict):
         for op_type, op_user_cfg in optype_wise_usr_cfg.items():
@@ -528,18 +505,23 @@ class TuningSpace:
         logger.info(self.ops_data_type)
         return parsed_cap
     
+
+
+            
+    
     def _create_tuning_space(self, capability, usr_cfg):
         """Create tuning space.
         
-        step1. merge the capability with usr_cfg
-        step2. create the tuning space
+        steo1. convert the capability into internal format.
+        step2. merge the capability with usr_cfg
+        step3. create the tuning space
         :param capability:
         :param usr_cfg:
         :return:
         """
-        # using new tuning space.
-        tmp_cap2 = self._parse_cap_helper(deepcopy(capability['op']))
-        capability['op'] = tmp_cap2
+        
+        
+        capability['op'] = self._parse_cap_helper(deepcopy(capability['op']))
         if usr_cfg:
             logger.info(f"#############  Before merged with user cfg")
             logger.info(capability)
--- test/strategy/test_tuning_space_v2.py
+++ test/strategy/test_tuning_space_v2.py
@@ -5,7 +5,7 @@ from copy import deepcopy
 import unittest
 
 op_cap = {
-    # op have both weight and activation and support static/dynamic/fp32
+    # op1 have both weight and activation and support static/dynamic/fp32/b16
     ('op_name1', 'op_type1'): [
         {
             'activation':
@@ -76,7 +76,7 @@ op_cap = {
                 }
         },
     ],
-    # op have both weight and activation and support static/dynamic/fp32
+    # op2 have both weight and activation and support static/dynamic/fp32
     ('op_name2', 'op_type1'): [
         {
             'activation':
@@ -121,89 +121,7 @@ op_cap = {
                 }
         },
     ],
-    # op have both weight and activation and support static/fp32
-    ('op_name3', 'op_type2'): [
-        {
-            'activation':
-                {
-                    'dtype': ['int8'],
-                    'quant_mode': 'static',
-                    'scheme': ['sym'],
-                    'granularity': ['per_channel'],
-                    'algorithm': ['minmax', 'kl']
-                },
-            'weight':
-                {
-                    'dtype': ['int8'],
-                    'scheme': ['sym'],
-                    'granularity': ['per_channel'],
-                    'algorithm': ['minmax', 'kl']
-                }
-        },
-        {
-            'activation':
-                {
-                    'dtype': 'fp32'
-                },
-            'weight':
-                {
-                    'dtype': 'fp32'
-                }
-        },
-    ],
-    # op have both weight and activation and support dynamic/fp32
-    ('op_name4', 'op_type3'): [
-        {
-            'activation':
-                {
-                    'dtype': ['int8'],
-                    'quant_mode': 'static',
-                    'scheme': ['sym'],
-                    'granularity': ['per_channel', 'per_tensor'],
-                    'algorithm': ['minmax', 'kl']
-                },
-        },
-        {
-            'activation':
-                {
-                    'dtype': ['int8'],
-                    'quant_mode': 'dynamic',
-                    'scheme': ['sym'],
-                    'granularity': ['per_channel', 'per_tensor'],
-                    'algorithm': ['minmax']
-                },
-        },
-        {
-            'activation':
-                {
-                    'dtype': 'fp32'
-                },
-        },
-    ]
 }
-
-
-op_cap2 = {
-    # The granularity of op activation do not support per_tensor.
-    ('op_name4', 'op_type1'): [
-        {
-            'activation':
-                {
-                    'dtype': ['int8'],
-                    'quant_mode': 'static',
-                    'scheme': ['sym'],
-                    'granularity': ['per_channel'],
-                    'algorithm': ['minmax', 'kl']
-                },
-            'weight':
-                {
-                    'dtype': ['int8'],
-                    'scheme': ['sym'],
-                    'granularity': ['per_channel', 'per_tensor']
-                }
-        },]
-}
-
 
 class TestTuningSpace(unittest.TestCase):
     def setUp(self) -> None:
@@ -211,128 +129,40 @@ class TestTuningSpace(unittest.TestCase):
             'calib': {'calib_sampling_size': [1, 10, 50]},
             'op': deepcopy(op_cap)
         }
-
-        self.optype_wise_user_config = {
-            'op_type1': {
-                'activation': {
-                    'algorithm': ['minmax'],
-                    'granularity': ['per_channel', 'per_tensor'],
-                }
-            }
-        }
-        self.model_wise_user_config = {
-            'activation': {
-                'granularity': ['per_channel'],
-            }
-        }
-
-        self.op_wise_user_config = {
+        
+        self.op_wise_user_cfg_for_fallback = {
             'op_name1': {
                 'activation': {
-                    'dtype': ['bf16']
+                    'dtype': ['fp32']
                 },
                 'weight': {
-                    'dtype': ['bf16']
+                    'dtype': ['fp32']
                 }
             },
-            'op_name2': {
-                'activation': {
-                    'dtype': ['fp32']
-                },
-                'weight': {
-                    'dtype': ['fp32']
-                }
-            }, # Fallback op_name2 to fp32 by manually, only keep fp32 capability
-            'op_name3': {
-                'activation': {
-                    'dtype': ['int8']
-                },
-                'weight': {
-                    'dtype': ['int8']
-                }
-            }, # Only keep int8 capability
-            'op_name4': {
-                'activation': {
-                    'granularity': ['per_channel'],
-                }
-            }
         }
-
-        self.op_wise_user_config2 = {
-            'op_name4': {
-                'activation': {
-                    'granularity': ['per_tensor'],
-                }
-            }
-        }
-        
-        self.capability2 = {
-            'calib': {'calib_sampling_size': [1, 10]},
-            'op': deepcopy(op_cap2)
-        }
-
-    
-
-    def test_tuning_parse_helper(self):
-        # op-wise
-        conf = {
-            'usr_cfg': {
-                'quantization':{}
-            }
-
-        }
-        conf = DotDict(conf)
-        tuning_space2 = TuningSpace(deepcopy(self.capability), deepcopy(conf))
-        logger.debug(tuning_space2.root_item.get_details())
-        
-    def test_sampler(self):
-        from neural_compressor.strategy.utils.tuning_sampler import OpTypeWiseTuningSampler
-        from neural_compressor.strategy.utils.tuning_structs import OpTuningConfig
-        tuning_space2 = TuningSpace(deepcopy(self.capability), {})
-        logger.debug(tuning_space2.root_item.get_details())
-        from neural_compressor.strategy.utils.tuning_space import get_op_mode_by_query_order
-        from neural_compressor.strategy.utils.constant import static_query_order as query_order
-        op_item_dtype_dict = get_op_mode_by_query_order(tuning_space2, query_order)
-        print(op_item_dtype_dict)
-        # Initialize the tuning cfg 
-        initial_op_tuning_cfg = {}
-        for item in tuning_space2.root_item.options:
-            if item.item_type == 'op':
-                op_name, op_type = item.name
-                opconfig = OpTuningConfig(op_name, op_type, 'fp32', tuning_space2)
-                initial_op_tuning_cfg[item.name] = opconfig
-                logger.info(opconfig)
-
-        optype_wise_tuning_sampler = OpTypeWiseTuningSampler(deepcopy(tuning_space2), [], [], 
-                                                             op_item_dtype_dict, initial_op_tuning_cfg)
 
     def test_tuning_space_merge_op_wise(self):
         # op-wise
         conf = {
             'usr_cfg': {
                 'quantization': {
-                    'op_wise': self.op_wise_user_config,
+                    'op_wise': self.op_wise_user_cfg_for_fallback,
                 }
             }
 
         }
         conf = DotDict(conf)
+        # test fallback
         tuning_space2 = TuningSpace(deepcopy(self.capability), deepcopy(conf))
         logger.debug(tuning_space2.root_item.get_details())
-        # found_quant_op_name4 = False
-        # found_fp32_op_name4 = False
-        # for quant_mode in ['static', 'dynamic']:
-        #     for item in tuning_space2.query_items_by_quant_mode(quant_mode):
-        #         if 'op_name4' in item.name:
-        #             found_quant_op_name4 = True
-        #             break
-
-        # for item in tuning_space2.query_items_by_quant_mode('fp32'):
-        #     if 'op_name4' in item.name:
-        #         found_fp32_op_name4 = True
-        #         break
-        # self.assertFalse(found_quant_op_name4)
-        # self.assertTrue(found_fp32_op_name4)
+        op_name1_only_fp32 = True
+        for quant_mode in ['static', 'dynamic']:
+            for item in tuning_space2.query_items_by_quant_mode(quant_mode):
+                if item.name[0] == 'op_name1':
+                    op_name1_only_fp32 = False
+        self.assertTrue(op_name1_only_fp32)
+        # test options merge
+        
 
 
 if __name__ == "__main__":
