fb62bacf69b13ca801b49e87cb1209c7ae4219ae
Cheng, Penghui <penghui.cheng@intel.com>
2023-02-24 08:41:52 +0800

    Fixed mxnet UT error
    Signed-off-by: Cheng, Penghui <penghui.cheng@intel.com>

--- neural_compressor/model/model.py
+++ neural_compressor/model/model.py
@@ -165,7 +165,7 @@ class Model(object):
             backend_tmp = get_model_fwk_name(root)
             if backend_tmp == "pytorch" and backend == "default":
                 backend = "pytorch_fx"
-            elif "tensorflow" in backend_tmp:
+            else:
                 backend = backend_tmp
         elif backend == "ipex":
             backend = "pytorch_ipex"
