e90e7f432511dc3ea6ab0f8b419e0fc9ba5aa2c4
Mengni Wang <mengni.wang@intel.com>
2023-02-24 14:57:49 +0800

    fix op
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- neural_compressor/adaptor/ox_utils/operators/direct_q8.py
+++ neural_compressor/adaptor/ox_utils/operators/direct_q8.py
@@ -81,28 +81,6 @@ class Direct8BitOperator(Operator):
             return
         self.quantizer.dtype_cast(self.node, self.dtype)
 
-@op_registry(op_types="Shape, Loop, Slice, Compress, Dropout, DynamicSlice, Expand, Flatten, \
-                       GatherElements, GatherND, Identity, If, IsNaN, Loop, Max, Min, Mod, \
-                       ReverseSequence, Round, Scan, Scatter, ScatterElements, ScatterND, \
-                       Shrink, Sign6, ExpandDims, Inverse, SkipLayerNormalization, \
-                       QuantizeLinear, QAttention, LongformerAttention, Gelu, FusedMatMul, \
-                       FastGelu, DequantizeLinear, BiasGelu, Where, Upsample, Tanh, Sub, Sqrt, \
-                       Softmax, Scatter, ReduceMean, Pow, LayerNormalization, GlobalMaxPool, \
-                       BatchNormalization")
-class DirectCastOperator(Operator): # pragma: no cover
-    """Direct8bit Operator Cast."""
-
-    def __init__(self, onnx_quantizer, onnx_node):
-        """Initialization."""
-        super(DirectCastOperator, self).__init__(onnx_quantizer, onnx_node)
-
-    def cast(self):
-        """Cast node."""
-        node = self.node
-        if node.input[0] not in [i.tensor_name for i in self.quantizer.new_value_info.values()]:
-            return
-        self.quantizer.dtype_cast(self.node, self.dtype)
-
 @qop_registry(op_types="Reshape, Transpose, Squeeze, Unsqueeze")
 class QDirectOperator(QOperator):
     """QDirect Operator."""
