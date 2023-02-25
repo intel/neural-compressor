cda51aa1362099399ca048fd5e5f26e2f66db5a3
Wang, Mengni <mengni.wang@intel.com>
2023-02-25 16:10:30 +0800

    add default value for recipes (#571)
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- neural_compressor/strategy/strategy.py
+++ neural_compressor/strategy/strategy.py
@@ -857,6 +857,9 @@ class TuneStrategy(object):
                  'recipes': self.cfg.quantization.recipes,
                  'performance_only': self.cfg.tuning.exit_policy.performance_only,
                  'use_bf16': self.cfg.use_bf16 if self.cfg.use_bf16 is not None else False})
+            for item in ['scale_propagation_max_pooling', 'scale_propagation_concat']:
+                if item not in framework_specific_info['recipes']:
+                    framework_specific_info['recipes'].update({item: True})
             if self.cfg.model.backend == 'itex':
                 self.cfg.model.framework = 'tensorflow_itex'
                 framework = 'tensorflow_itex'
