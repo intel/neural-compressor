ecbbcdaf5ebfcbf78eda86753d44adf7adc12a35
yiliu30 <yi4.liu@intel.com>
2023-02-28 17:00:46 +0800

    refine auto
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- neural_compressor/strategy/auto.py
+++ neural_compressor/strategy/auto.py
@@ -16,6 +16,7 @@
 # limitations under the License.
 """The auto tuning strategy."""
 import copy
+from copy import deepcopy
 import numpy as np
 from collections import OrderedDict
 from .strategy import strategy_registry, TuneStrategy, STRATEGIES
@@ -45,23 +46,67 @@ class AutoTuneStrategy(TuneStrategy):
         self.eval_func = eval_func
         self.resume = resume
         self.q_hooks = q_hooks
-        self.strategies_sequence = ['conservative', 'basic']
+        self.strategies_sequence = ['fwk','conservative', 'basic']
 
 
-    def next_tune_cfg(self):
+    def traverse(self):
         """Generate and yield the next tuning config.
 
         Returns:
             tune_config (dict): A dict containing the tuning configuration for quantization.
         """
         # Step1, be consistent with fwk
-        _, _, initial_op_tuning_cfg = self.initial_tuning_cfg()
-        yield initial_op_tuning_cfg
+        # _, _, initial_op_tuning_cfg = self.initial_tuning_cfg()
+        # calib_sampling_size_lst = self.tuning_space.root_item.get_option_by_name('calib_sampling_size').options
+        # initial_op_tuning_cfg['calib_sampling_size'] = calib_sampling_size_lst[0]
+        # yield initial_op_tuning_cfg
         # Step2. try different strategies sequentially
+        pre_strategy = None
         for strategy_name in self.strategies_sequence:
             logger.info(f"Start {strategy_name} tuning.")
             strategy = STRATEGIES[strategy_name](self.model, self.conf, self.q_dataloader, self.q_func, \
                 self.eval_dataloader, self.eval_func, self.resume, self.q_hooks)
-            for tune_cfg in strategy.next_tune_cfg():
-                yield tune_cfg
+            if pre_strategy:
+                strategy.baseline = deepcopy(pre_strategy.baseline)
+                strategy.trials_count = pre_strategy.trials_count
+                strategy.objectives.baseline = deepcopy(pre_strategy.baseline)
+            pre_strategy = strategy
+            import pdb
+            pdb.set_trace()
+            strategy.traverse()
+            self.best_qmodel = strategy.best_qmodel
+            if self.best_qmodel:
+                return 
+
+    # def next_tune_cfg(self):
+    #     """Generate and yield the next tuning config.
+
+    #     Returns:
+    #         tune_config (dict): A dict containing the tuning configuration for quantization.
+    #     """
+    #     # Step1, be consistent with fwk
+    #     _, _, initial_op_tuning_cfg = self.initial_tuning_cfg()
+    #     calib_sampling_size_lst = self.tuning_space.root_item.get_option_by_name('calib_sampling_size').options
+    #     initial_op_tuning_cfg['calib_sampling_size'] = calib_sampling_size_lst[0]
+    #     yield initial_op_tuning_cfg
+    #     # Step2. try different strategies sequentially
+    #     strategy = None
+    #     for strategy_name in self.strategies_sequence:
+    #         if strategy:
+    #             self.got_model_with_quant_ops = strategy.got_model_with_quant_ops
+    #         if self.got_model_with_quant_ops:
+    #             return 
+    #         logger.info(f"Start {strategy_name} tuning.")
+    #         strategy = STRATEGIES[strategy_name](self.model, self.conf, self.q_dataloader, self.q_func, \
+    #             self.eval_dataloader, self.eval_func, self.resume, self.q_hooks)
+    #         for tune_cfg in strategy.next_tune_cfg():
+    #             self.re_quant = strategy.re_quant
+    #             yield tune_cfg
+    #             strategy.acc_meet_flag =  self.acc_meet_flag
+    #             if self.acc_meet_flag:
+    #                 self.best_qmodel = self.last_qmodel
+    #             strategy.cur_best_tuning_cfg = deepcopy(self.cur_best_tuning_cfg)
+
+                
+
             
