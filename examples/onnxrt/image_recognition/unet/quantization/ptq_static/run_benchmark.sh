18fae118127bc27b950d7dc9349e3d4ee39fdabd
Mengni Wang <mengni.wang@intel.com>
2023-03-02 19:15:05 +0800

    fix script
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

---
+++ examples/onnxrt/image_recognition/unet/quantization/ptq_static/run_benchmark.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x
+
+function main {
+  init_params "$@"
+  run_benchmark
+
+}
+
+# init params
+function init_params {
+  for var in "$@"
+  do
+    case $var in
+      --input_model=*)
+          input_model=$(echo $var |cut -f2 -d=)
+      ;;
+      --mode=*)
+          mode=$(echo $var |cut -f2 -d=)
+      ;;
+    esac
+  done
+
+}
+
+# run_benchmark
+function run_benchmark {
+
+    python main.py \
+            --model_path ${input_model} \
+            --mode=${mode} \
+            --benchmark
+            
+}
+
+main "$@"
