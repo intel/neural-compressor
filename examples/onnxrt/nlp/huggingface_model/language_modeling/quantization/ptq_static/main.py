ad1323de80206637c0c3771dd0b1a811d151b10a
yuwenzho <yuwen.zhou@intel.com>
2023-03-02 19:51:04 +0800

    update benchmark
    Signed-off-by: yuwenzho <yuwen.zhou@intel.com>

--- examples/onnxrt/nlp/huggingface_model/language_modeling/quantization/ptq_static/main.py
+++ examples/onnxrt/nlp/huggingface_model/language_modeling/quantization/ptq_static/main.py
@@ -217,7 +217,17 @@ def main():
         return evaluate(args, model, tokenizer)
 
     if args.benchmark:
-        evaluate(args, model, tokenizer)
+        if args.mode == 'performance':
+            from neural_compressor.benchmark import fit
+            from neural_compressor.config import BenchmarkConfig
+            from neural_compressor.data.dataloaders.onnxrt_dataloader import DefaultDataLoader
+            conf = BenchmarkConfig(iteration=100,
+                                   cores_per_instance=4,
+                                   num_of_instance=1)
+            b_dataloader = DefaultDataLoader(ds, args.eval_batch_size)
+            fit(model, conf, b_dataloader=b_dataloader)
+        else:
+            evaluate(args, model, tokenizer)
         
     if args.tune:
         # GPT2 optimizer
