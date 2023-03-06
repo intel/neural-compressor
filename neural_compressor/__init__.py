dfae2d935e0fafdbd2a594840553a8a5a0630750
chensuyue <suyue.chen@intel.com>
2023-03-06 20:46:03 +0800

    Revert "for test"
    This reverts commit 740b21b435ff4ca9500bc549ea8603efdb7e3ad8.

--- neural_compressor/__init__.py
+++ neural_compressor/__init__.py
@@ -14,7 +14,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# for test
 
 """Intel® Neural Compressor: An open-source Python library supporting popular model compression techniques."""
 from .benchmark import Benchmark
