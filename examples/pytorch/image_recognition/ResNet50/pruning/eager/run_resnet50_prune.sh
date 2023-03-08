cc0e9f984adbe5e5803bca531e09583ec4daaca8
YIYANGCAI <yiyang.cai@intel.com>
2023-03-08 14:09:50 +0800

    Some slight modifications on scripts.
    Signed-off-by: YIYANGCAI <yiyang.cai@intel.com>

--- examples/pytorch/image_recognition/ResNet50/pruning/eager/run_resnet50_prune.sh
+++ examples/pytorch/image_recognition/ResNet50/pruning/eager/run_resnet50_prune.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES="6"
-DATA="/dataset/imagenet/img_raw/"
-/home/cyy/anaconda3/envs/cyy_resnet50/bin/python ./examples/pytorch/image_recognition/ResNet50/pruning/eager/train.py \
+DATA="/path/to/your/dataset/"
+python ./train.py \
     ${DATA} \
     --model "resnet50" \
     --num-classes 1000 \
@@ -17,5 +16,4 @@ DATA="/dataset/imagenet/img_raw/"
     --pruning-pattern "2x1" \
     --update-frequency-on-step 2000 \
     --distillation-loss-weight "1.0" \
-    --output ./outputs/03-08/ \
-    2>&1 | tee ./outputs/03-08/03-08.log
+    --output ./outputs/ \
