9f08912dd90bc2613640086259ce9c8275cf253c
Agata Radys <agata.radys@intel.com>
2023-03-14 14:38:00 +0100

    INC Bench diagnosis tab update (#633)
    Signed-off-by: aradys-intel <agata.radys@intel.com>
    Signed-off-by: bmyrcha <bartosz.myrcha@intel.com>

--- neural_compressor/ux/gui/src/app/histogram/histogram.component.ts
+++ neural_compressor/ux/gui/src/app/histogram/histogram.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ModelService } from '../services/model.service';
 
@@ -7,7 +7,7 @@ import { ModelService } from '../services/model.service';
   templateUrl: './histogram.component.html',
   styleUrls: ['./histogram.component.scss', './../error/error.component.scss']
 })
-export class HistogramComponent implements OnChanges {
+export class HistogramComponent implements OnInit, OnChanges {
 
   @Input() modelId;
   @Input() opName;
@@ -15,13 +15,22 @@ export class HistogramComponent implements OnChanges {
 
   data = [];
   layout;
+  showSpinner = false;
 
   constructor(
     private modelService: ModelService,
     public activatedRoute: ActivatedRoute,
   ) { }
 
+  ngOnInit() {
+    this.modelService.colorMode$.subscribe(change => {
+      const isDark = change === 'dark/';
+      this.setLayout(isDark);
+    });
+  }
+
   ngOnChanges(): void {
+    this.showSpinner = true;
     this.data = [];
     this.getHistogramData();
   }
@@ -30,28 +39,75 @@ export class HistogramComponent implements OnChanges {
     this.modelService.getHistogram(this.activatedRoute.snapshot.params.id, this.modelId, this.opName, this.type)
       .subscribe(
         response => {
+          this.data = [];
+          const colorPalette = this.generateColor(response[0].histograms.length);
           response[0].histograms.forEach((series, index) => {
             this.data.push(
               {
                 x: series.data,
-                type: 'histogram',
-                opacity: 0.6,
-                name: 'channel ' + index
+                type: 'violin',
+                orientation: 'h',
+                side: 'negative',
+                y0: 'channel ' + index,
+                name: 'channel ' + index,
+                width: 100,
+                opacity: 0.8,
+                fillcolor: colorPalette[index],
+                hoverinfo: 'none',
+                line: {
+                  width: 1,
+                  color: series.data.length === 1 ? colorPalette[index] : '#fff',
+                },
+                points: false
               }
             );
-          });
+          }
+          );
 
-          this.layout = {
-            bargap: 0.05,
-            bargroupgap: 0.2,
-            barmode: 'overlay',
-            xaxis: { title: 'Value' },
-            yaxis: { title: 'Count' }
-          };
-
-          document.getElementById('histograms').scrollIntoView({ behavior: 'smooth' });
-
+          this.setLayout(localStorage.getItem('darkMode') === 'darkMode');
+          this.showSpinner = false;
         },
         error => this.modelService.openErrorDialog(error));
   };
+
+  setLayout(isDark: boolean) {
+    this.layout = {
+      height: 550,
+      width: 800,
+      yaxis: {
+        autorange: 'reversed',
+        showgrid: true,
+      },
+      legend: {
+        tracegroupgap: 0,
+      },
+      violinmode: 'overlay',
+      opacity: 1,
+      margin: {
+        l: 150,
+        r: 50,
+        b: 100,
+        t: 50,
+        pad: 40
+      }
+    };
+
+    if (isDark) {
+      this.layout.xaxis = { color: '#fff' };
+      this.layout.yaxis.color = '#fff';
+      this.layout.legend.font = { color: '#fff' };
+      this.layout.plot_bgcolor = '#424242';
+      this.layout.paper_bgcolor = '#424242';
+    }
+  }
+
+  generateColor(num: number) {
+    const colorPalette = [];
+    const step = 100 / num;
+    for (let i = num; i > 0; --i) {
+      colorPalette.push(`rgb(${20 + (step * i)}, ${100 - (step * i * 0.1)}, ${200 - (step * i * 0.1)})`);
+    }
+    return colorPalette;
+  }
+
 }
