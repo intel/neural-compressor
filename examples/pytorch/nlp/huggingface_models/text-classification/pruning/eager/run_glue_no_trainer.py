475adda948e7c0dc94c228e0abc61520a426b601
wenhuach21 <wenhua.cheng@intel.com>
2023-03-15 22:43:16 +0800

    add copyright in example
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>

--- examples/pytorch/nlp/huggingface_models/text-classification/pruning/eager/run_glue_no_trainer.py
+++ examples/pytorch/nlp/huggingface_models/text-classification/pruning/eager/run_glue_no_trainer.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+# Copyright (c) 2023 Intel Corporation
 # Copyright 2021 The HuggingFace Inc. team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
