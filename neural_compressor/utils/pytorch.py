53f0ee22d41ffbf4e0a844b13fdc35de75510a29
Xin He <xin3.he@intel.com>
2023-03-16 09:35:56 +0800

    fix bug
    Signed-off-by: Xin He <xin3.he@intel.com>

--- neural_compressor/utils/pytorch.py
+++ neural_compressor/utils/pytorch.py
@@ -43,10 +43,22 @@ def is_int8_model(model):
     Returns:
         result(bool): Return True if the input model is a int8 model.
     """
-    stat_dict = dict(model.state_dict())
-    for k, v in stat_dict.items():
-        if hasattr(v, 'dtype') and 'int8' in str(v.dtype):
+    def _is_int8_value(value):
+        """Check whether the input tensor is a int8 tensor."""
+        if hasattr(value, 'dtype') and 'int8' in str(value.dtype):
             return True
+        else:
+            return False
+
+    stat_dict = dict(model.state_dict())
+    for name, value in stat_dict.items():
+        if _is_int8_value(value):
+            return True
+        # value maybe a tuple, such as 'linear._packed_params._packed_params'
+        if isinstance(value, tuple):
+            for v in value:
+                if _is_int8_value(v):
+                    return True
     return False
 
 def _set_sub_module_scale_zeropoint(model, tune_cfg, prefix=''):
