cbe3d7f5d1e4ad54b771957bc6394d651ff93553
XuehaoSun <xuehao.sun@intel.com>
2023-03-28 13:05:24 +0800

    fix bug
    Signed-off-by: XuehaoSun <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/models/collect_log_model.py
+++ .azure-pipelines/scripts/models/collect_log_model.py
@@ -230,7 +230,7 @@ def check_status(precision, precision_upper, check_accuracy=False):
     current_performance = performance_result.get(precision).get("Value")
     refer_performance = refer.get(f"{precision_upper}_Performance")
     print(f"current_performance_data = {current_performance:.3f}, refer_performance_data = {refer_performance:.3f}")
-    assert (refer_performance - current_performance) / refer_performance > args.gap
+    assert (refer_performance - current_performance) / refer_performance <= args.gap
 
     if check_accuracy:
         _, accuracy_result = get_model_tuning_dict_results()
