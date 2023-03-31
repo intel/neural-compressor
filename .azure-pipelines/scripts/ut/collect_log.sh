b5c65656f761d9e141be4e059a46a4bbfd44e91a
XuehaoSun <xuehao.sun@intel.com>
2023-03-31 21:50:37 +0800

    update
    Signed-off-by: XuehaoSun <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/collect_log.sh
+++ .azure-pipelines/scripts/ut/collect_log.sh
@@ -45,12 +45,12 @@ ls -l log_dir/coverage_base/htmlcov
 
 get_coverage_data() {
     # Input argument
-    local coverage_log="$1"
+    local coverage_xml="$1"
 
     # Get coverage data
-    local coverage_data=$(python3 -c "import xml.etree.ElementTree as ET; root = ET.parse('$coverage_log').getroot(); print(ET.tostring(root).decode())")
+    local coverage_data=$(python3 -c "import xml.etree.ElementTree as ET; root = ET.parse('$coverage_xml').getroot(); print(ET.tostring(root).decode())")
     if [[ -z "$coverage_data" ]]; then
-        echo "Failed to get coverage data from $coverage_log."
+        echo "Failed to get coverage data from $coverage_xml."
         exit 1
     fi
 
@@ -91,9 +91,6 @@ $BOLD_BLUE && echo "PR branches coverage: $branches_PR_covered/$branches_PR_vali
 $BOLD_BLUE && echo "BASE lines coverage: $lines_base_covered/$lines_base_valid ($coverage_base_lines%)" && $RESET
 $BOLD_BLUE && echo "BASE branches coverage: $branches_base_covered/$branches_base_valid ($coverage_base_branches%)" && $RESET
 
-coverage_PR_total=$(cat ${coverage_log} | grep TOTAL | awk '{print $NF}' | sed "s|%||g")
-coverage_base_total=$(cat ${coverage_log_base} | grep TOTAL | awk '{print $NF}' | sed "s|%||g")
-
 $BOLD_YELLOW && echo "clear upload path" && $RESET
 rm -fr log_dir/coverage_PR/.coverage*
 rm -fr log_dir/coverage_base/.coverage*
@@ -102,9 +99,6 @@ rm -fr log_dir/ut-coverage-*
 # Declare an array to hold failed items
 declare -a fail_items=()
 
-if [[ ${coverage_PR_total} -lt ${coverage_base_total} ]]; then
-    fail_items+=("total")
-fi
 if [[ ${coverage_PR_lines} -lt ${coverage_base_lines} ]]; then
     fail_items+=("lines")
 fi
@@ -119,9 +113,6 @@ if [[ ${#fail_items[@]} -ne 0 ]]; then
     )
     for item in "${fail_items[@]}"; do
         case "$item" in
-        total)
-            decrease=$(($coverage_PR_total - $coverage_base_total))
-            ;;
         lines)
             decrease=$(($coverage_PR_lines - $coverage_base_lines))
             ;;
@@ -140,8 +131,7 @@ if [[ ${#fail_items[@]} -ne 0 ]]; then
     bash -x /neural-compressor/.azure-pipelines/scripts/ut/compare_coverage.sh ${coverage_compare} ${coverage_log} ${coverage_log_base} "FAILED"
     exit 1
 else
-    $BOLD_GREEN && echo "Unit Test success with coverage ${coverage_PR_total}%" && $RESET
+    $BOLD_GREEN && echo "Unit Test success with coverage lines: ${coverage_PR_lines}%, branches: ${coverage_PR_branches}%" && $RESET
     $BOLD_GREEN && echo "compare coverage to give detail info" && $RESET
     bash -x /neural-compressor/.azure-pipelines/scripts/ut/compare_coverage.sh ${coverage_compare} ${coverage_log} ${coverage_log_base} "SUCCESS"
-    #sed "1i\Unit Test success with coverage ${coverage_PR_total}\n" ${coverage_log}
 fi
