30a24170cc58d1b4ccb99758e6cf4e0d58132633
Guo, Heng <heng.guo@intel.com>
2023-04-06 13:48:12 +0800

    add hpo

---
+++ neural_compressor/experimental/hpo/sa_optimizer.py
@@ -0,0 +1,98 @@
+import time
+import math
+import logging
+from random import random
+
+import numpy as np
+
+logger = logging.getLogger("sa_optimizer")
+
+
+class SimulatedAnnealingOptimizer(object):
+    def __init__(
+            self,
+            generate_func=None,
+            T0=100,
+            Tf=0.01,
+            higher_is_better=True,
+            alpha=None,
+            iter=500,
+            early_stop=50,
+            log_interval=50
+            ):
+        self.generate_func = generate_func
+        self.T0 = T0
+        self.Tf = Tf
+        self.T = self.T0
+        self.higher_is_better = higher_is_better
+        self.alpha = alpha
+        self.iter = iter
+        self.early_stop = early_stop
+        self.log_interval = log_interval
+        self.best = (float('-inf'), None) if self.higher_is_better else (float('inf'), None)
+        self.history = {'T': [], 'F': []}
+    
+    def _metrospolis(self, f, f_new):
+        if (not self.higher_is_better and f_new <= f) or (self.higher_is_better and f_new >= f):
+            return 1
+        else:
+            if self.higher_is_better:
+                p = math.exp((f_new - f) / self.T)
+            else:
+                p = math.exp((f - f_new) / self.T)
+            if random() < p:
+                return 1
+            else:
+                return 0
+    
+    def _generate_new_points(self, points):
+        new_points = np.array(points)
+        new_points += self.T * (np.random.random(new_points.shape) - np.random.random(new_points.shape))
+        return new_points
+
+    def gen_next_params(self, func, points):
+        count = 0
+        last_modify = 0
+        self.T = self.T0
+        self.best = (float('-inf'), None) if self.higher_is_better else (float('inf'), None)
+        scores = func(points)
+
+        self.history = {'T': [], 'F': [], 'P': []}
+        st = time.time()
+
+        while self.T > self.Tf:
+            # generate new points
+            if self.generate_func:
+                new_points = self.generate_func(points)
+            else:
+                new_points = self._generate_new_points(points)
+            new_scores = func(new_points)
+            for i, s in enumerate(new_scores):
+                if self._metrospolis(scores[i], s):
+                    points[i] = new_points[i]
+                    scores[i] = s
+                if (not self.higher_is_better and scores[i] < self.best[0]) or (self.higher_is_better and scores[i] > self.best[0]):
+                    last_modify = count
+                    self.best = (scores[i], [float(v) for v in points[i]])
+            
+            self.history['T'].append(self.T)
+            if self.higher_is_better:
+                self.history['F'].append(max(scores))
+                self.history['P'].append(points[np.argmax(scores)])
+            else:
+                self.history['F'].append(min(scores))
+                self.history['P'].append(points[np.argmax(scores)])
+
+            if self.alpha:
+                self.T *= self.alpha
+            else:
+                self.T -= (self.T0 - self.Tf) / (self.iter + 1)
+            count += 1
+
+            if self.log_interval and count % self.log_interval == 0:
+                elapse = time.time() - st
+                logger.debug(f'SA iter: {count}\tlast_update: {last_modify}\tmax score: {self.best[0]}\tpoint: {self.best[1]}\ttemp: {self.T}\telasped: {elapse}')
+
+            if count - last_modify > self.early_stop:
+                break
+        return self.best[1]
---
+++ neural_compressor/experimental/hpo/search_space.py
@@ -0,0 +1,95 @@
+import random
+
+class BaseSearchSpace(object):
+    """Base class for search space."""
+    def __init__(
+            self,
+            bound=None,
+            interval=None,
+            value=None,
+            type=None
+            ):
+        """Initialize the attributes."""
+        if bound:
+            if not isinstance(bound, (list, tuple)):
+                raise TypeError("bound sould be list or tuple, not {}".format(type(bound)))
+            if len(bound) != 2:
+                raise ValueError("bound sould only contain two elements, [start, end)")
+            if bound[1] <= bound[0]:
+                raise ValueError("empty range for [{}, {})".format(bound[0], bound[1]))
+        assert value or bound, "must set value or bound to initialize the search space"
+
+        self.bound = bound
+        self.interval = interval
+        self.value = value
+        self.type = type
+        if type == 'discrete':
+            if value:
+                self.total_num = len(value)
+            else:
+                self.total_num = int((bound[1] - bound[0]) / interval)
+        else:
+            self.total_num = float("inf")
+
+    def get_value():
+        pass
+
+
+class DiscreteSearchSpace(BaseSearchSpace):
+    def __init__(self, bound=None, interval=None, value=None):
+        if bound and interval is None:
+            if isinstance(bound[0], int) and isinstance(bound[1], int):
+                interval = 1
+            else:
+                interval = 0.01
+        super().__init__(bound=bound,
+                         interval=interval,
+                         value=value,
+                         type='discrete')
+
+    def get_random_value(self):
+        idx = random.randint(0, self.total_num - 1)
+        return self.get_nth_value(idx)
+
+    def get_nth_value(self, idx):
+        if self.bound:
+            return round(self.bound[0] + idx * self.interval, 10)
+        else:
+            return self.value[idx]
+
+    def get_all(self):
+        return [self.get_nth_value(i) for i in range(self.total_num)]
+
+    def get_value(self, idx=None):
+        if idx is not None:
+            if not isinstance(idx, int):
+                raise TypeError("The type of idx should be int, not {}".format(type(idx)))
+            if idx < 0:
+                return self.get_all()
+            value = self.get_nth_value(idx)
+        else:
+            value = self.get_random_value()
+        return value
+    
+    def index(self, value):
+        if self.value:
+            return self.value.index(value)
+        else:
+            return int((value - self.bound[0]) / self.interval)
+    
+
+class ContinuousSearchSpace(BaseSearchSpace):
+    def __init__(self, bound):
+        super().__init__(bound=bound, type='continuous')
+
+    def get_value(self):
+        if self.bound[1] > 1:
+            int_num = random.randrange(int(self.bound[0]), int(self.bound[1]) + 1)
+        else:
+            int_num = 0
+        while True:
+            value = random.random() * self.bound[1]
+            value = int_num + value
+            if value > self.bound[0] and value < self.bound[1]:
+                break
+        return value
