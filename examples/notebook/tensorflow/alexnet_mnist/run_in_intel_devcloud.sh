710da19613ac0e451381d9a7acb536d8eed543fd
Nazneen <nazneen.nighar.sultana@intel.com>
2023-04-12 12:10:54 +0530

    Update run_in_intel_devcloud.sh

--- examples/notebook/tensorflow/alexnet_mnist/run_in_intel_devcloud.sh
+++ examples/notebook/tensorflow/alexnet_mnist/run_in_intel_devcloud.sh
@@ -2,6 +2,6 @@
 
 echo "Enable Conda Env."
 source /opt/intel/oneapi/setvars.sh
-conda activate env_inc_devcloud
+conda activate env_inc
 export PYTHONPATH=$(find $CONDA_PREFIX -type d -name "site-packages" | head -n 1)
 bash run_sample.sh
