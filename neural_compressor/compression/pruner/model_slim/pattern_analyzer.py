e47a0ac9a71a8eaa2df5808014f9009d6a6ed59c
YIYANGCAI <yiyang.cai@intel.com>
2023-04-18 14:41:03 +0800

    Use lazy import in auto slim.
    Signed-off-by: YIYANGCAI <yiyang.cai@intel.com>

--- neural_compressor/compression/pruner/model_slim/pattern_analyzer.py
+++ neural_compressor/compression/pruner/model_slim/pattern_analyzer.py
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import torch
+from ..utils import torch
 import re
 from ..utils import logger
 
