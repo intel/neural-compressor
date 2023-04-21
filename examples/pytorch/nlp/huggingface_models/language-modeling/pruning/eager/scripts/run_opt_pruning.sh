ce949965b2da8fc936e15b70391c06bcea1781ed
WeiweiZhang1 <109071285+WeiweiZhang1@users.noreply.github.com>
2023-04-21 21:09:14 +0800

    refine llm example, add remove_block_mask func (#815)
    * refine llm example, add remove_block_mask feature
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    
    * fix typos
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    
    * add scripts and readme
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    
    * update pruning acc data
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>
    
    ---------
    
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>

---
+++ examples/pytorch/nlp/huggingface_models/language-modeling/pruning/eager/scripts/run_opt_pruning.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+CUDA_VISIBLE_DEVICES=4 python \
+    examples/pytorch/nlp/huggingface_models/language-modeling/pruning/eager/run_clm_no_trainer.py \
+    --model_name_or_path facebook/opt-1.3b \
+    --calibration_dataset_name NeelNanda/pile-10k \
+    --evaluation_dataset_name lambada \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 16 \
+    --max_pruning_steps 3002 \
+    --weight_decay  0 \
+    --block_size 512 \
+    --max_length 512 \
+    --do_prune \
+    --auto_slim \
+    --output_dir ./sparse_model \
+    --target_sparsity 0.1 \
+    --pruning_pattern channelx1 \
+    --pruning_frequency 500
+    
+    
