57618f259e2c4ad7fef6a826402b619d89d121ae
Lv, Liang1 <liang1.lv@intel.com>
2023-04-24 17:45:16 +0800

    update ut case with newapi
    Signed-off-by: Lv, Liang1 <liang1.lv@intel.com>

--- neural_compressor/adaptor/tf_utils/smooth_quant_scaler.py
+++ neural_compressor/adaptor/tf_utils/smooth_quant_scaler.py
@@ -105,7 +105,7 @@ class SmoothQuantScaler:
                         logger.info("Skip smoothing the node: {}".format(W_node_lst[w_i]))
                         continue
                     # clip the scales that are too small
-                    scale = tf.clip_by_value(scale, clip_value_min=1e-2, clip_value_max=1e8)
+                    scale = np.clip(scale, a_min=1e-2, a_max=1e8)
                     # skip smoothing the op where scale has elements that less than 1
                     if np.any(scale < 1):
                         logger.info("skip smooth quant: {}".format(input_node_name))
