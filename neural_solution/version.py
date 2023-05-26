0412203481cddcfee018d1d28290bce3bdfee01e
yiliu30 <yi4.liu@intel.com>
2023-05-26 10:09:38 +0800

    remove Intel®
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- neural_solution/version.py
+++ neural_solution/version.py
@@ -15,5 +15,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Intel® Neural Solution."""
+"""Neural Solution."""
 __version__ = "2.2"
