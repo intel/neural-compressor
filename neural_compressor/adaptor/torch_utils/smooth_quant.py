8cf20bc8a3ab619fb9154df33eb13acdef35714f
chen, suyue <suyue.chen@intel.com>
2023-06-14 12:09:09 +0800

    fix pylint scan issue

--- neural_compressor/adaptor/torch_utils/smooth_quant.py
+++ neural_compressor/adaptor/torch_utils/smooth_quant.py
@@ -723,7 +723,8 @@ class TorchSmoothQuant:
 
                 input_maxes = self._calibrate(self.absorb_to_layer, calib_iter, save_input_output)
 
-                # Check if input_maxes match self.absorb_to_layer (due to self._get_all_layer_names use layer tree instead of forward_path)
+                # Check if input_maxes match self.absorb_to_layer 
+                # (due to self._get_all_layer_names use layer tree instead of forward_path)
                 if not folding:
                     diff_modules = set(self.absorb_to_layer.keys()).difference(input_maxes.keys())
                     for d in diff_modules:
