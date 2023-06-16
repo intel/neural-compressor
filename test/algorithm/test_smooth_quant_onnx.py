94cd521e7f715eb7d04b8edc94569901e23d7099
Xin He <xin3.he@intel.com>
2023-06-16 13:07:20 +0800

    remove
    Signed-off-by: Xin He <xin3.he@intel.com>

--- test/algorithm/test_smooth_quant_onnx.py
+++ test/algorithm/test_smooth_quant_onnx.py
@@ -7,33 +7,6 @@ import shutil
 from neural_compressor.data import Datasets, DATALOADERS
 from neural_compressor.adaptor.ox_utils.smooth_quant import ORTSmoothQuant
 
-def build_onnx_model():
-    A = helper.make_tensor_value_info('A', TensorProto.FLOAT, [1, 5, 5])
-    C = helper.make_tensor_value_info('C', TensorProto.FLOAT, [1, 5, 2])
-    H = helper.make_tensor_value_info('H', TensorProto.FLOAT, [1, 5, 2])
-
-    g_value = np.random.uniform(low=0.001, high=0.5, size=(25)).astype(np.float32)
-    G_init = helper.make_tensor('G', TensorProto.FLOAT, [5, 5], g_value.reshape(25).tolist())
-    matmul_node = onnx.helper.make_node('MatMul', ['A', 'G'], ['C'], name='Matmul')
-
-    b_value = np.random.uniform(low=0.001, high=0.5, size=(10)).astype(np.float32)
-    B_init = helper.make_tensor('B', TensorProto.FLOAT, [5, 2], b_value.reshape(10).tolist())
-    matmul_node2 = onnx.helper.make_node('MatMul', ['C', 'B'], ['I'], name='Matmul2')
-
-    e_value = np.random.uniform(low=0.001, high=0.5, size=(10)).astype(np.float32)
-    E_init = helper.make_tensor('E', TensorProto.FLOAT, [5, 2], e_value.reshape(10).tolist())
-    matmul_node3 = onnx.helper.make_node('MatMul', ['C', 'E'], ['K'], name='Matmul3')
-
-    add = onnx.helper.make_node('Add', ['I', 'E'], ['D'], name='add')
-
-    f_value = np.random.uniform(low=0.001, high=0.5, size=(10)).astype(np.float32)
-    F_init = helper.make_tensor('F', TensorProto.FLOAT, [5, 2], f_value.reshape(10).tolist())
-    add2 = onnx.helper.make_node('Add', ['D', 'F'], ['H'], name='add2')
-
-    graph = helper.make_graph([matmul_node, matmul_node2, matmul_node3, add, add2], 'test_graph_1', [A], [H], [B_init, E_init, F_init, G_init])
-    model = helper.make_model(graph)
-    model = helper.make_model(graph, **{'opset_imports': [helper.make_opsetid('', 13)]})
-    return model
 
 def build_onnx_model():
     A = helper.make_tensor_value_info('A', TensorProto.FLOAT, [1, 5, 5])
@@ -62,6 +35,7 @@ def build_onnx_model():
     model = helper.make_model(graph)
     model = helper.make_model(graph, **{'opset_imports': [helper.make_opsetid('', 13)]})
     return model
+
 
 class TestORTSq(unittest.TestCase):
     @classmethod
