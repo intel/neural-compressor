501440ab560056e2e3a1a75c922361ebf614fc04
wenhuach21 <108330088+wenhuach21@users.noreply.github.com>
2023-06-29 13:58:27 +0800

    PT supports weight only (#1009)
    * add weight only naive code
    
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>
    
    * fix 1 bit issue
    
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>
    
    * fix bug
    
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>
    
    * align with torch
    
    * fix some issues
    
    * add weight_only approach
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * add stats dump API
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * add save API
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * add quant type
    
    Signed-off-by: yiliu30 <yi4.liu@intel.com>
    
    * add init value for weight only
    
    Signed-off-by: yiliu30 <yi4.liu@intel.com>
    
    * add debug mode for weight_config
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * fix bug
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * fix bug
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * add group_size=-1 and enhance UT
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * add doc, WIP
    
    * fix typos
    
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>
    
    * fix spelling
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * support no-list config, change bit to bits and doc
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * fix doc
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * fix doc
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * fix typo
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * fix docstring
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * fix load bug
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    * add docsting; update doc
    
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>
    
    * add readme link
    
    Signed-off-by: Xin He <xin3.he@intel.com>
    
    ---------
    
    Signed-off-by: wenhuach21 <wenhua.cheng@intel.com>
    Signed-off-by: Xin He <xin3.he@intel.com>
    Signed-off-by: yiliu30 <yi4.liu@intel.com>
    Co-authored-by: Xin He <xin3.he@intel.com>
    Co-authored-by: yiliu30 <yi4.liu@intel.com>

---
+++ test/adaptor/pytorch_adaptor/test_weight_only_adaptor.py
@@ -0,0 +1,142 @@
+import shutil
+import torch
+import unittest
+from neural_compressor import quantization, PostTrainingQuantConfig
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.fc1 = torch.nn.Linear(30, 40)
+        self.fc2 = torch.nn.Linear(40, 30)
+        self.fc3 = torch.nn.Linear(30, 10)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
+
+
+def eval_func(model):
+    # switch to evaluate mode
+    model.eval()
+    with torch.no_grad():
+        input = torch.randn(3,30)
+        # compute output
+        output = model(input)
+    return 0.0
+
+
+class TestPytorchWeightOnlyAdaptor(unittest.TestCase):
+    approach = 'weight_only'
+
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree("./saved", ignore_errors=True)
+        shutil.rmtree("runs", ignore_errors=True)
+
+    def test_RTN_func(self):
+        # TODO
+        pass
+
+    def test_RTN_quant(self):
+        input = torch.randn(3,30)
+        model = Model()
+        out1 = model(input)
+
+        conf = PostTrainingQuantConfig(
+            approach='weight_only',
+        )
+        q_model = quantization.fit(model, conf)
+        out2 = q_model(input)
+        self.assertTrue(torch.all(torch.isclose(out1, out2, atol=5e-1)))
+        self.assertFalse(torch.all(out1 == out2))
+
+        conf = PostTrainingQuantConfig(
+            approach='weight_only',
+            op_type_dict={
+                '.*':{ 	# re.match
+                    "weight": {
+                        'bits': 8, # 1-8 bits 
+                        'group_size': -1,  # -1 (per-channel)
+                        'scheme': 'sym', 
+                        'algorithm': 'RTN', 
+                    },
+                },
+            },
+            recipes={
+                'gptq_args':{'percdamp': 0.01},
+                'awq_args':{'alpha': 'auto', 'clip': True},
+            },
+        )
+        q_model = quantization.fit(model, conf, eval_func=eval_func)
+        out2 = q_model(input)
+        self.assertTrue(torch.all(torch.isclose(out1, out2, atol=5e-1)))
+        self.assertFalse(torch.all(out1 == out2))
+
+        conf = PostTrainingQuantConfig(
+            approach='weight_only',
+            op_type_dict={
+                '.*':{ 	# re.match
+                    "weight": {
+                        'bits': 4, # 1-8 bits 
+                        'group_size': 32,  # 1 - 1024 or higher
+                        'scheme': 'asym', 
+                        'algorithm': 'RTN', 
+                    },
+                },
+            },
+            recipes={
+                'gptq_args':{'percdamp': 0.01},
+                'awq_args':{'alpha': 'auto', 'clip': True},
+            },
+        )
+        q_model = quantization.fit(model, conf, eval_func=eval_func)
+        out2 = q_model(input)
+        self.assertTrue(torch.all(torch.isclose(out1, out2, atol=5e-1)))
+        self.assertFalse(torch.all(out1 == out2))
+
+        conf = PostTrainingQuantConfig(
+            approach='weight_only',
+            op_name_dict={
+                'fc1':{ 	# re.match
+                    "weight": {
+                        'bits': 4, # 1-8 bits 
+                        'group_size': 32,  # 1 - 1024 or higher
+                        'scheme': 'sym', 
+                        'algorithm': 'RTN', 
+                    },
+                },
+                'fc2':{ 	# re.match
+                    "weight": {
+                        'bits': 3, # 1-8 bits 
+                        'group_size': 16,  # 1 - 1024 or higher
+                        'scheme': 'asym', 
+                        'algorithm': 'RTN', 
+                    },
+                },
+                'fc3':{ 	# re.match
+                    "weight": {
+                        'dtype': 'fp32',
+                    },
+                },
+            },
+            recipes={
+                'gptq_args':{'percdamp': 0.01},
+                'awq_args':{'alpha': 'auto', 'clip': True},
+            },
+        )
+        q_model = quantization.fit(model, conf, eval_func=eval_func)
+        out2 = q_model(input)
+        self.assertTrue(torch.all(torch.isclose(out1, out2, atol=5e-1)))
+        self.assertFalse(torch.all(out1 == out2))
+        q_model.save('saved')
+        from neural_compressor.utils.pytorch import load
+        new_model = load('saved', model)
+        out1 = new_model(input)
+        self.assertTrue(torch.all(out1 == out2))
+
+
+if __name__ == "__main__":
+    unittest.main()
