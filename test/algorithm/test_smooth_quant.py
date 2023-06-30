a14e8eb964fc706dadf76a5db9ab1a79fe0d390e
yiliu30 <yi4.liu@intel.com>
2023-06-30 10:26:19 +0800

    add sampler for sq alpha
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- test/algorithm/test_smooth_quant.py
+++ test/algorithm/test_smooth_quant.py
@@ -701,5 +701,64 @@ class TestSqLinearOpFuse(unittest.TestCase):
         output2 = q_model.model(input_ids)
 
 
+class DemoModel(torch.nn.Module):
+    def __init__(self):
+        super(DemoModel, self).__init__()
+        self.fc1 = torch.nn.Linear(3, 4)
+        self.fc2 = torch.nn.Linear(4, 3)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.fc2(out)
+        return out
+    
+class DemoCalibDataloader:
+    def __init__(self):
+        self.batch_size = 1
+    def __iter__(self):
+        yield torch.randn([2, 3])
+        
+class TestTuneSqAlpha(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.ns_workspace = "./nc_workspace"
+    
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree(self.ns_workspace, ignore_errors=True)
+        
+    def test_sq_tune_alpha(self):
+        from neural_compressor import quantization
+        from neural_compressor.config import PostTrainingQuantConfig, TuningCriterion
+        tuning_criterion = TuningCriterion(max_trials=5)
+
+        fp32_model = DemoModel()
+        conf = PostTrainingQuantConfig(
+            quant_level=1,
+            tuning_criterion=tuning_criterion,
+            calibration_sampling_size=8,
+            recipes={"smooth_quant": True, 
+                     "smooth_quant_args": {'folding': False,
+                                           "alpha_list": np.arange(0.1, 0.2, 0.05).tolist(),
+                                           }
+                     }
+        )
+        
+        eval_result_lst = [1, 0.9, 0.8, 0.7, 1.1]
+        def fake_eval(model):
+            acc = eval_result_lst.pop(0)
+            return acc
+            
+        q_model = quantization.fit(
+            fp32_model,
+            conf,
+            calib_dataloader=DemoCalibDataloader(),
+            eval_func=fake_eval,
+        )
+        
+        q_model.save(self.ns_workspace + "saved_result")
+
+
+
 if __name__ == '__main__':
     unittest.main()
