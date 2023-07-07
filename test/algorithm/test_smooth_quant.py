73db20eaf2c9e674ffa9d7c17d3e10bd6945c4ef
yiliu30 <yi4.liu@intel.com>
2023-07-07 14:22:41 +0800

    add ut
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- test/algorithm/test_smooth_quant.py
+++ test/algorithm/test_smooth_quant.py
@@ -835,24 +835,39 @@ class TestTuneSqAlpha(unittest.TestCase):
             quant_level=1,
             tuning_criterion=tuning_criterion,
             calibration_sampling_size=8,
-            recipes={"smooth_quant": True, 
+            recipes={"smooth_quant": True,
                      "smooth_quant_args": {'folding': False,
                                            "alpha": np.arange(0.1, 0.4, 0.05).tolist()}
                      }
         )
-        
         eval_result_lst = [1, 0.9, 0.8, 0.7, 1.1]
         def fake_eval(model):
             acc = eval_result_lst.pop(0)
             return acc
             
-        q_model = quantization.fit(
-            fp32_model,
-            conf,
-            calib_dataloader=DemoCalibDataloader(),
-            eval_func=fake_eval,
+        q_model = quantization.fit(fp32_model,conf,calib_dataloader=DemoCalibDataloader(),eval_func=fake_eval)
+        q_model.save(self.ns_workspace + "saved_result")
+
+    def test_sq_tune_alpha2(self):
+        from neural_compressor import quantization
+        from neural_compressor.config import PostTrainingQuantConfig, TuningCriterion
+        tuning_criterion = TuningCriterion(max_trials=5)
+
+        fp32_model = DemoModel()
+        conf = PostTrainingQuantConfig(
+            quant_level=1,
+            tuning_criterion=tuning_criterion,
+            calibration_sampling_size=8,
+            recipes={"smooth_quant": True,
+                     "smooth_quant_args": {"alpha": np.arange(0.1, 0.4, 0.05).tolist()}
+                     }
         )
-        
+        eval_result_lst = [1, 0.9, 0.8, 0.7, 1.1]
+        def fake_eval(model):
+            acc = eval_result_lst.pop(0)
+            return acc
+
+        q_model = quantization.fit(fp32_model,conf,calib_dataloader=DemoCalibDataloader(),eval_func=fake_eval)
         q_model.save(self.ns_workspace + "saved_result")
 
 
