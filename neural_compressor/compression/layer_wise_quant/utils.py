4680fa2cba4b89e27eef295524f35e706e924f5c
Guo, Heng <heng.guo@intel.com>
2023-07-13 13:25:52 +0800

    fix load
    Signed-off-by: Guo, Heng <heng.guo@intel.com>

--- neural_compressor/compression/layer_wise_quant/utils.py
+++ neural_compressor/compression/layer_wise_quant/utils.py
@@ -80,8 +80,11 @@ def load_from_shard(path, tensor_name):
 
 def load_tensor_from_shard(path, tensor_name, prefix=None):
     idx_dict = json.load(open(os.path.join(path, 'pytorch_model.bin.index.json'), 'r'))['weight_map']
-    assert tensor_name in idx_dict.keys() or tensor_name.replace(f'{prefix}.', '') in idx_dict.keys(), \
-        '{} not in the index.json'.format(tensor_name)
+    if tensor_name not in idx_dict.keys():
+        if tensor_name.replace(f'{prefix}.', '') in idx_dict.keys():
+            tensor_name = tensor_name.replace(f'{prefix}.', '')
+        else:
+            assert False, '{} not in the index.json'.format(tensor_name)
     return load_tensor(os.path.join(path, idx_dict[tensor_name]), tensor_name, prefix)
 
 
