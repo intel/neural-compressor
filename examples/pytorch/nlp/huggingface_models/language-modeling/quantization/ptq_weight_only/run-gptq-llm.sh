2306984d3ac3fc4905194882b4b1a2a9793d17a3
YIYANGCAI <yiyang.cai@intel.com>
2023-08-17 10:48:39 +0800

    fix script missed items
    Signed-off-by: YIYANGCAI <yiyang.cai@intel.com>

--- examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_weight_only/run-gptq-llm.sh
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_weight_only/run-gptq-llm.sh
@@ -1,6 +1,6 @@
 python examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_weight_only/run-gptq-llm.py \
     --model_name_or_path facebook/opt-125m \
-    --weight_only_algo GPTQ
+    --weight_only_algo GPTQ \
     --dataset NeelNanda/pile-10k \
     --wbits 4 \
     --group_size 128 \
