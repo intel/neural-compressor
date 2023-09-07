3a36060e0589c1f0d66d946444a573d30a59e90c
n1ck-guo <110074967+n1ck-guo@users.noreply.github.com>
2023-09-07 10:38:44 +0800

    fix pruning kthvalue error (#1224)
    * fix pruning kthvalue error
    
    Signed-off-by: Guo, Heng <heng.guo@intel.com>

--- neural_compressor/compression/pruner/patterns/base.py
+++ neural_compressor/compression/pruner/patterns/base.py
@@ -536,12 +536,12 @@ class PytorchBasePattern(BasePattern):
         """
         flattern_score = torch.flatten(score)
         k = int(exact_sparsity_ratio * flattern_score.numel())
-        threshold, _ = torch.kthvalue(flattern_score, k)
         if not k < 1:
+            threshold, _ = torch.kthvalue(flattern_score, k)
             zero = torch.tensor([False]).to(score.device)
             one = torch.tensor([True]).to(score.device)
             mask = torch.where(score <= threshold, zero, one)
-        else:
+        else:  # pragma: no cover
             mask = torch.ones(score.shape, device=score.device)
         if self.block:
             mask = mask.float()
