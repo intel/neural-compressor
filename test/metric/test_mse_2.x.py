f6eb81975b3b827d0057c8e6807d20d787b85d4b
yiliu30 <yi4.liu@intel.com>
2023-09-11 10:29:32 +0800

    add metric-related UT for 2.0
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

---
+++ test/metric/test_mse_2.x.py
@@ -0,0 +1,166 @@
+"""Tests for mse metric/strategy."""
+import os
+import shutil
+import unittest
+
+import numpy as np
+
+
+def build_fake_model():
+    import tensorflow as tf
+
+    try:
+        graph = tf.Graph()
+        graph_def = tf.GraphDef()
+        with tf.Session() as sess:
+            x = tf.placeholder(tf.float64, shape=(1, 3, 3, 1), name="x")
+            y = tf.constant(np.random.random((2, 2, 1, 1)), name="y")
+            op = tf.nn.conv2d(input=x, filter=y, strides=[1, 1, 1, 1], padding="VALID", name="op_to_store")
+
+            sess.run(tf.global_variables_initializer())
+            constant_graph = tf.graph_util.convert_variables_to_constants(sess, sess.graph_def, ["op_to_store"])
+
+        graph_def.ParseFromString(constant_graph.SerializeToString())
+        with graph.as_default():
+            tf.import_graph_def(graph_def, name="")
+    except:
+        graph = tf.Graph()
+        graph_def = tf.compat.v1.GraphDef()
+        with tf.compat.v1.Session() as sess:
+            x = tf.compat.v1.placeholder(tf.float64, shape=(1, 3, 3, 1), name="x")
+            y = tf.compat.v1.constant(np.random.random((2, 2, 1, 1)), name="y")
+            op = tf.nn.conv2d(input=x, filters=y, strides=[1, 1, 1, 1], padding="VALID", name="op_to_store")
+
+            sess.run(tf.compat.v1.global_variables_initializer())
+            constant_graph = tf.compat.v1.graph_util.convert_variables_to_constants(
+                sess, sess.graph_def, ["op_to_store"]
+            )
+
+        graph_def.ParseFromString(constant_graph.SerializeToString())
+        with graph.as_default():
+            tf.import_graph_def(graph_def, name="")
+    return graph
+
+
+def build_ox_model():
+    import torch
+    import torchvision
+
+    path = "mb_v2.onnx"
+    model = torchvision.models.mobilenet_v2()
+
+    x = torch.randn(100, 3, 224, 224, requires_grad=True)
+    torch_out = model(x)
+
+    torch.onnx.export(
+        model,
+        x,
+        path,
+        export_params=True,
+        opset_version=12,
+        do_constant_folding=True,
+        input_names=["input"],
+        output_names=["output"],
+        dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
+    )
+
+
+class dataset:
+    def __init__(self):
+        self.data = []
+        self.label = []
+        for i in range(10):
+            self.data.append(np.zeros((3, 224, 224)).astype(np.float32))
+            self.label.append(0)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index], self.label[index]
+
+
+class TestMetric(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.constant_graph = build_fake_model()
+        build_ox_model()
+
+    @classmethod
+    def tearDownClass(self):
+        os.remove("mb_v2.onnx")
+        os.remove("fake_yaml4.yaml")
+        os.remove("fake_yaml3.yaml")
+
+        shutil.rmtree("saved", ignore_errors=True)
+
+    def _test_tf_model_helper(self, config, eval_func):
+        from neural_compressor.data import DATALOADERS, Datasets
+        from neural_compressor.quantization import fit
+
+        # dataset and dataloader
+        dataset = Datasets("tensorflow")["dummy"](((100, 3, 3, 1)))
+        dataloader = DATALOADERS["tensorflow"](dataset)
+
+        q_model = fit(model=self.constant_graph, conf=config, calib_dataloader=dataloader, eval_func=eval_func)
+        return q_model
+
+    def test_run_mse_one_trial(self):
+        from neural_compressor.config import PostTrainingQuantConfig, TuningCriterion
+
+        tuning_criterion = TuningCriterion(strategy="mse")
+        config = PostTrainingQuantConfig(tuning_criterion=tuning_criterion)
+        q_model = self._test_tf_model_helper(config, lambda mode: 1)
+        self.assertIsNotNone(q_model)
+
+    def test_run_mse_max_trials(self):
+        from neural_compressor.config import AccuracyCriterion, PostTrainingQuantConfig, TuningCriterion
+
+        tuning_criterion = TuningCriterion(strategy="mse", max_trials=5)
+        accuracy_criterion = AccuracyCriterion(tolerable_loss=-0.01)
+        config = PostTrainingQuantConfig(tuning_criterion=tuning_criterion, accuracy_criterion=accuracy_criterion)
+        q_model = self._test_tf_model_helper(config, lambda mode: 1)
+        self.assertIsNone(q_model)
+
+    def test_run_rmse_metric(self):
+        from neural_compressor.config import AccuracyCriterion, PostTrainingQuantConfig, TuningCriterion
+        from neural_compressor.data import DATALOADERS, Datasets
+        from neural_compressor.metric import METRICS
+        from neural_compressor.quantization import fit
+
+        # dataset and dataloader
+        dataset = Datasets("tensorflow")["dummy"](((100, 3, 3, 1)))
+        dataloader = DATALOADERS["tensorflow"](dataset)
+        metrics = METRICS("tensorflow")
+        rmse = metrics["RMSE"]()
+
+        tuning_criterion = TuningCriterion(strategy="mse", max_trials=5)
+        accuracy_criterion = AccuracyCriterion(tolerable_loss=-1)
+        config = PostTrainingQuantConfig(tuning_criterion=tuning_criterion, accuracy_criterion=accuracy_criterion)
+
+        q_model = fit(model=self.constant_graph, conf=config, calib_dataloader=dataloader, eval_metric=rmse)
+        self.assertIsNone(q_model)
+
+    def _test_ort_model_helper(self, config, eval_func):
+        from neural_compressor.data import DATALOADERS
+        from neural_compressor.quantization import fit
+
+        # dataset and dataloader
+        ds = dataset()
+        dataloader = DATALOADERS["onnxrt_qlinearops"](ds)
+
+        q_model = fit(model="mb_v2.onnx", conf=config, calib_dataloader=dataloader, eval_func=eval_func)
+        return q_model
+
+    def test_ox_mse(self):
+        from neural_compressor.config import AccuracyCriterion, PostTrainingQuantConfig, TuningCriterion
+
+        tuning_criterion = TuningCriterion(strategy="mse", max_trials=5)
+        accuracy_criterion = AccuracyCriterion(tolerable_loss=-0.01)
+        config = PostTrainingQuantConfig(tuning_criterion=tuning_criterion, accuracy_criterion=accuracy_criterion)
+        q_model = self._test_ort_model_helper(config, lambda mode: 1)
+        self.assertIsNone(q_model)
+
+
+if __name__ == "__main__":
+    unittest.main()
