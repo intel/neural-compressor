ea40d58719d4a85605d4bc8813f71fe3ce553c59
chensuyue <suyue.chen@intel.com>
2023-09-21 17:40:03 +0800

    downgrade tensorflow version due to std::runtime_error
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/ut/ut_fwk_version.sh
+++ .azure-pipelines/scripts/ut/ut_fwk_version.sh
@@ -4,7 +4,7 @@ echo "export UT fwk version..."
 test_mode=$1
 
 if [ "$test_mode" == "coverage" ]; then
-    export tensorflow_version='2.13.0'
+    export tensorflow_version='2.12.0'
     export pytorch_version='2.0.1+cpu'
     export torchvision_version='0.15.2+cpu'
     export ipex_version='2.0.1+cpu'
@@ -12,7 +12,7 @@ if [ "$test_mode" == "coverage" ]; then
     export onnxruntime_version='1.15.1'
     export mxnet_version='1.9.1'
 else
-    export tensorflow_version='2.12.0'
+    export tensorflow_version='2.11.0'
     export pytorch_version='1.13.0+cpu'
     export torchvision_version='0.14.0+cpu'
     export ipex_version='1.13.0+cpu'
