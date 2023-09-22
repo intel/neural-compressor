121da9a1e2851a5f24deae90cfeb3c1fa24ae25f
chensuyue <suyue.chen@intel.com>
2023-09-22 16:02:59 +0800

    update fw version
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/ut/ut_fwk_version.sh
+++ .azure-pipelines/scripts/ut/ut_fwk_version.sh
@@ -4,8 +4,8 @@ echo "export UT fwk version..."
 test_mode=$1
 
 if [ "$test_mode" == "coverage" ]; then
-    export tensorflow_version='2.12.0'
-    export pytorch_version='2.0.1+cpu'
+    export tensorflow_version='2.13.0'
+    export pytorch_version='2.0.0+cpu'
     export torchvision_version='0.15.2+cpu'
     export ipex_version='2.0.1+cpu'
     export onnx_version='1.14.1'
