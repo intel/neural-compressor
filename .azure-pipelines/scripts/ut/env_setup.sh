d1375295983dbc4f64abee7dca86b8ea4fa1e705
chensuyue <suyue.chen@intel.com>
2023-09-28 15:44:35 +0800

    update tf/pt binary
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/ut/env_setup.sh
+++ .azure-pipelines/scripts/ut/env_setup.sh
@@ -24,7 +24,7 @@ echo "========= test case is ${test_case}"
 if [[ "${tensorflow_version}" == *"-official" ]]; then
     pip install tensorflow==${tensorflow_version%-official}
 elif [[ "${tensorflow_version}" == "spr-base" ]]; then
-    pip install /tf_dataset/tf_binary/221212/tensorflow*.whl
+    pip install /tf_dataset/tf_binary/230928/tensorflow*.whl
     pip install protobuf==3.20.3
     pip install horovod==0.27.0
     if [[ $? -ne 0 ]]; then
@@ -48,13 +48,13 @@ if [[ "${torchvision_version}" != "" ]]; then
 fi
 
 if [[ "${ipex_version}" == "1.13.0+cpu" ]]; then
-    ipex_whl="https://github.com/intel/intel-extension-for-pytorch/releases/download/v1.13.0%2Bcpu/intel_extension_for_pytorch-1.13.0-cp38-cp38-manylinux2014_x86_64.whl"
+    ipex_whl="https://github.com/intel/intel-extension-for-pytorch/releases/download/v1.13.0%2Bcpu/intel_extension_for_pytorch-1.13.0-cp310-cp310-manylinux2014_x86_64.whl"
     pip install $ipex_whl
 elif [[ "${ipex_version}" == "2.0.0+cpu" ]]; then
-    ipex_whl="https://intel-extension-for-pytorch.s3.amazonaws.com/ipex_stable/cpu/intel_extension_for_pytorch-2.0.0%2Bcpu-cp38-cp38-linux_x86_64.whl"
+    ipex_whl="https://intel-extension-for-pytorch.s3.amazonaws.com/ipex_stable/cpu/intel_extension_for_pytorch-2.0.0%2Bcpu-cp310-cp310-linux_x86_64.whl"
     pip install $ipex_whl
 elif [[ "${ipex_version}" == "2.0.1+cpu" ]]; then
-    ipex_whl="https://intel-extension-for-pytorch.s3.amazonaws.com/ipex_stable/cpu/intel_extension_for_pytorch-2.0.100%2Bcpu-cp38-cp38-linux_x86_64.whl"
+    ipex_whl="https://intel-extension-for-pytorch.s3.amazonaws.com/ipex_stable/cpu/intel_extension_for_pytorch-2.0.100%2Bcpu-cp310-cp310-linux_x86_64.whl"
     pip install $ipex_whl
 elif [[ "${ipex_version}" == "2.1.0" ]]; then
     pip install /tf_dataset/pt_binary/ww32/torch-*.whl
@@ -86,7 +86,8 @@ fi
 
 # install special test env requirements
 # common deps
-pip install --no-cache-dir horovod
+pip install cmake
+pip install horovod
 pip install transformers
 
 if [[ $(echo "${test_case}" | grep -c "others") != 0 ]];then
