9abfcc4ec8b55f4db85e939fe8cc2766d478c060
chensuyue <suyue.chen@intel.com>
2023-10-07 10:51:29 +0800

    update for core dump issue
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/ut/ut_fwk_version.sh
+++ .azure-pipelines/scripts/ut/ut_fwk_version.sh
@@ -11,12 +11,6 @@ if [ "$test_mode" == "coverage" ]; then
     export onnx_version='1.14.1'
     export onnxruntime_version='1.15.1'
     export mxnet_version='1.9.1'
-
-    # import torch before import tensorflow
-    cd /neural-compressor/test || exit 1
-    find . -name "test*.py" | xargs sed -i 's/import tensorflow as tf/import torch; import tensorflow as tf/g'
-    find . -name "test*.py" | xargs sed -i 's/import tensorflow.compat.v1 as tf/import torch; import tensorflow.compat.v1 as tf/g'
-    find . -name "test*.py" | xargs sed -i 's/from tensorflow import keras/import torch; from tensorflow import keras/g'
 else
     export tensorflow_version='2.12.0'
     export pytorch_version='1.13.0+cpu'
@@ -27,6 +21,11 @@ else
     export mxnet_version='1.9.1'
 fi
 
+# import torch before import tensorflow
+cd /neural-compressor/test || exit 1
+find . -name "test*.py" | xargs sed -i 's/import tensorflow as tf/import torch; import tensorflow as tf/g'
+find . -name "test*.py" | xargs sed -i 's/import tensorflow.compat.v1 as tf/import torch; import tensorflow.compat.v1 as tf/g'
+find . -name "test*.py" | xargs sed -i 's/from tensorflow import keras/import torch; from tensorflow import keras/g'
 
 
 
