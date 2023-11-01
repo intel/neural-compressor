779cffeb5c7440960356e0c0d06cc756558d2f20
Xin He <xin3.he@intel.com>
2023-11-01 17:58:03 +0800

    fix bug
    Signed-off-by: Xin He <xin3.he@intel.com>

--- neural_compressor/experimental/component.py
+++ neural_compressor/experimental/component.py
@@ -123,6 +123,7 @@ class Component(object):
             if self.adaptor is None:
                 framework_specific_info = {
                     "device": self.cfg.device,
+                    "backend": self.cfg.backend,
                     "approach": "post_training_static_quant",
                     "random_seed": self.cfg.tuning.random_seed,
                     "workspace_path": self.cfg.tuning.workspace.path,
@@ -143,6 +144,7 @@ class Component(object):
         if self.adaptor is None:
             framework_specific_info = {
                 "device": self.cfg.device,
+                "backend": self.cfg.backend,
                 "approach": "quant_aware_training",
                 "random_seed": self.cfg.tuning.random_seed,
                 "workspace_path": self.cfg.tuning.workspace.path,
@@ -171,6 +173,7 @@ class Component(object):
             # create adaptor
             framework_specific_info = {
                 "device": self.cfg.device,
+                "backend": self.cfg.backend,
                 "random_seed": self.cfg.tuning.random_seed,
                 "workspace_path": self.cfg.tuning.workspace.path,
                 "q_dataloader": None,
