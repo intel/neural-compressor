91d86720fa3047551555b63d7dd7acac2c83dd0f
Zhang, Weiwei1 <weiwei1.zhang@intel.com>
2023-11-07 16:02:04 +0800

    enable multiple datasets
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>

--- examples/pytorch/nlp/huggingface_models/language-modeling/quantization/signround/eval.py
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/signround/eval.py
@@ -3,23 +3,32 @@ import torch
 import torch.nn as nn
 
 
-def eval_model(model, model_name, tokenizer, tasks=["lambada_openai", "hellaswag", "winogrande", "piqa"], eval_bs=32):
+def eval_model(model, model_name, tokenizer, tasks=["lambada_openai", "hellaswag", "winogrande", "piqa"], eval_bs=32, use_accelerate=False, device=None):
+    if device is None: 
+        device = str(model.device)
+    if str(device) == "cpu"  or (hasattr(model, 'config') and model.config.torch_dtype is torch.bfloat16):
+        model = model.to(torch.bfloat16)
+        dtype = 'bfloat16'
+    else:
+        dtype = 'float16'
+        model = model.half()
+        
     try:
         from intel_extension_for_transformers.llm.evaluation.lm_eval import evaluate as lm_evaluate
         print("evaluation with itrex lm-eval", flush=True)
-
-        if str(model.device) == "cpu":
-            model = model.to(torch.bfloat16)
-            dtype = 'bfloat16'
+            
+        if use_accelerate:
+            user_model = None
+            model_args = f'pretrained={model_name},tokenizer="{model_name}",dtype={dtype},use_accelerate=True'
         else:
-            model = model.half()
-            dtype = 'float16'
+            user_model = model
+            model_args = f'pretrained="{model_name}",tokenizer="{model_name}",dtype={dtype}'
         model.eval()
         results = lm_evaluate(model="hf-causal",
-                              model_args=f'pretrained="{model_name}",tokenizer="{model_name}",dtype={dtype}',
-                              user_model=model,
+                              model_args=model_args,
+                              user_model=user_model,
                               tasks=tasks,
-                              device=str(model.device),
+                              device=str(device),
                               batch_size=eval_bs)
 
     except:
@@ -35,14 +44,16 @@ def eval_model(model, model_name, tokenizer, tasks=["lambada_openai", "hellaswag
         if output_dir is not None:
             model.save_pretrained(output_dir)
             tokenizer.save_pretrained(output_dir)
-        if str(model.device) == "cpu":
-            dtype = 'bfloat16'
+            
+        if use_accelerate:
+            model_args = f'pretrained={output_dir},tokenizer="{output_dir}",dtype={dtype},use_accelerate=True'
         else:
-            dtype = 'float16'
-        results = simple_evaluate(model="hf-causal",
-                                  model_args=f'pretrained="{output_dir}",tokenizer="{output_dir}",dtype={dtype}',
+            model_args = f'pretrained="{output_dir}",tokenizer="{output_dir}",dtype={dtype}'
+            
+        results = simple_evaluate(model="hf-causal", 
+                                  model_args=model_args,
                                   tasks=tasks,
-                                  device=str(model.device),
+                                  device=str(device),
                                   batch_size=eval_bs,
                                   no_cache=True)
         dumped = json.dumps(results, indent=2)
@@ -50,45 +61,3 @@ def eval_model(model, model_name, tokenizer, tasks=["lambada_openai", "hellaswag
 
         if os.path.exists(output_dir):
             shutil.rmtree(output_dir)
-
-    @torch.no_grad()
-    def eval_same_with_gptq(model, testenc, dev):
-        print('Evaluating ...', flush=True)
-        # model.eval()
-        model.to(dev)
-
-        testenc = testenc.input_ids
-        nsamples = testenc.numel() // model.seqlen
-
-        use_cache = model.config.use_cache
-        model.config.use_cache = False
-
-        testenc = testenc.to(dev)
-        nlls = []
-        for i in range(nsamples):
-            batch = testenc[:, (i * model.seqlen):((i + 1) * model.seqlen)].to(dev)
-            lm_logits = model(batch).logits
-            shift_logits = lm_logits[:, :-1, :].contiguous()
-            shift_labels = testenc[
-                           :, (i * model.seqlen):((i + 1) * model.seqlen)
-                           ][:, 1:]
-            loss_fct = nn.CrossEntropyLoss()
-            loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
-            neg_log_likelihood = loss.float() * model.seqlen
-            nlls.append(neg_log_likelihood)
-        ppl = torch.exp(torch.stack(nlls).sum() / (nsamples * model.seqlen))
-        print(ppl.item())
-
-        model.config.use_cache = use_cache
-        return ppl.item()
-
-    datasets = ['wikitext2', 'ptb-new', 'c4-new']
-
-    from gptq_data_loader import get_loaders
-    for dataset in datasets:
-        dataloader, testloader = get_loaders(
-            dataset, seed=0, model=model_name, seqlen=model.seqlen
-        )
-        print(dataset, flush=True)
-        ppl = eval_same_with_gptq(model, testloader, str(model.device))
-        results.update({dataset: ppl})
