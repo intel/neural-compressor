27fbe7ca06546fab1673754d85b3463897ce6f48
Wang, Mengni <mengni.wang@intel.com>
2023-11-10 11:00:36 +0800

    Update onnxrt.py

--- neural_compressor/adaptor/onnxrt.py
+++ neural_compressor/adaptor/onnxrt.py
@@ -1659,7 +1659,7 @@ class ONNXRT_WeightOnlyAdaptor(ONNXRUNTIMEAdaptor):
             actorder = self.recipes.get("gptq_args", {}).get("actorder", False)
             mse = self.recipes.get("gptq_args", {}).get("mse", False)
             perchannel = self.recipes.get("gptq_args", {}).get("perchannel", True)
-            compute_type = self.recipes.get("gptq_args", {}).get("compute_type", -1)
+            accuracy_level = self.recipes.get("gptq_args", {}).get("accuracy_level", 0)
             calib_sampling_size = tune_cfg.get("calib_sampling_size", 1)
             tmp_model = gptq_quantize(
                 tmp_model,
@@ -1671,7 +1671,7 @@ class ONNXRT_WeightOnlyAdaptor(ONNXRUNTIMEAdaptor):
                 actorder=actorder,
                 mse=mse,
                 perchannel=perchannel,
-                compute_type=compute_type,
+                accuracy_level=accuracy_level,
             )
         if "AWQ" in algos:
             from neural_compressor.adaptor.ox_utils.weight_only import awq_quantize
@@ -1679,7 +1679,7 @@ class ONNXRT_WeightOnlyAdaptor(ONNXRUNTIMEAdaptor):
             assert data_loader is not None, "AWQ WOQ algorithm needs to pass 'calib_dataloader' to quantization.fit()"
             enable_auto_scale = self.recipes.get("awq_args", {}).get("enable_auto_scale", True)
             enable_mse_search = self.recipes.get("awq_args", {}).get("enable_mse_search", True)
-            compute_type = self.recipes.get("awq_args", {}).get("compute_type", -1)
+            accuracy_level = self.recipes.get("awq_args", {}).get("accuracy_level", 0)
             calib_sampling_size = tune_cfg.get("calib_sampling_size", 1)
             tmp_model = awq_quantize(
                 tmp_model,
@@ -1688,16 +1688,16 @@ class ONNXRT_WeightOnlyAdaptor(ONNXRUNTIMEAdaptor):
                 n_samples=calib_sampling_size,
                 enable_auto_scale=enable_auto_scale,
                 enable_mse_search=enable_mse_search,
-                compute_type=compute_type,
+                accuracy_level=accuracy_level,
             )
         elif "RTN" in algos:
             from neural_compressor.adaptor.ox_utils.weight_only import rtn_quantize
 
-            compute_type = self.recipes.get("rtn_args", {}).get("compute_type", -1)
+            accuracy_level = self.recipes.get("rtn_args", {}).get("accuracy_level", 0)
             tmp_model = rtn_quantize(
                 tmp_model,
                 quant_config,
-                compute_type=compute_type,
+                accuracy_level=accuracy_level,
             )
         tmp_model.q_config = copy.deepcopy(quant_config)
         self._dump_model_op_stats(tmp_model, tune_cfg)
