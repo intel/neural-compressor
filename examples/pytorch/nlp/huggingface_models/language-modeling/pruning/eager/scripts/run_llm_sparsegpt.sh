dea102bd6992bcc71f3556de4976f81bcb6a3d8b
Zhang, Weiwei1 <weiwei1.zhang@intel.com>
2023-11-12 17:22:10 +0800

    Refinement of sparseGPT results.
    Signed-off-by: Zhang, Weiwei1 <weiwei1.zhang@intel.com>

--- examples/pytorch/nlp/huggingface_models/language-modeling/pruning/eager/scripts/run_llm_sparsegpt.sh
+++ examples/pytorch/nlp/huggingface_models/language-modeling/pruning/eager/scripts/run_llm_sparsegpt.sh
@@ -13,14 +13,11 @@ python examples/pytorch/nlp/huggingface_models/language-modeling/pruning/eager/r
     --model_name_or_path /PATH/TO/LLM/ \
     --calibration_dataset_name wikitext-2-raw-v1 \
     --evaluation_dataset_name lambada \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 16 \
-    --block_size 512 \
-    --max_length 512 \
     --do_prune \
     --device=0 \
     --output_dir=/PATH/TO/SAVE/ \
     --target_sparsity 0.5 \
     --pruning_pattern 1x1
+
 
 
