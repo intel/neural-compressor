dc9328c09b243d7df3bccc0a35a8a12feaabb40a
Yi30 <106061964+yiliu30@users.noreply.github.com>
2023-11-14 17:47:39 +0800

    Introduce INC 3.0 quantization API and port torch RTN into 3.0 (#1380)
    Signed-off-by: yiliu30 <yi4.liu@intel.com>
    Signed-off-by: chensuyue <suyue.chen@intel.com>

---
+++ neural_compressor/torch/utils.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from typing import Callable, Dict
+
+# Dictionary to store a mapping between algorithm names and corresponding algo implementation(function)
+algos_mapping: Dict[str, Callable] = {}
+
+
+def register_algo(name):
+    """Decorator function to register algorithms in the algos_mapping dictionary.
+
+    Usage example:
+        @register_algo(name=example_algo)
+        def example_algo(model: torch.nn.Module, quant_config: RTNWeightQuantConfig) -> torch.nn.Module:
+            ...
+
+    Args:
+        name (str): The name under which the algorithm function will be registered.
+
+    Returns:
+        decorator: The decorator function to be used with algorithm functions.
+    """
+
+    def decorator(algo_func):
+        algos_mapping[name] = algo_func
+        return algo_func
+
+    return decorator
