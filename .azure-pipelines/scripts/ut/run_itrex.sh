a9e1704ec519c86976129f13a2c875986a8f6dee
Sun, Xuehao <xuehao.sun@intel.com>
2023-11-20 14:47:30 +0800

    for test
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/run_itrex.sh
+++ .azure-pipelines/scripts/ut/run_itrex.sh
@@ -15,23 +15,14 @@ mkdir -p ${LOG_DIR}
 ut_log_name=${LOG_DIR}/ut_itrex.log
 
 # run unit test
-pip install pytest
-pytest -v -s --log-cli-level=WARNING --junitxml=test-ITREX.xml /intel-extension-for-transformers/tests -k "not test_tf" 2>&1 | tee -a ${ut_log_name}
+find . -name "test*.py" | grep -v "test_tf" | sed 's,\.\/,python ,g' | sed 's/$/ --verbose/' >run.sh
 
-xml_data=$(cat test-ITREX.xml)
-errors=$(echo "$xml_data" | grep -o 'errors="[0-9]*"' | sed 's/errors="\([0-9]*\)"/\1/')
-failures=$(echo "$xml_data" | grep -o 'failures="[0-9]*"' | sed 's/failures="\([0-9]*\)"/\1/')
-
-echo "Errors: $errors"
-echo "Failures: $failures"
-
-if [ "$errors" -ne 0 ] || [ "$failures" -ne 0 ]; then
-    echo "unit test failed!"
-    exit 1
-else
-    echo "unittest succeed!"
-fi
-
+# run UT
+$BOLD_YELLOW && echo "cat run.sh..." && $RESET
+cat run.sh | tee ${ut_log_name}
+$BOLD_YELLOW && echo "------UT start-------" && $RESET
+bash -x run.sh 2>&1 | tee -a ${ut_log_name}
+$BOLD_YELLOW && echo "------ UT end -------" && $RESET
 
 if [ $(grep -c "FAILED" ${ut_log_name}) != 0 ] || [ $(grep -c "core dumped" ${ut_log_name}) != 0 ] || [ $(grep -c "ModuleNotFoundError:" ${ut_log_name}) != 0 ] || [ $(grep -c "OK" ${ut_log_name}) == 0 ];then
     echo "Find errors in UT test, please check the output..."
