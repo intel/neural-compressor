567f2f7f2b036df81ac20358c8d8de4bfa271308
Lu, Yintong <yintong.lu@intel.com>
2023-11-21 10:28:01 +0800

    [Algo] update RMSnorm
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>

--- neural_compressor/adaptor/torch_utils/util.py
+++ neural_compressor/adaptor/torch_utils/util.py
@@ -1278,18 +1278,19 @@ from torch import nn
 
 
 class LlamaRMSNorm_bias(nn.Module):
-    def __init__(self, hidden_size, eps=1e-6):
+    def __init__(self, hidden_size, eps=1e-6, bias=None):
         """LlamaRMSNorm is equivalent to T5LayerNorm."""
         super().__init__()
         self.weight = nn.Parameter(torch.ones(hidden_size))
         self.variance_epsilon = eps
+        self.bias = bias
 
     def forward(self, hidden_states):
         input_dtype = hidden_states.dtype
         hidden_states = hidden_states.to(torch.float32)
         variance = hidden_states.pow(2).mean(-1, keepdim=True)
         hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-        if hasattr(self, "bias") and self.bias is not None:  # lyt_add
+        if self.bias is not None:  # lyt_add
             logger.info(f"lyt_debug util modeling_llama bias_added, {torch.mean(self.bias)}")
             return self.weight * hidden_states.to(input_dtype) + self.bias.to(input_dtype)
         else:
