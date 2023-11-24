88e709afd9aa3d79dc250157b01dbbacfca0b5ac
yiliu30 <yi4.liu@intel.com>
2023-11-24 13:21:06 +0800

    add more UTs for GPTQ
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- test/3x/torch/test_gptq.py
+++ test/3x/torch/test_gptq.py
@@ -18,32 +18,27 @@ def get_gpt_j():
 
 
 class GPTQLLMDataLoader:
-    def __init__(self):
+    def __init__(self, length=512):
         self.batch_size = 1
+        self.length = length
 
     def __iter__(self):
         for i in range(10):
-            yield torch.ones([1, 512], dtype=torch.long)
+            yield torch.ones([1, self.length], dtype=torch.long)
 
 
-class GPTQLLMDataLoaderList:
-    def __init__(self):
-        self.batch_size = 1
-
+class GPTQLLMDataLoaderList(GPTQLLMDataLoader):
     def __iter__(self):
         for i in range(10):
-            yield (torch.ones([1, 512], dtype=torch.long), torch.ones([1, 512], dtype=torch.long))
+            yield (torch.ones([1, self.length], dtype=torch.long), torch.ones([1, self.length], dtype=torch.long))
 
 
-class GPTQLLMDataLoaderDict:
-    def __init__(self):
-        self.batch_size = 1
-
+class GPTQLLMDataLoaderDict(GPTQLLMDataLoader):
     def __iter__(self):
         for i in range(10):
             yield {
-                "input_ids": torch.ones([1, 512], dtype=torch.long),
-                "attention_mask": torch.ones([1, 512], dtype=torch.long),
+                "input_ids": torch.ones([1, self.length], dtype=torch.long),
+                "attention_mask": torch.ones([1, self.length], dtype=torch.long),
             }
 
 
@@ -60,7 +55,7 @@ class TestGPTQ(unittest.TestCase):
         # print the test name
         logger.info(f"Running TestGPTQ test: {self.id()}")
 
-    def test_default_gptq(self):
+    def test_gptq(self):
         # Ported from test/adaptor/pytorch_adaptor/test_weight_only_adaptor.py
         # TestPytorchWeightOnlyAdaptor.test_GPTQ_fixed_length_quant
         from neural_compressor.torch import GPTQConfig, quantize
@@ -78,6 +73,40 @@ class TestGPTQ(unittest.TestCase):
         q_model = quantize(model=model_1, quant_config=quant_config, calib_dataloader=dataloader)
         out1 = q_model(input)
         self.assertTrue(torch.allclose(out1[0], out0[0], atol=1e-02))
+
+    def _apply_gptq(self, input, model, quant_config, dataloader):
+        logger.info(f"Test GPTQ with config {quant_config}")
+        from neural_compressor.torch import quantize
+
+        out0 = model(input)
+        q_model = quantize(model=model, quant_config=quant_config, calib_dataloader=dataloader)
+        out1 = q_model(input)
+        self.assertTrue(torch.allclose(out1[0], out0[0], atol=1e-02))
+
+    def test_more_gptq(self):
+        import random
+        from itertools import product
+
+        from neural_compressor.torch import GPTQConfig
+
+        # some tests were skipped to accelerate the CI
+        input = torch.ones([1, 512], dtype=torch.long)
+        # dataloader
+        dataloader_collections = [GPTQLLMDataLoader, GPTQLLMDataLoaderList, GPTQLLMDataLoaderDict]
+        gptq_options = {
+            "weight_sym": [False, True],
+            "weight_group_size": [8],
+            "use_max_length": [False, True],
+            "pad_max_length": [512],
+        }
+        for dataloader in dataloader_collections:
+            for value in product(*gptq_options.values()):
+                d = dict(zip(gptq_options.keys(), value))
+                quant_config = GPTQConfig(**d)
+                length = 512 if quant_config.use_max_length else random.randint(1, 1024)
+                self._apply_gptq(
+                    model=get_gpt_j(), input=input, quant_config=quant_config, dataloader=dataloader(length)
+                )
 
 
 if __name__ == "__main__":
