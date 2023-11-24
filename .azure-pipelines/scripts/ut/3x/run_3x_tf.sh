b326e39ed4e36a748e3336168b9add9c18a0821b
chensuyue <suyue.chen@intel.com>
2023-11-24 14:53:11 +0800

    support 3x tf CI test
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/ut/3x/run_3x_tf.sh
+++ .azure-pipelines/scripts/ut/3x/run_3x_tf.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 python -c "import neural_compressor as nc;print(nc.version.__version__)"
-test_case="run 3x Torch"
+test_case="run 3x TensorFlow"
 echo "${test_case}"
 
 # install requirements
 echo "set up UT env..."
-pip install -r /neural-compressor/requirements_pt.txt
-pip install transformers
+pip install -r /neural-compressor/requirements_tf.txt
 pip install coverage
 pip install pytest
 pip list
 
-export COVERAGE_RCFILE=/neural-compressor/.azure-pipelines/scripts/ut/coverage.3x_pt
+export COVERAGE_RCFILE=/neural-compressor/.azure-pipelines/scripts/ut/coverage.3x_tf
 lpot_path=$(python -c 'import neural_compressor; import os; print(os.path.dirname(neural_compressor.__file__))')
 cd /neural-compressor/test || exit 1
-find ./3x/torch/* -name "test*.py" | sed 's,\.\/,coverage run --source='"${lpot_path}"' --append ,g' | sed 's/$/ --verbose/'> run.sh
+find ./3x/tensorflow/* -name "test*.py" | sed 's,\.\/,coverage run --source='"${lpot_path}"' --append ,g' | sed 's/$/ --verbose/'> run.sh
 
 LOG_DIR=/neural-compressor/log_dir
 mkdir -p ${LOG_DIR}
-ut_log_name=${LOG_DIR}/ut_3x_pt.log
+ut_log_name=${LOG_DIR}/ut_3x_tf.log
 
 echo "cat run.sh..."
 sort run.sh -o run.sh
