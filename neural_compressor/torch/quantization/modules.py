4220909232f50ce624f2f10edd98deae4ae483f7
Xin He <xin3.he@intel.com>
2023-11-29 16:24:08 +0800

    fix import issue
    Signed-off-by: Xin He <xin3.he@intel.com>

--- neural_compressor/torch/quantization/modules.py
+++ neural_compressor/torch/quantization/modules.py
@@ -24,9 +24,9 @@ from packaging.version import Version
 from torch.autograd import Function
 from torch.nn import functional as F
 
+from neural_compressor.common import logger
+from neural_compressor.common.logger import DEBUG, level
 from neural_compressor.torch.algorithms.weight_only.rtn import quant_weight
-from neural_compressor.utils import logger
-from neural_compressor.utils.logger import DEBUG, level
 
 
 def get_torch_version():
@@ -140,7 +140,7 @@ class WeightOnlyLinear(torch.nn.Module):
         self.use_hf_format = use_hf_format
         self.dtype = dtype
         if "int" not in self.dtype:  # for nf4, fp4
-            from neural_compressor.adaptor.torch_utils.weight_only import FLOAT_MAPPING, INT_MAPPING
+            from neural_compressor.torch.algorithms.weight_only.rtn import FLOAT_MAPPING, INT_MAPPING
 
             float_list = FLOAT_MAPPING[self.dtype]
             int_list = INT_MAPPING[self.dtype]
