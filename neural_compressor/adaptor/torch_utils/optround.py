fix a bug in loss calculaiton at gradient accumulate gradients!=1 verified lamini-gpt125, seqlen should be <=512

neural_compressor/adaptor/torch_utils/optround.py
@@ -774,6 +774,16 @@ class OPTRoundQuantizer(object):
             optimizer.zero_grad()
             lr_schedule.step()
 
+    def is_supported_type(self,m):
+        return hasattr(m,"orig_layer")
+        # supported = False
+        # for t in self.supported_types:
+        #     if isinstance(m, t):
+        #         supported=True
+        #         break
+        # return  supported
+
+
     def quant_block(
         self, block, input_ids, input_others, num_bits, group_size, schema, q_input=None, device=torch.device("cpu")
     ):
@@ -799,7 +809,7 @@ class OPTRoundQuantizer(object):
         round_params = []
         minmax_params = []
         for n, m in block.named_modules():
-            if isinstance(m, WrapperLinear):
+            if self.is_supported_type(m):
                 round_params.append(m.value)
                 minmax_params.append(m.min_scale)
                 minmax_params.append(m.max_scale)
@@ -862,6 +872,7 @@ class OPTRoundQuantizer(object):
                         loss = mse_loss(output_q, current_output)
                 else:
                     loss = mse_loss(output_q, current_output)
+                loss = loss/self.gradient_accumulate_steps
                 total_loss += loss.item()
                 scale_loss = self.loss_scale_and_backward(scaler, loss)
 