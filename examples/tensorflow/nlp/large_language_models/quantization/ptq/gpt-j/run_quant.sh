712946832577963a72bddba9febb1f742449787d
zehao-intel <zehao.huang@intel.com>
2023-12-05 21:28:12 +0800

    fix prepare model
    Signed-off-by: zehao-intel <zehao.huang@intel.com>

--- examples/tensorflow/nlp/large_language_models/quantization/ptq/gpt-j/run_quant.sh
+++ examples/tensorflow/nlp/large_language_models/quantization/ptq/gpt-j/run_quant.sh
@@ -10,12 +10,11 @@ function main {
 
 # init params
 function init_params {
-  fp32_path="./gpt-j-6B"
   for var in "$@"
   do
     case $var in
-      --fp32_path=*)
-          fp32_path=$(echo "$var" |cut -f2 -d=)
+      --input_model=*)
+          input_model=$(echo "$var" |cut -f2 -d=)
       ;;
       --output_model=*)
           output_model=$(echo "$var" |cut -f2 -d=)
@@ -28,7 +27,7 @@ function init_params {
 # run_tuning
 function run_tuning {
     python main.py \
-        --fp32_path=${fp32_path} \
+        --input_model=${input_model} \
         --output_model=${output_model} \
         --output_dir="./outputs" \
         --tune
