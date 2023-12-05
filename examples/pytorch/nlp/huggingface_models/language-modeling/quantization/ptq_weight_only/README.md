4f6294676b3c871e0128e5c5afd9b5af10beb731
chensuyue <suyue.chen@intel.com>
2023-12-05 20:49:53 +0800

    Revert "remove examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_weight_only"
    This reverts commit b94b3f4fc22d077bb1c3dff5569cb91153ab308c.

---
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_weight_only/README.md
@@ -0,0 +1,148 @@
+Step-by-Step
+============
+
+This document is used to list the steps of reproducing weight only quantization and benchmarking results.
+
+# Prerequisite
+## 1. Environment
+Python 3.6 or higher version is recommended.
+The dependent packages are all in requirements, please install as following.
+```shell
+cd examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_weight_only
+pip install -r requirements.txt
+```
+
+# Run
+## 1. Quantization
+```shell
+python run_clm.py \
+  --model_name_or_path EleutherAI/gpt-j-6B \
+  --dataset_name wikitext\
+  --dataset_config_name wikitext-2-raw-v1 \
+  --do_train \
+  --do_eval \
+  --weight_only_bits 8 \
+  --weight_only_group -1 \
+  --weight_only_scheme sym \
+  --weight_only_algorithm RTN \
+  --tune \
+  --output_dir saved_results
+```
+or
+```bash
+sh run_quant.sh --topology=gpt_j_wikitext_weight_only --input_model=EleutherAI/gpt-j-6B --weight_only_bits=8 --weight_only_group=-1 --weight_only_scheme=sym --weight_only_algorithm=RTN
+```
+
+> NOTE
+>
+> `weight_only_bits`, `weight_only_group`, `weight_only_scheme`, and `weight_only_algorithm` can be modified by user. For details, please refer to [README](../../../../../../../docs/source/quantization_weight_only.md).
+
+### Run MLPerf on GPT-J-6B using GPTQ
+Use the following link to get
+[**CNN Daily Mail** datasets](https://github.com/intel-innersource/frameworks.ai.benchmarking.mlperf.submission.inference-submission-v3-1/tree/master/closed/Intel/code/gpt-j/pytorch-cpu#download-and-prepare-dataset)
+and [gpt-j-6B mlperf model](https://github.com/mlcommons/inference/tree/master/language/gpt-j#download-gpt-j-model)
+
+Then run following command to do quantization (please refer to *run_gptj_mlperf_int4.sh*)
+```shell
+python -u examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_weight_only/run_gptj_mlperf_int4.py \
+    --model_name_or_path /your/gptj/model/ \
+    --wbits 4 \
+    --sym \
+    --group_size -1 \
+    --nsamples 128 \
+    --calib-data-path /your/data/calibration-data/cnn_dailymail_calibration.json \
+    --val-data-path /your/data/validation-data/cnn_dailymail_validation.json \
+    --calib-iters 128 \
+    --use_max_length \
+    --pad_max_length 2048 \
+    --use_gpu
+```
+Notes: for per channel quantization, set group_size to **-1**, otherwise 32, 128, etc. More comprehensive details about user-defined arguments are available at our [weight_onlly quantization documentations](https://github.com/intel/neural-compressor/blob/master/docs/source/quantization_weight_only.md#quantization-capability)
+
+### Run general examples on a wide variety of LLMs using GPTQ
+We also support GPTQ algorithm on various language models (OPTs, Blooms, LLaMAs, MPTs, Falcons, ChatGLMs, etc.) in a generalized code. Please refer to script *run-gptq-llm.py* for more information.
+
+You can simply use following command to do quantization (please refer to *run-gptq-llm.sh*).
+```shell
+python examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_weight_only/run-gptq-llm.py \
+    --model_name_or_path facebook/opt-125m \
+    --weight_only_algo GPTQ \
+    --dataset NeelNanda/pile-10k \
+    --wbits 4 \
+    --group_size 128 \
+    --pad_max_length 2048 \
+    --use_max_length \
+    --seed 0 \
+    --gpu
+```
+
+## 2. Benchmark
+```bash
+# int8
+sh run_benchmark.sh --topology=topology_name --mode=performance --int8=true --input_model=model_name_or_path  --config=saved_results
+# fp32
+sh run_benchmark.sh --topology=topology_name --mode=performance --input_model=model_name_or_path
+```
+## 3. Validated Model List
+<table>
+<thead>
+  <tr>
+    <th>Topology Name</th>
+    <th>Model Name</th>
+    <th>Dataset/Task Name</th>
+  </tr>
+</thead>
+<tbody align="center">
+  <tr>
+    <td>gpt_j_wikitext_weight_only</td>
+    <td><a href="https://huggingface.co/EleutherAI/gpt-j-6B">EleutherAI/gpt-j-6B</a></td>
+    <td><a href="https://huggingface.co/datasets/wikitext">wikitext</a></td>
+  </tr>
+</tbody>
+</table>
+
+## 4. Saving and Loading Model
+### Saving model:
+```python
+from neural_compressor.config import AccuracyCriterion, PostTrainingQuantConfig
+from neural_compressor import quantization
+op_type_dict={
+    '.*':{
+        "weight": {
+            'bits': 8,
+            'group_size': 32,
+            'scheme': 'sym', 
+            'algorithm': 'RTN', 
+        },
+    },
+}
+accuracy_criterion = AccuracyCriterion(higher_is_better=False, tolerable_loss=0.01)
+conf = PostTrainingQuantConfig(accuracy_criterion=accuracy_criterion,
+                            approach='weight_only',
+                            op_type_dict=op_type_dict)
+q_model = quantization.fit(model,
+                           conf,
+                           calib_dataloader=dataloader(),
+                           eval_func=eval_func)
+q_model.save("output_dir")
+```
+Here, `q_model` is the Neural Compressor model class, so it has "save" API:
+
+```python
+q_model.save("Path_to_save_quantized_model")
+```
+### Loading model:
+```python
+from neural_compressor.utils.pytorch import load
+quantized_model = load(tuned_checkpoint, model)
+```
+--------
+
+For more details, please refer to the [sample code](./run_clm.py).
+
+# (May Remove Later) Run GPTQ algorithm
+```
+sh run-gptq-llm.sh
+# You may want to move script run-gptq-llm.sh to root dir of neural compressor and modify python file's path.
+# Please make sure pile dataset is downloaded.
+```
---
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/ptq_weight_only/run-gptq-llm.py
@@ -0,0 +1,306 @@
+import sys
+sys.path.append("./")
+import math
+import time
+import re
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.nn.functional import pad
+
+import transformers
+from transformers import AutoModelForCausalLM, AutoModel, AutoTokenizer
+import datasets
+from datasets import load_dataset
+
+#from neural_compressor.adaptor.torch_utils.weight_only import gptq_quantize
+from neural_compressor.adaptor.torch_utils.weight_only import gptq_quantize
+from neural_compressor import quantization, PostTrainingQuantConfig
+from evaluation import evaluate as lm_evaluate
+
+@torch.no_grad()
+def eval_ppl_with_gptq(model, test_dataloader, dev):
+    print('Evaluating ...', flush=True)
+    # model.eval()
+    model.to(dev)
+
+    testenc = test_dataloader.input_ids
+    nsamples = test_dataloader.numel() // model.seqlen
+
+    use_cache = model.config.use_cache
+    model.config.use_cache = False
+
+    test_dataloader = test_dataloader.to(dev)
+    nlls = []
+    for i in range(nsamples):
+        batch = test_dataloader[:, (i * model.seqlen):((i + 1) * model.seqlen)].to(dev)
+        lm_logits = model(batch).logits
+        shift_logits = lm_logits[:, :-1, :].contiguous()
+        shift_labels = test_dataloader[:, (i * model.seqlen):((i + 1) * model.seqlen)][:, 1:]
+        loss_fct = nn.CrossEntropyLoss()
+        loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)), shift_labels.view(-1))
+        neg_log_likelihood = loss.float() * model.seqlen
+        nlls.append(neg_log_likelihood)
+    ppl = torch.exp(torch.stack(nlls).sum() / (nsamples * model.seqlen))
+    print(ppl.item())
+
+    model.config.use_cache = use_cache
+    return ppl.item()
+
+class INCDataloader(object):
+    def __init__(self, gptq_dataloader):
+        self.batch_size = 1
+        self.gptq_dataloader = gptq_dataloader
+        self.length = len(gptq_dataloader)
+        self.batch_size = 1
+
+    def __iter__(self):
+        pass
+
+# INC original dataloader example
+class Evaluator:
+    def __init__(self, dataset, tokenizer, batch_size=8, pad_val=1, pad_max=196, is_calib=False):
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.batch_size = batch_size
+        self.pad_val = pad_val
+        self.pad_max = pad_max
+        self.is_calib = is_calib
+
+        # tokenize the dataset
+        self.dataset = self.dataset.map(self.tokenize_function, batched=True)
+        self.dataset.set_format(type="torch", columns=["input_ids"])
+
+    @torch.no_grad()
+    def tokenize_function(self, examples):
+        if args.weight_only_algo in ['AWQ', 'TEQ']:
+            if self.tokenizer.pad_token is None:
+                self.tokenizer.pad_token = self.tokenizer.eos_token
+            example = self.tokenizer(examples["text"], padding="max_length", max_length=self.pad_max)
+        else:
+            example = self.tokenizer(examples["text"])
+        return example
+
+    @torch.no_grad()
+    def collate_batch(self, batch):
+
+        input_ids_padded = []
+        last_ind = []
+
+        for text in batch:
+            input_ids = text["input_ids"]
+            pad_len = self.pad_max - input_ids.shape[0]
+            last_ind.append(input_ids.shape[0] - 1)
+            if self.is_calib:
+                # input_ids = input_ids[:self.pad_max] if len(input_ids) > self.pad_max else input_ids
+                input_ids = input_ids
+            else:
+                input_ids = pad(input_ids, (0, pad_len), value=self.pad_val)
+            input_ids_padded.append(input_ids)
+
+        return (torch.vstack(input_ids_padded), torch.tensor(last_ind))
+
+    @torch.no_grad()
+    def evaluate(self, model):
+
+        model.eval()
+        # The task is to predict the last word of the input.
+        total, hit = 0, 0
+        latency = 0
+        test_dataloader = DataLoader(
+            self.dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            collate_fn=self.collate_batch,
+        )
+        for i, (input_ids, last_ind) in enumerate(test_dataloader):
+            label = input_ids[torch.arange(len(last_ind)), last_ind]
+            input_ids[torch.arange(len(last_ind)), last_ind] = self.pad_val
+            pad_len = self.pad_max - last_ind - 1
+
+            start = time.time()
+            outputs = model(input_ids)
+            latency += time.time() - start
+
+            last_token_logits = outputs[0][torch.arange(len(last_ind)), -2 - pad_len, :]
+            pred = last_token_logits.argmax(dim=-1)
+            total += label.size(0)
+            hit += (pred == label).sum().item()
+            if i % 50 == 0:
+                print(hit / total)
+                print("Processed minibatch:", i)
+
+        acc = hit / total
+        print("Accuracy: ", acc)
+        lantecy = latency / len(self.dataset)
+        print("Latency: ", latency)
+        return acc
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--model_name_or_path', type=str,
+        help='BLOOM model to load; pass `bigscience/bloom-X`.'
+    )
+    parser.add_argument(
+        '--dataset', type=str, default="NeelNanda/pile-10k",
+        help='Where to extract calibration data from.'
+    )
+    parser.add_argument(
+        '--seed',
+        type=int, default=0, help='Seed for sampling the calibration data.'
+    )
+    parser.add_argument("--weight_only_algo", default="RTN", 
+        choices=['RTN', 'AWQ', 'TEQ', 'GPTQ'],
+        help="Weight-only parameter."
+    )
+    parser.add_argument(
+        '--nsamples', type=int, default=128,
+        help='Number of calibration data samples.'
+    )
+    parser.add_argument(
+        '--percdamp', type=float, default=.01,
+        help='Percent of the average Hessian diagonal to use for dampening. Please refer to GPTQ paper Part IV for more information'
+    )
+    parser.add_argument(
+        '--nearest', action='store_true',
+        help='Whether to run the RTN baseline.'
+    )
+    parser.add_argument(
+        '--wbits', type=int, default=16, choices=[2, 3, 4, 16],
+        help='#bits to use for quantization; use 16 for evaluating base model.'
+    )
+    parser.add_argument(
+        '--group_size', type=int, default=-1,
+        help='Groupsize to use for quantization, in every group weights shares same quantization parameters; default uses full row.'
+    )
+    parser.add_argument(
+        '--block_size', type=int, default=128,
+        help='Block size. sub weight matrix size to run GPTQ.'
+    )
+    parser.add_argument(
+        '--sym', action='store_true',
+        help='Whether to perform symmetric quantization.'
+    )
+    parser.add_argument(
+        '--new-eval', action='store_true',
+        help='Whether to use the new PTB and C4 eval'
+    )
+    parser.add_argument(
+        '--pad_max_length', type=int, default=2048,
+        help='sequence length for GPTQ calibration'
+    )
+    parser.add_argument(
+        '--calib_size', type=int, default=1,
+        help='batch size for calibration'
+    )
+    parser.add_argument('--act-order', action='store_true', 
+        help='Whether to apply the activation order GPTQ heuristic'
+    )
+    parser.add_argument('--use_max_length', action='store_true', 
+        help='Only select data whose length equals or more than model.seqlen, please refer to GPTQ original implementation'
+    )
+    parser.add_argument('--gpu', action='store_true', help='Whether to use gpu')
+
+    args = parser.parse_args()
+
+    def skip(*args, **kwargs):
+        pass
+    torch.nn.init.kaiming_uniform_ = skip
+    torch.nn.init.uniform_ = skip
+    torch.nn.init.normal_ = skip
+
+    # model
+    if re.search("chatglm", args.model_name_or_path.lower()): # chatglm requires a different way to be loaded
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, trust_remote_code=True)
+        model = AutoModel.from_pretrained(args.model_name_or_path, trust_remote_code=True)
+    else:
+        tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, use_fast=True)
+        model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, low_cpu_mem_usage=True, trust_remote_code=True)
+    model = model.eval()
+
+    calib_dataset = load_dataset(args.dataset, split="train") # default
+    # calib_dataset = datasets.load_from_disk('/your/local/pile-10k/') # use this if trouble with connecting to HF
+    calib_dataset = calib_dataset.shuffle(seed=args.seed)
+    calib_evaluator = Evaluator(calib_dataset, tokenizer, args.calib_size, is_calib=True)
+    calib_dataloader = DataLoader(
+        calib_evaluator.dataset,
+        batch_size=args.calib_size,
+        shuffle=False,
+        collate_fn=calib_evaluator.collate_batch,
+    )
+
+    if args.gpu and torch.cuda.is_available():
+        DEV = torch.device('cuda:0')
+    else:
+        DEV = torch.device('cpu')
+    
+    model = model.to(DEV)
+
+    if args.sym:
+        sym_opt = "sym"
+    else:
+        sym_opt = "asym"
+
+    # method 1: use general INC API
+    # conf = PostTrainingQuantConfig(
+    #     approach='weight_only',
+    #     op_type_dict={
+    #         '.*':{ 	# re.match
+    #             "weight": {
+    #                 'bits': args.wbits, # 1-8 bits 
+    #                 'group_size': args.group_size,  # -1 (per-channel)
+    #                 'scheme': sym_opt, 
+    #                 'algorithm': 'GPTQ', 
+    #             },
+    #         },
+    #     },
+    #     op_name_dict={
+    #         '.*lm_head':{ 	# re.match
+    #             "weight": {
+    #                 'dtype': 'fp32'
+    #             },
+    #         },
+    #     },
+    #     recipes={
+    #         'gptq_args':{
+    #             'percdamp': 0.01, 
+    #             'act_order':args.act_order, 
+    #             'block_size': args.block_size, 
+    #             'nsampeles': args.nsamples,
+    #             'use_max_length': args.use_max_length,
+    #             'pad_max_length': args.pad_max_length
+    #         },
+    #     },
+    # )
+    # q_model = quantization.fit(model, conf, calib_dataloader=calib_dataloader,)
+
+    # method 2: directly use INC built-in function, for some models like falcon, please use this function
+    conf = {
+        ".*":{
+            'wbits': args.wbits, # 1-8 bits 
+            'group_size': args.group_size,  # -1 (per-channel)
+            'sym': (sym_opt == "sym"),
+            'act_order': args.act_order,
+        }
+    } 
+    q_model, gptq_config = gptq_quantize(
+        model, 
+        weight_config=conf, 
+        dataloader=calib_dataloader, 
+        nsamples = args.nsamples, 
+        use_max_length = args.use_max_length,
+        pad_max_length = args.pad_max_length
+    )
+
+    results = lm_evaluate(
+        model="hf-causal",
+        model_args='pretrained='+args.model_name_or_path+',tokenizer='+args.model_name_or_path+',dtype=float32',
+        user_model=q_model.to(DEV), tasks=["lambada_openai"],
+        device=DEV.type,
+        batch_size=4
+    )
