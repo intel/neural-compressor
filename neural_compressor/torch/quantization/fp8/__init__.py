2448dd8a6eaf712bec864089822e3bcaf1b7ba10
yiliu30 <yi4.liu@intel.com>
2023-12-13 15:21:48 +0800

    fixed typos
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- neural_compressor/torch/quantization/fp8/__init__.py
+++ neural_compressor/torch/quantization/fp8/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .fp8_quantization_impl import quantize_dynamic, quantize
+from .quantization_impl import quantize_dynamic, quantize
