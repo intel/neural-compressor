ee6bc284b1e65c37b2f46857da9cd315019b208a
yintong-lu <108845308+yintong-lu@users.noreply.github.com>
2023-12-13 09:17:50 +0800

    Lyt/blockwise (#1441)
    * [Algo] blockwise tuning
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [Algo] code update
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [Algo] sq argument update
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [Algo] log update
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] code update
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] fix bugs
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] log update
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] enable blockwise on Llama models
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [Algo] enable blockwise on Llama models
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] code update
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] format code
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [Algo] fix bug
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [Algo] add ut
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] fix format issue
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [Algo] log update
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] move do_blockwise arg
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] fix bug
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] fix bug
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] fix bug
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [Algo] fix bug
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    * [Algo] fix bug
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    
    ---------
    
    Signed-off-by: Lu, Yintong <yintong.lu@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- test/algorithm/test_smooth_quant.py
+++ test/algorithm/test_smooth_quant.py
@@ -1548,5 +1548,32 @@ class TestInputConfig(unittest.TestCase):
         assert sq.auto_alpha_args["alpha_min"] == 0.5
 
 
+class TestAlphaAutoLinearBlockwise(unittest.TestCase):
+    @classmethod
+    def test_sq_linear_Blockwise_auto(self):
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            "facebook/opt-125m",
+            torchscript=True,
+        )
+        sq = TorchSmoothQuant(model, LLMCalibDataloader())
+        sq.transform(
+            alpha="auto",
+            calib_iter=1,
+            folding=False,
+            auto_alpha_args={
+                "alpha_min": 0.45,
+                "alpha_max": 0.55,
+                "alpha_step": 0.01,
+                "shared_criterion": "mean",
+                "do_blockwise": True,
+            },
+        )
+        for i in range(12):
+            op_name1 = "model.decoder.layers." + str(i) + ".self_attn.out_proj"
+            op_name2 = "model.decoder.layers." + str(i) + ".fc1"
+            assert sq.alpha_per_layer[op_name1] == sq.alpha_per_layer[op_name2]
+        assert len(sq.block_names) == 13
+
+
 if __name__ == "__main__":
     unittest.main()
