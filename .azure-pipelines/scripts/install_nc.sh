dc1c3c33611eb33a5f2ec3925a6d86df74225beb
chensuyue <suyue.chen@intel.com>
2024-01-18 15:24:43 +0800

    support ort 3.x CI test
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/install_nc.sh
+++ .azure-pipelines/scripts/install_nc.sh
@@ -10,6 +10,10 @@ elif [[ $1 = *"3x_tf" ]]; then
     python -m pip install --no-cache-dir -r requirements_tf.txt
     python setup.py tf bdist_wheel
     pip install dist/neural_compressor*.whl
+elif [[ $1 = *"3x_ort" ]]; then
+    python -m pip install --no-cache-dir -r requirements_ort.txt
+    python setup.py ort bdist_wheel
+    pip install dist/neural_compressor*.whl
 else
     python -m pip install --no-cache-dir -r requirements.txt
     python setup.py 2x bdist_wheel
