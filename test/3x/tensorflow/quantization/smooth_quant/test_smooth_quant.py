66809013cfca2d8328fce52924b9c5ee67e01b9d
zehao-intel <zehao.huang@intel.com>
2024-01-31 11:24:51 +0800

    add more ut cases
    Signed-off-by: zehao-intel <zehao.huang@intel.com>

--- test/3x/tensorflow/quantization/smooth_quant/test_smooth_quant.py
+++ test/3x/tensorflow/quantization/smooth_quant/test_smooth_quant.py
@@ -6,10 +6,38 @@ import tensorflow as tf
 from tensorflow.compat.v1 import graph_util
 
 from neural_compressor.common import set_random_seed
-from neural_compressor.tensorflow import SmoothQuantConfig, quantize_model
-from neural_compressor.tensorflow.quantization.tf_utils.util import disable_random
 from neural_compressor.tensorflow.utils import DummyDataset
+from neural_compressor.tensorflow.quantization.tf_utils.util import disable_random
+from neural_compressor.tensorflow import SmoothQuantConfig, quantize_model, get_default_sq_config
 
+def build_conv_graph():
+    tf.compat.v1.disable_eager_execution()
+    x = tf.compat.v1.placeholder(tf.float32, [1, 56, 56, 16], name="input")
+    top_relu = tf.nn.relu(x)
+    paddings = tf.constant([[0, 0], [1, 1], [1, 1], [0, 0]])
+    x_pad = tf.pad(top_relu, paddings, "CONSTANT")
+    conv_weights = tf.compat.v1.get_variable(
+        "weight", [3, 3, 16, 16], initializer=tf.compat.v1.random_normal_initializer()
+    )
+    conv = tf.nn.conv2d(x_pad, conv_weights, strides=[1, 2, 2, 1], padding="VALID")
+    normed = tf.compat.v1.layers.batch_normalization(conv)
+
+    conv_weights2 = tf.compat.v1.get_variable(
+        "weight2", [3, 3, 16, 16], initializer=tf.compat.v1.random_normal_initializer()
+    )
+    conv2 = tf.nn.conv2d(top_relu, conv_weights2, strides=[1, 2, 2, 1], padding="SAME")
+    normed2 = tf.compat.v1.layers.batch_normalization(conv2)
+    add = tf.raw_ops.Add(x=normed, y=normed2, name="addv2")
+    relu = tf.nn.relu(add)
+    relu6 = tf.nn.relu6(relu, name="op_to_store")
+
+    out_name = relu6.name.split(":")[0]
+    with tf.compat.v1.Session() as sess:
+        sess.run(tf.compat.v1.global_variables_initializer())
+        output_graph_def = graph_util.convert_variables_to_constants(
+            sess=sess, input_graph_def=sess.graph_def, output_node_names=[out_name]
+        )
+        return output_graph_def
 
 class MyDataLoader:
     def __init__(self, dataset, batch_size=1):
@@ -37,7 +65,7 @@ class MyDataLoader:
 class TestSmoothQuantTF3xNewApi(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        pass
+        self.conv_graph = build_conv_graph()
 
     @classmethod
     def tearDownClass(self):
@@ -45,37 +73,44 @@ class TestSmoothQuantTF3xNewApi(unittest.TestCase):
 
     @disable_random()
     def test_conv(self):
-        x = tf.compat.v1.placeholder(tf.float32, [1, 56, 56, 16], name="input")
-        top_relu = tf.nn.relu(x)
-        paddings = tf.constant([[0, 0], [1, 1], [1, 1], [0, 0]])
-        x_pad = tf.pad(top_relu, paddings, "CONSTANT")
-        conv_weights = tf.compat.v1.get_variable(
-            "weight", [3, 3, 16, 16], initializer=tf.compat.v1.random_normal_initializer()
-        )
-        conv = tf.nn.conv2d(x_pad, conv_weights, strides=[1, 2, 2, 1], padding="VALID")
-        normed = tf.compat.v1.layers.batch_normalization(conv)
-
-        conv_weights2 = tf.compat.v1.get_variable(
-            "weight2", [3, 3, 16, 16], initializer=tf.compat.v1.random_normal_initializer()
-        )
-        conv2 = tf.nn.conv2d(top_relu, conv_weights2, strides=[1, 2, 2, 1], padding="SAME")
-        normed2 = tf.compat.v1.layers.batch_normalization(conv2)
-        add = tf.raw_ops.Add(x=normed, y=normed2, name="addv2")
-        relu = tf.nn.relu(add)
-        relu6 = tf.nn.relu6(relu, name="op_to_store")
-
-        out_name = relu6.name.split(":")[0]
-        with tf.compat.v1.Session() as sess:
-            sess.run(tf.compat.v1.global_variables_initializer())
-            output_graph_def = graph_util.convert_variables_to_constants(
-                sess=sess, input_graph_def=sess.graph_def, output_node_names=[out_name]
-            )
-
         set_random_seed(9527)
         quant_config = SmoothQuantConfig(alpha=0.5)
         dataset = DummyDataset(shape=(100, 56, 56, 16), label=True)
         calib_dataloader = MyDataLoader(dataset=dataset, batch_size=1)
-        q_model = quantize_model(output_graph_def, quant_config, calib_dataloader, calib_iteration=500)
+        q_model = quantize_model(self.conv_graph, quant_config, calib_dataloader, calib_iteration=500)
+
+        mul_count = 0
+        for i in q_model.graph_def.node:
+            if i.op == "Mul":
+                mul_count += 1
+
+        self.assertEqual(mul_count, 2)
+    
+    def test_sq_from_class_beginner(self):
+        set_random_seed(9527)
+        quant_config = get_default_sq_config()
+        dataset = DummyDataset(shape=(100, 56, 56, 16), label=True)
+        calib_dataloader = MyDataLoader(dataset=dataset, batch_size=1)
+        q_model = quantize_model(self.conv_graph, quant_config, calib_dataloader, calib_iteration=500)
+
+        mul_count = 0
+        for i in q_model.graph_def.node:
+            if i.op == "Mul":
+                mul_count += 1
+
+        self.assertEqual(mul_count, 2)
+
+    def test_sq_from_dict_beginner(self):
+        quant_config = {
+            "smooth_quant": {
+                "global": {
+                    "alpha": 0.5,
+                },
+            }
+        }
+        dataset = DummyDataset(shape=(100, 56, 56, 16), label=True)
+        calib_dataloader = MyDataLoader(dataset=dataset, batch_size=1)
+        q_model = quantize_model(self.conv_graph, quant_config, calib_dataloader, calib_iteration=500)
 
         mul_count = 0
         for i in q_model.graph_def.node:
