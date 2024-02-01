0181734c4812ec6661f239f4b97493a5743b1fe0
zehao-intel <zehao.huang@intel.com>
2024-02-01 16:54:47 +0800

    Migrate TF Smooth Quant to 3.x API
    Signed-off-by: zehao-intel <zehao.huang@intel.com>

--- neural_compressor/tensorflow/algorithms/static_quant/__init__.py
+++ neural_compressor/tensorflow/algorithms/static_quant/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from neural_compressor.tensorflow.algorithms.static_quantize.quantize_entry import static_quantize_entry
+from neural_compressor.tensorflow.algorithms.static_quant.keras import KerasAdaptor
