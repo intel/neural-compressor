47351b9e480e38e4df5a69fdf9710f53fea20792
Cheng, Zixuan <zixuan.cheng@intel.com>
2024-02-04 14:56:21 +0800

    update ut to pytest
    Signed-off-by: Cheng, Zixuan <zixuan.cheng@intel.com>

--- test/3x/torch/quantization/test_static_quant.py
+++ test/3x/torch/quantization/test_static_quant.py
@@ -1,5 +1,5 @@
 import copy
-import unittest
+import pytest
 
 import intel_extension_for_pytorch as ipex
 import torch
@@ -31,29 +31,33 @@ def run_fn(model):
     model(torch.rand((1, 30)))
 
 
-class TestStaticQuant(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
+class TestStaticQuant:
+    def setup_class(self):
         self.fp32_model = build_simple_torch_model()
         self.input = torch.randn(1, 30)
 
-    @classmethod
-    def tearDownClass(self):
+    def teardown_class(self):
         pass
 
-    def setUp(self):
-        logger.info(f"Running TestStaticQuant test: {self.id()}")
-
-    def test_quantize_rtn_from_dict_default(self):
+    def test_static_quant_default(self):
         fp32_model = copy.deepcopy(self.fp32_model)
         quant_config = get_default_static_config()
         example_inputs = self.input
-        out1 = fp32_model(example_inputs)
         q_model = quantize(fp32_model, quant_config=quant_config, run_fn=run_fn, example_inputs=example_inputs)
-        out2 = q_model(example_inputs)
-        # self.assertTrue(torch.allclose(out1[0], out2[0], atol=1e-05))
-        self.assertIsNotNone(q_model)
+        assert q_model is not None, "Quantization failed!"
 
-
-if __name__ == "__main__":
-    unittest.main()
+    @pytest.mark.parametrize(
+        "act_sym, act_algo",
+        [
+            (True, "kl"),
+            (True, "minmax"),
+            (False, "kl"),
+            (False, "minmax"),
+        ],
+    )
+    def test_static_quant_params(self, act_sym, act_algo):
+        fp32_model = copy.deepcopy(self.fp32_model)
+        quant_config = StaticQuantConfig(act_sym=act_sym, act_algo=act_algo)
+        example_inputs = self.input
+        q_model = quantize(fp32_model, quant_config=quant_config, run_fn=run_fn, example_inputs=example_inputs)
+        assert q_model is not None, "Quantization failed!"
