486b6ef1eb1f8bc94b8f855e1d27f3d4b5db1338
zehao-intel <zehao.huang@intel.com>
2024-02-22 16:26:53 +0800

    remove unexpected change
    Signed-off-by: zehao-intel <zehao.huang@intel.com>

--- neural_compressor/torch/quantization/quantize.py
+++ neural_compressor/torch/quantization/quantize.py
@@ -12,81 +12,72 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Callable, Dict, Tuple, Union
+import copy
+from typing import Any, Callable, Dict, Tuple
 
-import tensorflow as tf
+import torch
 
-from neural_compressor.common import logger
 from neural_compressor.common.base_config import BaseConfig, ComposableConfig, config_registry
-from neural_compressor.common.utils import STATIC_QUANT
-from neural_compressor.tensorflow.utils import BaseModel, KerasModel, Model, algos_mapping
+from neural_compressor.torch.quantization.config import SmoothQuantConfig, StaticQuantConfig
+from neural_compressor.torch.utils import is_ipex_available, logger
+from neural_compressor.torch.utils.utility import WHITE_MODULE_LIST, algos_mapping, get_model_info
+
+FRAMEWORK_NAME = "torch"
 
 
 def need_apply(configs_mapping: Dict[Tuple[str, callable], BaseConfig], algo_name):
     return any(config.name == algo_name for config in configs_mapping.values())
 
 
-def quantize_model(
-    model: Union[str, tf.keras.Model, BaseModel],
-    quant_config: Union[BaseConfig, list],
-    calib_dataloader: Callable = None,
-    calib_iteration: int = 100,
-):
-    """The main entry to quantize model.
-
-    Args:
-        model: a fp32 model to be quantized.
-        quant_config: single or lists of quantization configuration.
-        calib_dataloader: a data loader for calibration.
-        calib_iteration: the iteration of calibration.
-
-    Returns:
-        q_model: the quantized model.
-    """
-    q_model = Model(model)
-    if not isinstance(quant_config, list):
-        return quantize_model_with_single_config(q_model, quant_config, calib_dataloader, calib_iteration)
-
-    for config in quant_config:
-        q_model = quantize_model_with_single_config(q_model, config, calib_dataloader, calib_iteration)
-
-    return q_model
-
-def quantize_model_with_single_config(
-    q_model: BaseModel,
+def quantize(
+    model: torch.nn.Module,
     quant_config: BaseConfig,
-    calib_dataloader: Callable = None,
-    calib_iteration: int = 100,
-):
-    """Quantize model using single config.
+    run_fn: Callable = None,
+    run_args: Any = None,
+    inplace: bool = True,
+    example_inputs: Any = None,
+) -> torch.nn.Module:
+    """The main entry to quantize model with static mode.
 
     Args:
-        q_model: a model wrapped by INC TF model class.
+        model: a float model to be quantized.
         quant_config: a quantization configuration.
-        calib_dataloader: a data loader for calibration.
-        calib_iteration: the iteration of calibration.
+        run_fn: a calibration function for calibrating the model. Defaults to None.
+        run_args: positional arguments for `run_fn`. Defaults to None.
+        example_inputs: used to trace torch model.
 
     Returns:
-        q_model: the quantized model.
+        The quantized model.
     """
-    framework_name = "keras" if isinstance(q_model, KerasModel) else "tensorflow"
+    q_model = model if inplace else copy.deepcopy(model)
     registered_configs = config_registry.get_cls_configs()
     if isinstance(quant_config, dict):
-        quant_config = ComposableConfig.from_dict(quant_config, config_registry=registered_configs[framework_name])
+        quant_config = ComposableConfig.from_dict(quant_config, config_registry=registered_configs[FRAMEWORK_NAME])
         logger.info(f"Parsed a config dict to construct the quantization config: {quant_config}.")
     else:
         assert isinstance(
             quant_config, BaseConfig
         ), f"Please pass a dict or config instance as the quantization configuration, but got {type(quant_config)}."
-    logger.info(f"Quantize model with config: \n {quant_config.to_json_string()} \n")
+    logger.info("Quantize model with config:")
+    logger.info(quant_config.to_dict())
     # select quantization algo according to config
 
-    model_info = quant_config.get_model_info(model=q_model)
+    if is_ipex_available and (
+        isinstance(quant_config, StaticQuantConfig) or isinstance(quant_config, SmoothQuantConfig)
+    ):
+        model_info = quant_config.get_model_info(q_model, example_inputs)
+    else:
+        model_info = quant_config.get_model_info(model=q_model)
     configs_mapping = quant_config.to_config_mapping(model_info=model_info)
     logger.debug(configs_mapping)
     for algo_name, algo_func in algos_mapping.items():
         if need_apply(configs_mapping, algo_name):
             logger.info(f"Start to apply {algo_name} on the model.")
-            q_model = algo_func(q_model, configs_mapping, calib_dataloader, calib_iteration)
-
+            q_model = algo_func(
+                q_model,
+                configs_mapping,
+                run_fn=run_fn,
+                run_args=run_args,
+                example_inputs=example_inputs,
+            )
     return q_model
