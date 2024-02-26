9a3c36cf4c008a3e06e65d5cf413115cc05dac48
yuwenzho <yuwen.zhou@intel.com>
2024-02-26 10:45:15 +0800

    update lwq core
    Signed-off-by: yuwenzho <yuwen.zhou@intel.com>

--- neural_compressor/onnxrt/algorithms/layer_wise/core.py
+++ neural_compressor/onnxrt/algorithms/layer_wise/core.py
@@ -19,14 +19,14 @@
 # limitations under the License.
 
 import os
+import transformers
 from copy import deepcopy
 from pathlib import Path
 from typing import Callable, List, Union
+from packaging.version import Version
 
 import onnx
 import onnxruntime as ort
-import transformers
-from packaging.version import Version
 
 from neural_compressor.common import Logger
 from neural_compressor.onnxrt.quantization.calibrate import CalibrationDataReader
@@ -59,11 +59,11 @@ def layer_wise_quant(
     Returns:
         _type_: _description_
     """
+    # TODO: remove the limitation for lwq
     if Version(transformers.__version__) > Version("4.37.2"):
-        logger.warning(
-            "Model (such as llama-2) exported with transformers {} may fail in layer-wise quant. "
-            "we recommand downgrading transformers to 4.37.2 and try again.".format(transformers.__version__)
-        )
+        logger.warning("Model (such as llama-2) exported with transformers {} may fail in layer-wise quant. "
+                       "we recommand downgrading transformers to 4.37.2 and try again.".format(
+                        transformers.__version__))
 
     # check whether model shape is inferred
     if not check_model_with_infer_shapes(model):
