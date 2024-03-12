c5eda1eac6853be4136bb26e4b8202efd76a370b
Mengni Wang <mengni.wang@intel.com>
2024-03-12 15:37:55 +0800

    Fix code_detection example export issue
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- examples/onnxrt/nlp/huggingface_model/code_detection/quantization/ptq_dynamic/prepare_model.py
+++ examples/onnxrt/nlp/huggingface_model/code_detection/quantization/ptq_dynamic/prepare_model.py
@@ -1,35 +1,17 @@
 import argparse
 import os
-import subprocess
-
+from optimum.exporters.onnx import main_export
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_model", type=str, required=False, default="checkpoint-best-acc/")
-    parser.add_argument("--output_model", type=str, required=False, default="codebert-exported-onnx/")
+    parser.add_argument("--input_model", type=str, required=False, default="checkpoint-best-acc")
+    parser.add_argument("--output_model", type=str, required=False, default="codebert-exported-onnx")
     return parser.parse_args()
 
 def prepare_model(input_model, output_model):
     print("\nexport model...")
     print(f"Try to export model from {input_model} to {output_model}")
-    subprocess.run(
-        ["pip", "install", "optimum"],
-        stdout=subprocess.PIPE,
-        text=True,
-    )
-    subprocess.run(
-        [
-            "optimum-cli",
-            "export",
-            "onnx",
-            "--model",
-            f"{input_model}",
-            f"{output_model}",
-            "--task=text-classification",
-        ],
-        stdout=subprocess.PIPE,
-        text=True,
-    )
+    main_export(input_model, output=output_model, task="text-classification")
 
     assert os.path.exists(output_model), f"{output_model} doesn't exist!"
 
