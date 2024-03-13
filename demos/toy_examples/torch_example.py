3be159a361020cd57864ddb457f6492025d53dc7
yiliu30 <yi4.liu@intel.com>
2024-03-14 07:51:04 +0800

    update the model name
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- demos/toy_examples/torch_example.py
+++ demos/toy_examples/torch_example.py
@@ -3,6 +3,6 @@ from transformers import AutoModel
 from neural_compressor.config import PostTrainingQuantConfig
 from neural_compressor.quantization import fit
 
-float_model = AutoModel.from_pretrained("bert-base-uncased")
+float_model = AutoModel.from_pretrained("facebook/opt-125m")
 woq_conf = PostTrainingQuantConfig(approach="weight_only")
 quantized_model = fit(model=float_model, conf=woq_conf)
