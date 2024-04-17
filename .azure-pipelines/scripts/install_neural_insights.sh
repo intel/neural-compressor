164a613c915ff8cdbd3b9cf95a5300b43c9c51b9
chen, suyue <suyue.chen@intel.com>
2024-04-17 09:13:01 +0800

    Cut CI test for ONC (#1734)
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/install_neural_insights.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Install Neural Insights ... "
-cd /neural-compressor
-python -m pip install --no-cache-dir -r neural_insights/requirements.txt
-python setup.py neural_insights bdist_wheel
-pip install dist/neural_insights*.whl
-pip list
--- .azure-pipelines/scripts/ut/run_ort.sh
+++ .azure-pipelines/scripts/ut/run_ort.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 python -c "import neural_compressor as nc"
-test_case="run 3x ONNXRT"
+test_case="run ONNXRT"
 echo "${test_case}"
 
 # install requirements
 echo "set up UT env..."
-pip install -r /neural-compressor/test/3x/onnxrt/requirements.txt
+pip install -r /neural-compressor/test/onnxrt/requirements.txt
 pip install pytest-cov
 pip install pytest-html
 pip list
 
-export COVERAGE_RCFILE=/neural-compressor/.azure-pipelines/scripts/ut/3x/coverage.3x_ort
-inc_path=$(python -c 'import neural_compressor; print(neural_compressor.__path__[0])')
-cd /neural-compressor/test/3x || exit 1
-rm -rf torch
-rm -rf tensorflow
+export COVERAGE_RCFILE=/neural-compressor/.azure-pipelines/scripts/ut/coverage.ort
+inc_path=$(python -c 'import neural_compressor_ort; print(neural_compressor_ort.__path__[0])')
+cd /neural-compressor/test || exit 1
 
 LOG_DIR=/neural-compressor/log_dir
 mkdir -p ${LOG_DIR}
-ut_log_name=${LOG_DIR}/ut_3x_ort.log
+ut_log_name=${LOG_DIR}/ut_ort.log
 pytest --cov="${inc_path}" -vs --disable-warnings --html=report.html --self-contained-html . 2>&1 | tee -a ${ut_log_name}
 
 cp report.html ${LOG_DIR}/
