4728fdccbbc3d9d8a213a1234aed7921596ddd51
Kaihui-intel <kaihui.tang@intel.com>
2024-05-21 14:17:14 +0800

    Support `auto_round` integration 2.x (#1806)
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- test/adaptor/pytorch_adaptor/test_weight_only_adaptor_pytorch.py
+++ test/adaptor/pytorch_adaptor/test_weight_only_adaptor_pytorch.py
@@ -752,9 +752,7 @@ class TestPytorchWeightOnlyAdaptor(unittest.TestCase):
         tokenizer = transformers.AutoTokenizer.from_pretrained(
             "hf-internal-testing/tiny-random-GPTJForCausalLM", trust_remote_code=True
         )
-        dataloader = get_dataloader(
-            tokenizer, seqlen=10, seed=42, train_bs=8, dataset_split="train", dataset_name="NeelNanda/pile-10k"
-        )
+        dataloader = get_dataloader(tokenizer, 32, dataset_name="NeelNanda/pile-10k", seed=42, bs=8, n_samples=20)
         fp32_model = copy.deepcopy(self.gptj)
         conf = PostTrainingQuantConfig(
             approach="weight_only",
@@ -777,7 +775,7 @@ class TestPytorchWeightOnlyAdaptor(unittest.TestCase):
             recipes={
                 "autoround_args": {
                     "n_samples": 20,
-                    "seq_len": 10,
+                    "seqlen": 32,
                     "iters": 10,
                     "scale_dtype": "fp32",
                     "amp": False,
