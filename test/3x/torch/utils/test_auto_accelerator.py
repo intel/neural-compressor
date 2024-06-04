866fc373e6a651093dbb6b4c0cb6163a0a4e29b3
xinhe3 <xinhe3@habana.ai>
2024-06-04 10:59:52 +0300

    checked on hpu
    Signed-off-by: xinhe3 <xinhe3@habana.ai>

--- test/3x/torch/utils/test_auto_accelerator.py
+++ test/3x/torch/utils/test_auto_accelerator.py
@@ -14,77 +14,63 @@ from neural_compressor.torch.utils.auto_accelerator import (
 )
 
 
-@pytest.mark.skipif(not HPU_Accelerator.is_available(), reason="CUDA is not available")
+@pytest.mark.skipif(not HPU_Accelerator.is_available(), reason="HPEX is not available")
 class TestHPUAccelerator:
-
     def test_cuda_accelerator(self):
         assert os.environ.get("FORCE_DEVICE", None) is None, "FORCE_DEVICE shouldn't be set. HPU is the first priority."
         accelerator = auto_detect_accelerator()
         assert accelerator.current_device() == 0, f"{accelerator.current_device()}"
-        assert accelerator.current_device_name() == "hpu"
+        assert accelerator.current_device_name() == "hpu:0"
         assert accelerator.device() is not None
-        assert accelerator.empty_cache() is None
-        assert accelerator.synchronize() is None
-        assert accelerator.set_device(0) is None
-        assert accelerator.device_name(0) == "cuda:0"
+        assert accelerator.device_name(0) == "hpu:0"
         assert accelerator.is_available() is True
-        assert accelerator.name() == "cuda"
-        assert accelerator.device_name(1) == "cuda:1"
-        assert accelerator.set_device(1) is None
-        assert accelerator.device_name(1) == "cuda:1"
-        assert accelerator.current_device() == 1
-        assert accelerator.current_device_name() == "cuda:1"
+        assert accelerator.name() == "hpu"
+        assert accelerator.device_name(1) == "hpu:1"
         assert accelerator.synchronize() is None
         assert accelerator.empty_cache() is None
 
-    @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="Only one GPU is available")
+    @pytest.mark.skipif(torch.hpu.device_count() < 2, reason="Only one HPU is available")
     def test_get_device(self):
         accelerator = auto_detect_accelerator()
         assert accelerator.set_device(1) is None
-        assert accelerator.current_device_name() == "cuda:1"
+        assert accelerator.current_device_name() == "hpu:1"
         cur_device = get_accelerator().current_device_name()
-        assert cur_device == "cuda:1"
+        assert cur_device == "hpu:1"
         tmp_tensor = torch.tensor([1, 2], device=cur_device)
-        assert "cuda:1" == str(tmp_tensor.device)
+        assert "hpu:1" == str(tmp_tensor.device)
 
 
-@pytest.mark.skipif(not XPU_Accelerator.is_available(), reason="CUDA is not available")
+@pytest.mark.skipif(not XPU_Accelerator.is_available(), reason="XPU is not available")
 class TestXPUAccelerator:
 
     @pytest.fixture
-    def force_use_cuda(self, monkeypatch):
-        # Force use CUDA
-        monkeypatch.setenv("FORCE_DEVICE", "cuda")
+    def force_use_xpu(self, monkeypatch):
+        # Force use xpu
+        monkeypatch.setenv("FORCE_DEVICE", "xpu")
 
-    def test_cuda_accelerator(self, force_use_cuda):
+    def test_xpu_accelerator(self, force_use_xpu):
         print(f"FORCE_DEVICE: {os.environ.get('FORCE_DEVICE', None)}")
         accelerator = auto_detect_accelerator()
         assert accelerator.current_device() == 0, f"{accelerator.current_device()}"
-        assert accelerator.current_device_name() == "cuda:0"
+        assert accelerator.current_device_name() == "xpu:0"
         assert accelerator.device() is not None
-        assert accelerator.empty_cache() is None
-        assert accelerator.synchronize() is None
         assert accelerator.set_device(0) is None
-        assert accelerator.device_name(0) == "cuda:0"
+        assert accelerator.device_name(0) == "xpu:0"
         assert accelerator.is_available() is True
-        assert accelerator.name() == "cuda"
-        assert accelerator.device_name(1) == "cuda:1"
-        assert accelerator.set_device(1) is None
-        assert accelerator.device_name(1) == "cuda:1"
-        assert accelerator.current_device() == 1
-        assert accelerator.current_device_name() == "cuda:1"
+        assert accelerator.name() == "xpu"
+        assert accelerator.device_name(1) == "xpu:1"
         assert accelerator.synchronize() is None
         assert accelerator.empty_cache() is None
 
-    @pytest.mark.skipif(torch.cuda.device_count() < 2, reason="Only one GPU is available")
+    @pytest.mark.skipif(torch.xpu.device_count() < 2, reason="Only one GPU is available")
     def test_get_device(self):
         accelerator = auto_detect_accelerator()
         assert accelerator.set_device(1) is None
-        assert accelerator.current_device_name() == "cuda:1"
+        assert accelerator.current_device_name() == "xpu:1"
         cur_device = get_accelerator().current_device_name()
-        assert cur_device == "cuda:1"
+        assert cur_device == "xpu:1"
         tmp_tensor = torch.tensor([1, 2], device=cur_device)
-        assert "cuda:1" == str(tmp_tensor.device)
+        assert "xpu:1" == str(tmp_tensor.device)
 
 
 class TestCPUAccelerator:
@@ -119,17 +105,11 @@ class TestCUDAAccelerator:
         assert accelerator.current_device() == 0, f"{accelerator.current_device()}"
         assert accelerator.current_device_name() == "cuda:0"
         assert accelerator.device() is not None
-        assert accelerator.empty_cache() is None
-        assert accelerator.synchronize() is None
         assert accelerator.set_device(0) is None
         assert accelerator.device_name(0) == "cuda:0"
         assert accelerator.is_available() is True
         assert accelerator.name() == "cuda"
         assert accelerator.device_name(1) == "cuda:1"
-        assert accelerator.set_device(1) is None
-        assert accelerator.device_name(1) == "cuda:1"
-        assert accelerator.current_device() == 1
-        assert accelerator.current_device_name() == "cuda:1"
         assert accelerator.synchronize() is None
         assert accelerator.empty_cache() is None
 
