98301d5e254fdbf43146213796ac77da135d12ea
xin3he <xin3.he@intel.com>
2024-06-05 10:21:33 +0800

    fix bug
    Signed-off-by: xin3he <xin3.he@intel.com>

--- test/3x/torch/quantization/weight_only/test_rtn.py
+++ test/3x/torch/quantization/weight_only/test_rtn.py
@@ -76,7 +76,7 @@ class TestRTNQuant:
         model = convert(model)
         out = model(self.example_inputs)[0]
         assert (out != self.label).any(), "WOQ output should be different with raw output"
-        if is_hpex_available:
+        if is_hpex_available():
             assert "hpu" in out.device, "Neural Compressor should run on HPU when HPEX is available."
         if (bits, use_sym, group_size, group_dim) == (8, True, -1, 1):
             assert torch.allclose(out, self.label, atol=0.01), "Accuracy gap atol > 0.01 is unexpected."
