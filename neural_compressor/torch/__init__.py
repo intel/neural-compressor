0ea77fd1ffdbf184abe7521a35f753079fb7bb54
Kaihui-intel <kaihui.tang@intel.com>
2024-07-16 14:02:57 +0800

    enhance import&add pack ut
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- neural_compressor/torch/__init__.py
+++ neural_compressor/torch/__init__.py
@@ -11,3 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from neural_compressor.torch.algorithms.layer_wise import load_empty_model
