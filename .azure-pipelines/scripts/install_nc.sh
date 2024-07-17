ff3740146a829e845d79266acf233b202843d3fd
chen, suyue <suyue.chen@intel.com>
2024-07-17 23:11:15 +0800

    3.X API installation update (#1935)
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/install_nc.sh
+++ .azure-pipelines/scripts/install_nc.sh
@@ -10,10 +10,6 @@ elif [[ $1 = *"3x_tf"* ]]; then
     python -m pip install --no-cache-dir -r requirements_tf.txt
     python setup.py tf bdist_wheel
     pip install dist/neural_compressor*.whl --force-reinstall
-elif [[ $1 = *"3x_ort" ]]; then
-    python -m pip install --no-cache-dir -r requirements_ort.txt
-    python setup.py ort bdist_wheel
-    pip install dist/neural_compressor*.whl --force-reinstall
 else
     python -m pip install --no-cache-dir -r requirements.txt
     python setup.py bdist_wheel
