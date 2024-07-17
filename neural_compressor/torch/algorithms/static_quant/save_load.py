110f6c46a1c66c77e47e8d179b9c9bda0b81c47d
violetch24 <zixuan@aia-sdp-spr-117706.jf.intel.com>
2024-07-16 23:15:59 -0700

    fix ci error
    Signed-off-by: violetch24 <zixuan@aia-sdp-spr-117706.jf.intel.com>

--- neural_compressor/torch/algorithms/static_quant/save_load.py
+++ neural_compressor/torch/algorithms/static_quant/save_load.py
@@ -32,7 +32,8 @@ def save(model, output_dir="./saved_results"):
 
     qmodel_file_path = os.path.join(os.path.abspath(os.path.expanduser(output_dir)), WEIGHT_NAME)
     qconfig_file_path = os.path.join(os.path.abspath(os.path.expanduser(output_dir)), QCONFIG_NAME)
-    if next(model.parameters()).device.type == "cpu":
+    device = next(model.parameters(), None).device.type if next(model.parameters(), None) else "cpu"
+    if device == "cpu":
         model.ori_save(qmodel_file_path)
         with open(qconfig_file_path, "w") as f:
             json.dump(model.tune_cfg, f, indent=4)
