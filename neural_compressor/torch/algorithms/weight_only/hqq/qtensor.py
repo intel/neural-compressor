94cc9e6d8f294bfb04a5ae42662497cde7b01d6a
yiliu30 <yi4.liu@intel.com>
2024-07-19 10:50:15 +0800

    add docstring for hqq
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- neural_compressor/torch/algorithms/weight_only/hqq/qtensor.py
+++ neural_compressor/torch/algorithms/weight_only/hqq/qtensor.py
@@ -11,9 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""QTensor for HQQ."""
 
 from dataclasses import asdict, dataclass
-from typing import Tuple, Union
+from typing import Optional, Tuple, Union
 
 import torch
 
@@ -25,6 +26,16 @@ __all__ = [
 
 @dataclass
 class QTensorMetaInfo:
+    """Represents the meta information of a quantized tensor.
+
+    Attributes:
+        nbits (int): The number of bits used for quantization.
+        group_size (int): The size of the quantization group.
+        shape (Tuple): The shape of the tensor.
+        axis (int): The axis along which the tensor is quantized.
+        packing (bool): Indicates whether the tensor is packed.
+    """
+
     nbits: int
     group_size: int
     shape: Tuple
@@ -32,34 +43,45 @@ class QTensorMetaInfo:
     packing: bool
 
     def to_dict(self):
+        """Converts the QTensorMetaInfo object to a dictionary.
+
+        Returns:
+            dict: A dictionary representation of the QTensorMetaInfo object.
+        """
         return asdict(self)
 
 
 class QTensor:
-    val: torch.Tensor
-    scale: Union[torch.Tensor, "QTensor"] = None
-    zero: Union[torch.Tensor, "QTensor"] = None
-    meta_info: QTensorMetaInfo = None
-    """
-    val: torch.Tensor
-    scale:
+    """Represents a quantized tensor.
+
+    Example:
         val: torch.Tensor
-        scale: torch.Tensor
-        zero: torch.Tensor
-    zero:
-        torch.Tensor
+        scale:
+            val: torch.Tensor
+            scale: torch.Tensor
+            zero: torch.Tensor
+        zero:
+            torch.Tensor
     """
 
+    val: torch.Tensor
+    scale: Union[None, torch.Tensor, "QTensor"] = None
+    zero: Union[None, torch.Tensor, "QTensor"] = None
+    meta_info: Optional[QTensorMetaInfo] = None
+
     def __init__(self, val, scale=None, zero=None, meta_info=None):
+        """Init a QTensor object."""
         self.val = val
         self.scale = scale
         self.zero = zero
         self.meta_info = meta_info
 
     def is_scale_quantized(self) -> bool:
+        """Check if the scale is quantized."""
         return isinstance(self.scale, QTensor)
 
     def is_zero_quantized(self) -> bool:
+        """Check if the zero is quantized."""
         return isinstance(self.zero, QTensor)
 
     def _get_scale_repr(self) -> str:
@@ -89,6 +111,7 @@ class QTensor:
             return self.zero.__repr__() + "\n"
 
     def __repr__(self) -> str:
+        """Return the string representation of the QTensor object."""
         # TODO: refine it later
         return (
             f"QTensor(\n"
@@ -101,12 +124,14 @@ class QTensor:
         )
 
     def to(self, *args, **kwargs):
+        """Move the QTensor object to a new device or new dtype."""
         self.val = self.val.to(*args, **kwargs)
         self.scale = self.scale.to(*args, **kwargs)
         self.zero = self.zero.to(*args, **kwargs)
         return self
 
     def half(self):
+        """Convert the QTensor object to half precision."""
         # TODO: refine it later
         if self.val.dtype == torch.float32:
             self.val = self.val.half()
@@ -117,6 +142,7 @@ class QTensor:
         return self
 
     def to_state_dict(self):
+        """Convert the QTensor object to a state dictionary for serialization."""
         state = {}
         state["val"] = self.val
         state["meta_info"] = self.meta_info.to_dict()
