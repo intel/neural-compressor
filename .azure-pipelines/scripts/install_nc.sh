74e9381213e931524f25080773d7644c7f739bc9
chensuyue <suyue.chen@intel.com>
2024-07-28 15:40:51 +0800

    install torch cpu
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/install_nc.sh
+++ .azure-pipelines/scripts/install_nc.sh
@@ -3,12 +3,13 @@
 echo -e "\n Install Neural Compressor ... "
 cd /neural-compressor
 if [[ $1 = *"3x_pt"* ]]; then
+    if [[ $1 != *"3x_pt_fp8"* ]]; then
+        echo -e "\n Install torch CPU ... "
+        pip install torch==2.3.0 --index-url https://download.pytorch.org/whl/cpu
+    fi
     python -m pip install --no-cache-dir -r requirements_pt.txt
     python setup.py pt bdist_wheel
     pip install --no-deps dist/neural_compressor*.whl --force-reinstall
-    if [[ $1 != *"3x_pt_fp8"* ]]; then
-        pip install torch==2.3.0 --index-url https://download.pytorch.org/whl/cpu
-    fi
 elif [[ $1 = *"3x_tf"* ]]; then
     python -m pip install --no-cache-dir -r requirements_tf.txt
     python setup.py tf bdist_wheel
