a5b6ef802a1ffddda581f5df452d2cecd2534171
Uri Livne <ulivne@habana.ai>
2024-07-28 13:34:04 +0300

    [SW-184689] Adjust correct condition for one step flow
    align to 1.17
    
    Change-Id: I588680b463a9f8304d95863306b6d5b2503e6e62

--- neural_compressor/torch/algorithms/fp8_quant/fp8_quant.py
+++ neural_compressor/torch/algorithms/fp8_quant/fp8_quant.py
@@ -38,8 +38,8 @@ class FP8Quantizer(Quantizer):
         return model
 
     def convert(self, model):
-        if getattr(model, "prepared", False) and with_patched_module(model):  # if model was calibrated on hpu
-            finish_measurements(model)
+        if with_patched_module(model): # if model was calibrated on hpu
+            finish_measurements(model) # dump the measurements into files to be loaded in _convert
             # for INC flow, it calls `prepare` and then `convert` user-facing API in one run
             restore_patched_module(model)
         _convert(model, self.quant_config)
