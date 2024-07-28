4454f6f64ebd0ac4bf0cb67581f7ef299d3b0093
chensuyue <suyue.chen@intel.com>
2024-07-28 14:27:07 +0800

    freeze torch vision
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/install_nc.sh
+++ .azure-pipelines/scripts/install_nc.sh
@@ -7,7 +7,7 @@ if [[ $1 = *"3x_pt"* ]]; then
     python setup.py pt bdist_wheel
     pip install --no-deps dist/neural_compressor*.whl --force-reinstall
     if [[ $1 != *"3x_pt_fp8"* ]]; then
-        pip install torch --index-url https://download.pytorch.org/whl/cpu
+        pip install torch==2.3.0 --index-url https://download.pytorch.org/whl/cpu
     fi
 elif [[ $1 = *"3x_tf"* ]]; then
     python -m pip install --no-cache-dir -r requirements_tf.txt
