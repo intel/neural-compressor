41244d3bc65fd646d2d6d88ca2c6686f2ab65bc6
chen, suyue <suyue.chen@intel.com>
2024-07-29 23:05:36 +0800

    new previous results could not find all raise issues in CI model test (#1958)
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/models/generate_report.sh
+++ .azure-pipelines/scripts/models/generate_report.sh
@@ -245,13 +245,9 @@ function generate_html_core {
               if((new_result == nan && previous_result == nan) || new_result == "unknown"){
                     printf("<td class=\"col-cell col-cell3\" colspan=2></td>");
               } else{
-                  if(new_result == nan) {
-                        job_status = "fail"
-                        status_png = "background-color:#FFD2D2";
-                        printf("<td style=\"%s\" colspan=2></td>", status_png);
-                  } else{
-                        printf("<td class=\"col-cell col-cell3\" colspan=2></td>");
-                  }
+                    job_status = "fail"
+                    status_png = "background-color:#FFD2D2";
+                    printf("<td style=\"%s\" colspan=2></td>", status_png);
               }
             }
         }
