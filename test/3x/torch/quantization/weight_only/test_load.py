273787091e987ad6b2f4a7669a081c33730e8d08
yuwenzho <yuwen.zhou@intel.com>
2024-07-30 09:39:50 +0000

    enhance ut
    Signed-off-by: yuwenzho <yuwen.zhou@intel.com>

--- test/3x/torch/quantization/weight_only/test_load.py
+++ test/3x/torch/quantization/weight_only/test_load.py
@@ -16,13 +16,16 @@ class TestHFModelLoad:
     def setup_class(self):
         self.model_name = "TheBloke/TinyLlama-1.1B-python-v0.1-GPTQ"
         self.example_inputs = torch.tensor([[10, 20, 30, 40, 50, 60]], dtype=torch.long).to(device)
+        self.local_cache = "local_cache"
 
-        self.local_hf_model = "./TinyLlama-1.1B-python-v0.1-GPTQ"
+        self.local_hf_model = "TinyLlama-1.1B-Chat-v0.1-GPTQ"
         huggingface_hub.snapshot_download(self.model_name, local_dir=self.local_hf_model)
 
     def teardown_class(self):
         shutil.rmtree("TinyLlama-1.1B-python-v0.1-GPTQ", ignore_errors=True)
         shutil.rmtree("saved_results", ignore_errors=True)
+        shutil.rmtree("nc_workspace", ignore_errors=True)
+        shutil.rmtree("local_cache", ignore_errors=True)
 
     def get_woq_linear_num(self, model, woq_module_type_name):
         woq_linear_num = 0
@@ -54,12 +57,14 @@ class TestHFModelLoad:
 
     @pytest.mark.skipif(not is_hpex_available(), reason="no hpex in environment here.")
     def test_load_hf_woq_model_hpu(self):
-        # 1. use huggingface model_id (format=huggingface, device="hpu")
+        # use huggingface model_id (format=huggingface, device="hpu")
         # first load: linear -> INCWeightOnlyLinear -> HPUWeightOnlyLinear, save hpu_model.safetensors to local cache dir
         model = load(
             model_name_or_path=self.model_name,
             format="huggingface",
             device="hpu",
+            torch_dtype=torch.bfloat16,
+            cache_dir=self.local_cache,
         )
         assert (
             self.get_woq_linear_num(model, "HPUWeightOnlyLinear") == 154
@@ -71,33 +76,8 @@ class TestHFModelLoad:
             model_name_or_path=self.model_name,
             format="huggingface",
             device="hpu",
-        )
-        assert (
-            self.get_woq_linear_num(model, "HPUWeightOnlyLinear") == 154
-        ), "Incorrect number of HPUWeightOnlyLinear modules"
-        output2 = model(self.example_inputs)[0]
-
-        assert torch.equal(
-            output1, output2
-        ), "The model loaded the second time is different from the model loaded the first time"
-
-        # 2. use huggingface local model_path (format=huggingface, device="hpu")
-        # first load: linear -> INCWeightOnlyLinear -> HPUWeightOnlyLinear, save hpu_model.safetensors to local cache dir
-        model = load(
-            model_name_or_path=self.local_hf_model,
-            format="huggingface",
-            device="hpu",
-        )
-        assert (
-            self.get_woq_linear_num(model, "HPUWeightOnlyLinear") == 154
-        ), "Incorrect number of HPUWeightOnlyLinear modules"
-        output1 = model(self.example_inputs)[0]
-
-        # second load: linear -> HPUWeightOnlyLinear using hpu_model.safetensors saved in local cache dir
-        model = load(
-            model_name_or_path=self.local_hf_model,
-            format="huggingface",
-            device="hpu",
+            torch_dtype=torch.bfloat16,
+            cache_dir=self.local_cache,
         )
         assert (
             self.get_woq_linear_num(model, "HPUWeightOnlyLinear") == 154
