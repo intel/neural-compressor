611982e754ea11736a9392aa849cabcf78847a9d
Kaihui-intel <kaihui.tang@intel.com>
2024-08-16 13:16:37 +0800

    add print trigger
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- test/3x/torch/quantization/weight_only/test_rtn.py
+++ test/3x/torch/quantization/weight_only/test_rtn.py
@@ -174,6 +174,7 @@ class TestRTNQuant:
         ), "The tied lm_head weight is not deep copied, please check!"
 
     def test_layer_wise(self):
+        print("test PR CI layer wise gap")
         from neural_compressor.torch import load_empty_model
 
         model = load_empty_model("hf-internal-testing/tiny-random-GPTJForCausalLM")
