9b963e6577ed255e978022921fe00e1f3c39d6d4
yan tomsinsky <ytomsinsky@habana.ai>
2024-08-21 15:20:06 +0300

    [SW-180042] Change HQT HF8_143 max range
    Change-Id: Ie34d80ea536b7b01b38435fe48203c88a3442e37

--- neural_compressor/torch/algorithms/fp8_quant/_core/scale_methods/smooth_quant.py
+++ neural_compressor/torch/algorithms/fp8_quant/_core/scale_methods/smooth_quant.py
@@ -23,7 +23,7 @@ def linear_smoothquant_weights_opt_pow2_scales(mod, measurement, params):
     device = torch.device("hpu")
     lp_dtype = params["lp_dtype"]
     hp_dtype = params["hp_dtype"]
-    fullscale = MAX_RANGE[lp_dtype]
+    fullscale = get_fullscale(lp_dtype, device)
     input_backoff = params["input_backoff"]
     weight_backoff = params["weight_backoff"]
     alpha = params["alpha"]
@@ -60,7 +60,7 @@ def linear_smoothquant_weights_maxabs_pow2_scales(mod, measurement, params):
     device = torch.device("hpu")
     lp_dtype = params["lp_dtype"]
     hp_dtype = params["hp_dtype"]
-    fullscale = MAX_RANGE[lp_dtype]
+    fullscale = get_fullscale(lp_dtype, device)
     input_backoff = params["input_backoff"]
     weight_backoff = params["weight_backoff"]
     alpha = params["alpha"]
@@ -92,7 +92,7 @@ def linear_weaksmoothquant_weights_maxabs_pow2_scales(mod, measurement, params):
     device = torch.device("hpu")
     lp_dtype = params["lp_dtype"]
     hp_dtype = params["hp_dtype"]
-    fullscale = MAX_RANGE[lp_dtype]
+    fullscale = get_fullscale(lp_dtype, device)
     input_backoff = params["input_backoff"]
     weight_backoff = params["weight_backoff"]
     alpha = params["alpha"]
