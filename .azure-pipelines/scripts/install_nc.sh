f2c454f88c0ffbb4d30d66eedaa6fc56ad47f804
chen, suyue <suyue.chen@intel.com>
2024-08-29 13:45:39 +0800

    update installation and ci test for 3x api (#1991)
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .azure-pipelines/scripts/install_nc.sh
+++ .azure-pipelines/scripts/install_nc.sh
@@ -3,17 +3,20 @@
 echo -e "\n Install Neural Compressor ... "
 cd /neural-compressor
 if [[ $1 = *"3x_pt"* ]]; then
-    if [[ $1 != *"3x_pt_fp8"* ]]; then
+    python -m pip install --no-cache-dir -r requirements_pt.txt
+    if [[ $1 = *"3x_pt_fp8"* ]]; then
+        pip uninstall neural_compressor_3x_pt -y || true
+        python setup.py pt bdist_wheel
+    else
         echo -e "\n Install torch CPU ... "
         pip install torch==2.4.0 --index-url https://download.pytorch.org/whl/cpu
+        python -m pip install --no-cache-dir -r requirements.txt
+        python setup.py bdist_wheel
     fi
-    python -m pip install --no-cache-dir -r requirements_pt.txt
-    # python setup.py pt bdist_wheel
-    python setup.py bdist_wheel
     pip install --no-deps dist/neural_compressor*.whl --force-reinstall
 elif [[ $1 = *"3x_tf"* ]]; then
+    python -m pip install --no-cache-dir -r requirements.txt
     python -m pip install --no-cache-dir -r requirements_tf.txt
-    # python setup.py tf bdist_wheel
     python setup.py bdist_wheel
     pip install dist/neural_compressor*.whl --force-reinstall
 else
