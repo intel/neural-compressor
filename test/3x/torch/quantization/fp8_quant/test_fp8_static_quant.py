419bb2281db5062618b2e3d4801cf88840a1a512
xinhe3 <xinhe3@habana.ai>
2024-09-06 09:22:40 +0300

    [SW-199823] [HQT] fix INC one-step quantization API workflow
    1. fix test_fp8_static_quant.py::TestFP8StaticQuant::test_one_step_quant_cv failure by deepcoping forward function in common.py
    2. fix config.py: Object of type dict_keys is not JSON serializable by converting it to list
    3. fix download issue of UT by using local tiny_gptj.json
    
    Change-Id: I2ad3eac411e8fca9d88a021f6a5b9594e6c75ae9
    Signed-off-by: xinhe3 <xinhe3@habana.ai>

--- test/3x/torch/quantization/fp8_quant/test_fp8_static_quant.py
+++ test/3x/torch/quantization/fp8_quant/test_fp8_static_quant.py
@@ -36,12 +36,11 @@ def calib_func(model):
 @pytest.mark.skipif(not is_hpex_available(), reason="HPU environment is required!")
 class TestFP8StaticQuant:
     def setup_class(self):
-        self.tiny_gptj = transformers.AutoModelForCausalLM.from_pretrained(
-            "hf-internal-testing/tiny-random-GPTJForCausalLM",
-            device_map="cpu",
-        )
+        change_to_cur_file_dir()
+        config = transformers.AutoConfig.from_pretrained("./model_configs/tiny_gptj.json")
+        self.tiny_gptj = transformers.AutoModelForCausalLM.from_config(config)
         self.example_inputs = torch.tensor([[10, 20, 30, 40, 50, 60]], dtype=torch.long).to("hpu")
-        self.resnet18 = torchvision.models.resnet18(pretrained=True)
+        self.resnet18 = torchvision.models.resnet18()
         self.cv_dummy_inputs = torch.randn([1, 3, 224, 224]).to("hpu")
 
     def teardown_class(self):
@@ -88,7 +87,6 @@ class TestFP8StaticQuant:
         assert (fp32_out != fp8_out).any(), "FP32 output should be different with FP8 output"
 
     def test_two_step_quant_nlp(self):
-        change_to_cur_file_dir()
         # step 1: measurement
         model = copy.deepcopy(self.tiny_gptj)
         config = FP8Config.from_json_file("test_fp8_jsons/test_measure.json")
@@ -106,7 +104,6 @@ class TestFP8StaticQuant:
         ), "k_proj input dtype is not torch.float8_e4m3fn."
 
     def test_two_step_quant_cv(self):
-        change_to_cur_file_dir()
         # step 1: measurement
         model = copy.deepcopy(self.resnet18)
         config = FP8Config.from_json_file("test_fp8_jsons/test_measure.json")
