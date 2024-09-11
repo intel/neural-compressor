04bd8664588dc0c64b73d0f7510b4dd691c51a55
Roi Tiefenbrunn <rtiefenbrunn@habana.ai>
2024-09-11 15:38:18 +0300

    [SW-198220] Fix bug with scale_to_scalar with dim != 0
    Change the test to numel() == 1 instead of dim() == 0
    
    Change-Id: I510aa2cc8f04e30d4c5346040ed0611eaa407cf4

--- neural_compressor/torch/algorithms/fp8_quant/_core/scale_handler.py
+++ neural_compressor/torch/algorithms/fp8_quant/_core/scale_handler.py
@@ -1,6 +1,7 @@
 from .._quant_common.quant_config import ScaleFormat
 from torch import Tensor, nn
 
+
 def create_scale_tensor(orig_tensor, scale_format):
     if scale_format == ScaleFormat.CONST:
         return nn.Parameter(orig_tensor)
@@ -9,14 +10,15 @@ def create_scale_tensor(orig_tensor, scale_format):
     else:
         raise ValueError("unexpected scale format value {}".format(scale_format))
 
+
 # scalar scale is a performance optimization for LLM layers in small BS
 def scale_to_scalar(scale):
-    if isinstance(scale, Tensor): # tensor case
-        if scale.dim() == 0:
+    if isinstance(scale, Tensor):  # tensor case
+        if scale.numel() == 1:
             return scale.item()
-        else :
+        else:
             raise Exception("scale as scalar isn't supported for scale tensors of dim > 0")
-    elif isinstance(scale, float): # already scalar case
+    elif isinstance(scale, float):  # already scalar case
         return scale
     else:
         raise Exception("unexpected scale instance type, expected Torch.tensor or float number")
