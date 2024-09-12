2b16b7aef8ad756b118e77d1051bf47b59445cbb
Kaihui-intel <kaihui.tang@intel.com>
2024-09-12 11:01:53 +0800

    clean utility.py
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- neural_compressor/transformers/utils/utility.py
+++ neural_compressor/transformers/utils/utility.py
@@ -21,19 +21,12 @@ import os
 
 from neural_compressor.common.utils import CpuInfo, LazyImport, logger
 
-CONFIG_NAME = "best_configure.yaml"
-ENGINE_MODEL_NAME = "model.bin"
-ENGINE_MODEL_CONFIG = "conf.yaml"
-ENCODER_NAME = "encoder_model.bin"
-DECODER_NAME = "decoder_model.bin"
-DECODER_WITH_PAST_NAME = "decoder_with_past_model.bin"
+
 WEIGHTS_NAME = "pytorch_model.bin"
 WEIGHTS_INDEX_NAME = "pytorch_model.bin.index.json"
 QUANT_CONFIG = "quantize_config.json"
-SPARSITY_CONFIG = "sparsity_config.json"
 SAFE_WEIGHTS_NAME = "model.safetensors"
 SAFE_WEIGHTS_INDEX_NAME = "model.safetensors.index.json"
-"""Utility."""
 
 import importlib
 import sys
@@ -90,20 +83,6 @@ def is_autoround_available():
     return _autoround_available
 
 
-_neural_compressor_available = importlib.util.find_spec("neural_compressor") is not None
-_neural_compressor_version = "N/A"
-
-if _neural_compressor_available:
-    try:
-        _neural_compressor_version = importlib_metadata.version("neural_compressor")
-    except importlib_metadata.PackageNotFoundError:
-        _neural_compressor_available = False
-
-
-def is_neural_compressor_avaliable():
-    return _neural_compressor_available
-
-
 def get_device_type():
     if torch.cuda.is_available():
         device = "cuda"
@@ -114,56 +93,3 @@ def get_device_type():
     else:
         device = "cpu"
     return device
-
-
-# utility
-
-
-if is_ipex_available():
-    import intel_extension_for_pytorch as ipex
-torch = LazyImport("torch")
-
-
-def distributed_init(
-    backend="gloo",
-    world_size=1,
-    rank=-1,
-    init_method=None,
-    master_addr="127.0.0.1",
-    master_port="12345",
-):
-    """Init the distribute environment."""
-    rank = int(os.environ.get("RANK", rank))
-    world_size = int(os.environ.get("WORLD_SIZE", world_size))
-    if init_method is None:
-        master_addr = os.environ.get("MASTER_ADDR", master_addr)
-        master_port = os.environ.get("MASTER_PORT", master_port)
-        init_method = "env://{addr}:{port}".format(addr=master_addr, port=master_port)
-    torch.distributed.init_process_group(backend, init_method=init_method, world_size=world_size, rank=rank)
-
-
-def remove_label(input):
-    if "labels" in input:  # for GLUE
-        input.pop("labels")
-    elif "start_positions" in input and "end_positions" in input:  # for SQuAD
-        # pragma: no cover
-        input.pop("start_positions")
-        input.pop("end_positions")
-    return input
-
-
-def _build_inc_dataloader(dataloader):
-    # transformer issue #1
-    # for transformers 4.31.0: accelerate dataloader
-    # *** ValueError: batch_size attribute should not be set
-    # after DataLoaderShard is initialized
-    class INCDataLoader:
-        __iter__ = dataloader.__iter__
-        __len__ = dataloader.__len__
-
-        def __init__(self) -> None:
-            self.dataloader = dataloader
-            self.batch_size = dataloader.total_batch_size
-            self.dataset = dataloader.dataset
-
-    return INCDataLoader()
