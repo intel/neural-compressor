7feb0e110095df37bc0c2ec2ba6e8b6c4b5cee12
Sun, Xuehao <xuehao.sun@intel.com>
2024-10-18 17:42:54 +0800

    remove autoround limit
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/env_setup.sh
+++ .azure-pipelines/scripts/ut/env_setup.sh
@@ -92,7 +92,7 @@ elif [[ $(echo "${test_case}" | grep -c "tf pruning") != 0 ]]; then
 fi
 
 if [[ $(echo "${test_case}" | grep -c "api") != 0 ]] || [[ $(echo "${test_case}" | grep -c "adaptor") != 0 ]]; then
-    pip install git+https://github.com/intel/auto-round.git@5dd16fc34a974a8c2f5a4288ce72e61ec3b1410f
+    pip install auto-round
 fi
 
 # test deps
