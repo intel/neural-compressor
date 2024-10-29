e709ffd8a924d106272b4e4268476f1366228374
Kaihui-intel <kaihui.tang@intel.com>
2024-10-29 14:04:16 +0800

    fix non contiguous weight saving
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- neural_compressor/transformers/quantization/utils.py
+++ neural_compressor/transformers/quantization/utils.py
@@ -591,6 +591,10 @@ def convert_to_GPTQ_checkpoints(model, quantization_config):
             set_module(model, name, new_module)
     return model
 
+def make_contiguous(model):
+    for param in model.parameters():
+        if param.data.ndimension() > 1:
+            param.data = param.data.contiguous()
 
 def save_low_bit(self, save_directory: Union[str, os.PathLike], push_to_hub: bool = False, **kwargs):
 
@@ -603,6 +607,7 @@ def save_low_bit(self, save_directory: Union[str, os.PathLike], push_to_hub: boo
     os.makedirs(save_directory, exist_ok=True)
     # use transformers original `save_pretrained` function
     del self.save_pretrained
+    make_contiguous(self)
 
     if self.device == "cpu" or self.device == torch.device("cpu"):
         convert_to_GPTQ_checkpoints(self, self.quantization_config)
