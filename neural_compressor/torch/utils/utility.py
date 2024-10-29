52506ef875ed74832c09bc7be31e5f945eeeb5da
Kaihui-intel <kaihui.tang@intel.com>
2024-10-29 17:24:35 +0800

    update downloading file
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- neural_compressor/torch/utils/utility.py
+++ neural_compressor/torch/utils/utility.py
@@ -298,7 +298,7 @@ def get_processor_type_from_user_config(user_processor_type: Optional[Union[str,
     return processor_type
 
 
-def dowload_hf_model(repo_id, cache_dir=None, repo_type=None, revision=None, download_weight=False):
+def dowload_hf_model(repo_id, cache_dir=None, repo_type=None, revision=None):
     """Download hugging face model from hf hub."""
     import os
 
@@ -323,18 +323,22 @@ def dowload_hf_model(repo_id, cache_dir=None, repo_type=None, revision=None, dow
                 commit_hash = f.read()
     if storage_folder and commit_hash:
         pointer_path = os.path.join(storage_folder, "snapshots", commit_hash)
-        if os.path.isdir(pointer_path) and (
-            not download_weight
-            or any(file.endswith(".bin") or file.endswith(".safetensors") for file in os.listdir(pointer_path))
-        ):
+        if os.path.isdir(pointer_path) and \
+            any(file.endswith(".bin") or file.endswith(".safetensors") for file in os.listdir(pointer_path)):
             return pointer_path
-    from huggingface_hub import snapshot_download
-
-    file_path = snapshot_download(repo_id)
+    from huggingface_hub import  list_repo_files, snapshot_download
+    
+    files_info = list_repo_files("meta-llama/Llama-2-7b-hf")
+    ignore_patterns=["*.bin", "*.bin.index.json"] \
+        if (any(file for file in files_info if file.endswith('.bin')) and \
+            any(file for file in files_info if file.endswith('.safetensors'))) \
+        else None 
+    
+    file_path = snapshot_download(repo_id, ignore_patterns=ignore_patterns)
     return file_path
 
 
-def load_empty_model(pretrained_model_name_or_path, cls=None, download_weight=False, **kwargs):
+def load_empty_model(pretrained_model_name_or_path, cls=None, **kwargs):
     """Load a empty model."""
     import os
 
@@ -347,7 +351,7 @@ def load_empty_model(pretrained_model_name_or_path, cls=None, download_weight=Fa
     if is_local:  # pragma: no cover
         path = pretrained_model_name_or_path
     else:
-        path = dowload_hf_model(pretrained_model_name_or_path, download_weight=download_weight)
+        path = dowload_hf_model(pretrained_model_name_or_path)
     if cls.__base__ == _BaseAutoModelClass:
         config = AutoConfig.from_pretrained(path, **kwargs)
         with init_empty_weights():
