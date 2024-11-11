5028ed3b2c465af8d3373ecbbd46e97d543ff381
Kaihui-intel <kaihui.tang@intel.com>
2024-11-11 15:04:19 +0800

    Support client `use_layer_wise` setting (#2048)
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- neural_compressor/transformers/models/modeling_auto.py
+++ neural_compressor/transformers/models/modeling_auto.py
@@ -135,12 +135,11 @@ class _BaseINCAutoModelClass:
         ):
             logger.info("Applying Weight Only Quantization.")
             # set use_layer_wise on client
-            if hasattr(quantization_config, "use_layer_wise"):
+            if hasattr(quantization_config, "use_layer_wise") and quantization_config.use_layer_wise is None:
                 import neural_compressor.torch.utils as torch_utils
 
                 process_type = torch_utils.get_processor_type_from_user_config()
-                if process_type == torch_utils.ProcessorType.Client:
-                    quantization_config.use_layer_wise = True
+                quantization_config.use_layer_wise = process_type == torch_utils.ProcessorType.Client
 
             if hasattr(quantization_config, "use_layer_wise") and quantization_config.use_layer_wise:
                 from transformers.dynamic_module_utils import resolve_trust_remote_code
