17059fb398d73eb80457ced0a5b8a0f88d28a7af
Kaihui-intel <kaihui.tang@intel.com>
2024-11-14 16:11:35 +0800

    fix pt2e/static_quant/gptq
    Signed-off-by: Kaihui-intel <kaihui.tang@intel.com>

--- test/3x/torch/quantization/weight_only/test_transfomers.py
+++ test/3x/torch/quantization/weight_only/test_transfomers.py
@@ -13,7 +13,10 @@ from neural_compressor.transformers import (
     RtnConfig,
     TeqConfig,
 )
+from neural_compressor.torch.utils import get_ipex_version
+from packaging.version import Version
 
+ipex_version = get_ipex_version()
 
 class TestTansformersLikeAPI:
     def setup_class(self):
@@ -81,8 +84,10 @@ class TestTansformersLikeAPI:
         woq_model = AutoModelForCausalLM.from_pretrained(model_name_or_path, quantization_config=woq_config)
         woq_model.eval()
         output = woq_model(dummy_input)
-        assert isclose(float(output[0][0][0][0]), 0.17234990000724792, rel_tol=1e-04)
-
+        if ipex_version  < Version("2.5.0"):
+            assert isclose(float(output[0][0][0][0]), 0.17234990000724792, rel_tol=1e-04)
+        else:
+            assert isclose(float(output[0][0][0][0]), 0.17049233615398407, rel_tol=1e-04)
         # AUTOROUND
         woq_config = AutoRoundConfig(
             bits=4, weight_dtype="int4_clip", n_samples=128, seq_len=32, iters=5, group_size=16, tokenizer=tokenizer
