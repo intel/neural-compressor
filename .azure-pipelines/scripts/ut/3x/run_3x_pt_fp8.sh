ff53438c5b8b96667e00f047a0cd3783b880ff35
Sun, Xuehao <xuehao.sun@intel.com>
2024-12-11 14:33:36 +0800

    Refactor FP8 pytest script
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/3x/run_3x_pt_fp8.sh
+++ .azure-pipelines/scripts/ut/3x/run_3x_pt_fp8.sh
@@ -24,20 +24,21 @@ pip list
 
 export COVERAGE_RCFILE=/neural-compressor/.azure-pipelines/scripts/ut/3x/coverage.3x_pt_fp8
 inc_path=$(python -c 'import neural_compressor; print(neural_compressor.__path__[0])')
-cd /neural-compressor/test/3x || exit 1
-
 LOG_DIR=/neural-compressor/log_dir
-mkdir -p ${LOG_DIR}
-ut_log_name=${LOG_DIR}/ut_3x_pt_fp8.log
-pytest --cov="${inc_path}" -vs --disable-warnings --html=report_1.html --self-contained-html torch/quantization/weight_only/test_load.py 2>&1 | tee -a ${ut_log_name}
-pytest --cov="${inc_path}" -vs --disable-warnings --html=report_2.html --self-contained-html torch/quantization/weight_only/test_rtn.py 2>&1 | tee -a ${ut_log_name}
-# pytest --cov="${inc_path}" -vs --disable-warnings --html=report_3.html --self-contained-html torch/quantization/weight_only/test_autoround.py 2>&1 | tee -a ${ut_log_name}
-pytest --cov="${inc_path}" -vs --disable-warnings --html=report_4.html --self-contained-html torch/quantization/fp8_quant 2>&1 | tee -a ${ut_log_name}
-pytest --cov="${inc_path}" -vs --disable-warnings --html=report_5.html --self-contained-html torch/algorithms/fp8_quant 2>&1 | tee -a ${ut_log_name}
+HTML_DIR=/neural-compressor/html_dir
+mkdir -p ${LOG_DIR} ${HTML_DIR}
 
-mkdir -p report && mv *.html report
-pytest_html_merger -i ./report -o ./report.html
-cp report.html ${LOG_DIR}/
+cd /neural-compressor/test/3x/torch/algorithms/fp8_quant
+
+ut_log_name=${LOG_DIR}/ut_3x_pt_fp8.log
+pytest --cov="${inc_path}" -vs --disable-warnings --html=report_1.html --self-contained-html /neural-compressor/test/3x/torch/quantization/weight_only/test_load.py 2>&1 | tee -a ${ut_log_name}
+pytest --cov="${inc_path}" -vs --disable-warnings --html=report_2.html --self-contained-html /neural-compressor/test/3x/torch/quantization/weight_only/test_rtn.py 2>&1 | tee -a ${ut_log_name}
+# pytest --cov="${inc_path}" -vs --disable-warnings --html=report_3.html --self-contained-html torch/quantization/weight_only/test_autoround.py 2>&1 | tee -a ${ut_log_name}
+pytest --cov="${inc_path}" -vs --disable-warnings --html=report_4.html --self-contained-html . 2>&1 | tee -a ${ut_log_name}
+pytest --cov="${inc_path}" -vs --disable-warnings --html=report_5.html --self-contained-html . 2>&1 | tee -a ${ut_log_name}
+mv *.html ${HTML_DIR}
+
+pytest_html_merger -i ${HTML_DIR} -o ${LOG_DIR}/report.html
 
 if [ $(grep -c '== FAILURES ==' ${ut_log_name}) != 0 ] || [ $(grep -c '== ERRORS ==' ${ut_log_name}) != 0 ] || [ $(grep -c ' passed' ${ut_log_name}) == 0 ]; then
     echo "Find errors in pytest case, please check the output..."
