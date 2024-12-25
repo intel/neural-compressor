2b0ab7844351ffe3a4d59a32bdc992232f2aa0ce
Wang, Chang <changwang@habana.ai>
2024-12-25 13:30:37 +0800

    [SW-208528] Support FP8 per channel Q/DQ (#13)
    * add per channel qdq support
    
    Signed-off-by: changwang <changwang@habana.ai>
    
    * improve ut
    
    Signed-off-by: changwang <changwang@habana.ai>
    
    * improve get_scale_dtype func and qdq init
    
    Signed-off-by: changwangss <changwang@habana.ai>
    
    * improve DequantOutput QuantInput init
    
    Signed-off-by: changwangss <changwang@habana.ai>
    
    * add scale_method improve PCQ
    
    Signed-off-by: changwangss <changwang@habana.ai>
    
    * remove scale name
    
    Signed-off-by: changwangss <changwang@habana.ai>
    
    * fix PCQ scale_inv expanding
    
    Signed-off-by: changwangss <changwang@habana.ai>
    
    * merge the qdq_per_channel, qdq_per_tensor to qdq
    
    Signed-off-by: changwangss <changwang@habana.ai>
    
    * move scale_inv change to the QuantInput init
    
    Signed-off-by: changwangss <changwang@habana.ai>
    
    * remove  scale_dtype list judge
    
    Signed-off-by: changwangss <changwang@habana.ai>
    
    * fix missing axis parameter
    
    Signed-off-by: changwangss <changwang@habana.ai>
    
    ---------
    
    Signed-off-by: changwang <changwang@habana.ai>
    Signed-off-by: changwangss <changwang@habana.ai>

--- neural_compressor/torch/algorithms/fp8_quant/_core/quant_dequant.py
+++ neural_compressor/torch/algorithms/fp8_quant/_core/quant_dequant.py
@@ -17,21 +17,26 @@ import torch
 from torch.ao.quantization.fx._decomposed import quantized_decomposed_lib
 from abc import abstractmethod
 import habana_frameworks.torch.core as htcore
+import habana_frameworks.torch.utils.experimental as htexp
 from neural_compressor.torch.utils.auto_accelerator import auto_detect_accelerator
 cur_accelerator = auto_detect_accelerator()
 
 from .._core.scale_handler import create_scale_tensor, get_scale_dtype
-from .._quant_common.quant_config import ScaleFormat
+from .._quant_common.quant_config import ScaleFormat, ScaleMethod
 
 descale_fcn = lambda x, scale: torch.mul(x, scale)
 scale_fcn = lambda x, scale: torch.div(x, scale)
 cast_fcn = lambda x, dtype: x.to(dtype=dtype)
 cast_to_fp8_fcn = lambda x, dtype, scale_inv=None: torch.ops.hpu.cast_to_fp8_v2(x, scale_inv, False, False, dtype)[0]
 cast_from_fp8_fcn = lambda x, dtype, scale=None: torch.ops.hpu.cast_from_fp8(x, scale, dtype)
-quant_to_fp8_fcn = lambda x, scale, zero_point, quant_min, quant_max, dtype=None: \
+quantize_per_tensor_to_fp8 = lambda x, scale, zero_point, quant_min, quant_max, dtype=None, axis=None: \
     torch.ops.quantized_decomposed.quantize_per_tensor(x, scale, zero_point, quant_min, quant_max, dtype=dtype)
-dequant_from_fp8_fcn = lambda x, scale, zero_point, quant_min, quant_max, dtype, out_dtype=None: \
+dequantize_per_tensor_from_fp8= lambda x, scale, zero_point, quant_min, quant_max, dtype, out_dtype=None, axis=None: \
     torch.ops.quantized_decomposed.dequantize_per_tensor(x, scale, zero_point, quant_min, quant_max, dtype=dtype, out_dtype=out_dtype)
+quantize_per_channel_to_fp8 = lambda x, scale, zero_point, axis, quant_min, quant_max, dtype=None: \
+    torch.ops.quantized_decomposed.quantize_per_channel(x, scale, zero_point, axis, quant_min, quant_max, dtype=dtype)
+dequantize_per_channel_from_fp8 = lambda x, scale, zero_point, axis, quant_min, quant_max, dtype, out_dtype=None: \
+    torch.ops.quantized_decomposed.dequantize_per_channel(x, scale, zero_point, axis, quant_min, quant_max, dtype=dtype, out_dtype=out_dtype)
 
 
 class QuantDequantBase(nn.Module):
@@ -42,9 +47,16 @@ class QuantDequantBase(nn.Module):
         self.scale_format = kwargs.get("scale_format", ScaleFormat.CONST)
         self.use_qdq = kwargs.get("use_qdq", False)
         if self.use_qdq:
+            self.qdq_init()
+
+    def qdq_init(self):
+        if htexp._get_device_type() == htexp.synDeviceType.synDeviceGaudi2 and self.lp_dtype == torch.float8_e4m3fn:
+            self.quant_min = int(torch.finfo(torch.float8_e4m3fnuz).min)
+            self.quant_max = int(torch.finfo(torch.float8_e4m3fnuz).max)
+        else:
             self.quant_min = int(torch.finfo(self.lp_dtype).min)
             self.quant_max = int(torch.finfo(self.lp_dtype).max)
-            self.forward = self.forward_qdq
+        self.forward = self.forward_qdq
 
     @abstractmethod
     def forward(self, *args, **kwargs):
@@ -73,20 +85,25 @@ class QuantDequantNone(QuantDequantBase):
 class QuantInput(QuantDequantBase):
     def __init__(self, scale_inv, lp_dtype, hp_dtype, *args, **kwargs):
         super(QuantInput, self).__init__(lp_dtype, hp_dtype, *args, **kwargs)
-        self.scale_inv = create_scale_tensor(scale_inv, self.scale_format)
+        self.scale_inv = create_scale_tensor(
+                scale_inv.unsqueeze(1) if (scale_inv.numel() > 1 and not self.use_qdq) else scale_inv,
+                self.scale_format
+            )
         if self.use_qdq:
-            self.scale = create_scale_tensor(1 / scale_inv, self.scale_format)
+            self.scale = create_scale_tensor(1 / self.scale_inv, self.scale_format)
+            self.quantize_op = (
+                quantize_per_channel_to_fp8 if self.scale.numel() > 1 else quantize_per_tensor_to_fp8
+                )
 
     def forward(self, x):
-        # create PCQ inv scale as tmp local variable since its size/mem-usage is equal to the module weight
-        scale_inv = torch.mul(self.scale_inv[0], self.scale_inv[1]) if isinstance(self.scale_inv, list) else self.scale_inv
-        return cast_to_fp8_fcn(x, self.lp_dtype, scale_inv)
+        return cast_to_fp8_fcn(x, self.lp_dtype, self.scale_inv)
 
     def forward_qdq(self, x):
-        return quant_to_fp8_fcn(
+        return self.quantize_op(
                 x,
                 scale=self.scale,
-                zero_point=0,
+                zero_point=torch.tensor(0),
+                axis=0,
                 quant_min=self.quant_min,
                 quant_max=self.quant_max,
                 dtype=self.lp_dtype,
@@ -94,8 +111,7 @@ class QuantInput(QuantDequantBase):
 
     def extra_repr(self) -> str:
         repr = super(QuantInput, self).extra_repr()
-        scale_inv = self.scale_inv[0] if isinstance(self.scale_inv, list) else self.scale_inv
-        dtype = get_scale_dtype(scale_inv)
+        dtype = get_scale_dtype(self.scale_inv)
         return f"{repr}, scale_inv dtype={dtype}"
 
 
@@ -103,15 +119,20 @@ class DequantOutput(QuantDequantBase):
     def __init__(self, scale, lp_dtype, hp_dtype, *args, **kwargs):
         super(DequantOutput, self).__init__(lp_dtype, hp_dtype, *args, **kwargs)
         self.scale = create_scale_tensor(scale, self.scale_format)
+        if self.use_qdq:
+            self.dequantize_op = (
+                dequantize_per_channel_from_fp8 if self.scale.numel() > 1 else dequantize_per_tensor_from_fp8
+                )
 
     def forward(self, x):
         return cast_from_fp8_fcn(x, self.hp_dtype, self.scale)
 
     def forward_qdq(self, x):
-        return dequant_from_fp8_fcn(
+        return self.dequantize_op(
                 x,
-                scale=self.scale,
-                zero_point=0,
+                scale= self.scale,
+                zero_point=torch.tensor(0),
+                axis=1,
                 quant_min=self.quant_min,
                 quant_max=self.quant_max,
                 dtype=self.lp_dtype,
@@ -141,20 +162,20 @@ class QuantDequant(QuantDequantBase):
         return z
 
     def forward_qdq(self, x, *args, **kwargs):
-        y = quant_to_fp8_fcn(
+        y = quantize_per_tensor_to_fp8(
             x,
             scale=self.scale,
-            zero_point=0,
+            zero_point=torch.tensor(0),
             quant_min=self.quant_min,
             quant_max=self.quant_max,
             dtype=self.lp_dtype,
         )
-        z = dequant_from_fp8_fcn(
+        z = dequantize_per_tensor_from_fp8(
             y,
             scale=self.scale,
-            zero_point=0,
-            quant_min=int(torch.finfo(self.lp_dtype).min),
-            quant_max=int(torch.finfo(self.lp_dtype).max),
+            zero_point=torch.tensor(0),
+            quant_min=self.quant_min,
+            quant_max=self.quant_max,
             dtype=self.lp_dtype,
             out_dtype=self.hp_dtype,
         )
