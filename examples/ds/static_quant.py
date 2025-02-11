cdc1fa085b9b03666fc2a7eff38d6cc9c317e00a
Yi Liu <yiliu4@habana.ai>
2025-02-11 08:29:47 +0200

    use transformers
    Change-Id: Iee204311d566d528dd44eff0c227db45224ee242
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- examples/ds/static_quant.py
+++ examples/ds/static_quant.py
@@ -38,19 +38,23 @@ import os
 import torch
 import tqdm
 from loguru import logger
+import logging
 import safetensors
 from safetensors import safe_open
 from safetensors.torch import save_file
 import json
 
+logging.basicConfig(level=logging.DEBUG)
 torch.set_grad_enabled(False)
 
 # CONSTANTS
 SAFETENSORS = "safetensors"
 WEIGHT_SCALE_NAME = "scale_weight"
+INPUT_SCALE_NAME = "scale_input"
 SCALE_FILE_NAME = f"scales.{SAFETENSORS}"
 FULL_RANGE = torch.finfo(torch.float8_e4m3fn).max
 WEIGHT_BACKOFF = 0.5
+QUANT_MODULE_TYPES = (torch.nn.Linear,)
 """
 # https://docs.habana.ai/en/latest/PyTorch/Inference_on_PyTorch/Quantization/Inference_Using_FP8.html?highlight=backoff#supported-json-config-file-options
 Similarly, the maxabs value of a weight is scaled to weight_backoff*FP8_143_FULLSCALE. The default values are input_backoff=0.25 and weight_backoff=0.5.
@@ -90,7 +94,9 @@ def _maybe_create_dir(qmodel_path):
         os.makedirs(qmodel_path)
 
 
-def static_quant_model(model_path, qmodel_path):
+def static_quant_model_for_low_cpu_usage(model_path, qmodel_path):
+    # FIXME: need to skip some layers like embedding
+    logger.warning("It will quantize all weight tensors")
     _maybe_create_dir(qmodel_path)
     all_weight_filename = get_all_weight_filename(model_path)
     logger.info(f"Got {len(all_weight_filename)} weight files")
@@ -119,14 +125,40 @@ def static_quant_model(model_path, qmodel_path):
         json.dump(qtensor_mappping, f, indent=4)
 
 
+@torch.no_grad()
+def static_quant_model_tran(model_path, qmodel_path):
+    import transformers
+    from transformers.modeling_utils import no_init_weights
+    with no_init_weights():
+        model = transformers.AutoModelForCausalLM.from_pretrained(
+            model_path,
+            torch_dtype="auto",
+            low_cpu_mem_usage=True,
+            trust_remote_code=True,
+        )
+    for name, module in model.named_modules():
+        if isinstance(module, QUANT_MODULE_TYPES):
+            logger.debug(f"Processing {name}")
+            weight = module.weight
+            scale, qtensor = quant_tensor(weight)
+            module.weight.data = qtensor
+            setattr(module, "scale_weight", torch.nn.Parameter(scale, requires_grad=False))
+    logger.info(f"Saving quantized model to {qmodel_path}")
+    model.save_pretrained(qmodel_path)
+
+
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_path", type=str, required=True)
     parser.add_argument("--qmodel_path", type=str, required=True)
+    parser.add_argument("--low_cpu_mem", action="store_true", help="Load weight file one by one to reduce memory usage")
     args = parser.parse_args()
-    static_quant_model(args.model_path, args.qmodel_path)
+    if args.low_cpu_mem:
+        static_quant_model_for_low_cpu_usage(args.model_path, args.qmodel_path)
+    else:
+        static_quant_model_tran(args.model_path, args.qmodel_path)
 
 """
 model_path = "/software/users/yiliu4/HF_HOME/hub/DeepSeek-V3-BF16"
@@ -134,5 +166,6 @@ model_path = "/software/users/yiliu4/HF_HOME/hub/deepseekv3-bf16-4l/"
 qmodel_path = "/software/users/yiliu4/HF_HOME/hub/deepseekv3-bf16-4l-q/"
 static_quant_model(model_path, qmodel_path)
 python static_quant.py --model_path /software/users/yiliu4/HF_HOME/hub/deepseekv3-bf16-4l/ --qmodel_path /software/users/yiliu4/HF_HOME/hub/deepseekv3-bf16-4l-q/
+python static_quant.py --model_path /software/users/yiliu4/HF_HOME/hub/DeepSeek-V3-BF16/ --qmodel_path /software/users/yiliu4/HF_HOME/hub/DeepSeek-V3-BF16-q/
 
 """
