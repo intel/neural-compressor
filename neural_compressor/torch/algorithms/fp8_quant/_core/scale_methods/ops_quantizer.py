c0b760e0ac803db3137360e70f562ed9d1a5a75a
Yi Liu <yiliu4@habana.ai>
2025-02-21 18:47:59 +0200

    Revert "fix"
    This reverts commit 32356572728853395cd62fed64d7aad408888091.

--- neural_compressor/torch/algorithms/fp8_quant/_core/scale_methods/ops_quantizer.py
+++ neural_compressor/torch/algorithms/fp8_quant/_core/scale_methods/ops_quantizer.py
@@ -228,7 +228,7 @@ class FsdpaOpQuantizer(BaseOpQuantizer):
         self.output_scales_creators.append(self.scales_method_factory.get_scale_method(QuantTensorName.OUTPUT))
 
     def get_scales_module_config(self):
-        # ForkedPdb().set_trace()
+        ForkedPdb().set_trace()
         # 3 inputs calcs from input measurement
         input_scales = self.calc_input_scales(num_of_inputs=self.num_of_inputs - 1)
         # one input calcs from output measurement
