732a8fcae9cb8a9a7cdf3a4f6e6bdf31be0479e0
Yi Liu <yiliu4@habana.ai>
2025-02-24 08:36:00 +0200

    fix
    Change-Id: Ibad8394754e3bd418605b7595ce6221a53c91d55
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- neural_compressor/torch/algorithms/fp8_quant/_core/measure.py
+++ neural_compressor/torch/algorithms/fp8_quant/_core/measure.py
@@ -109,7 +109,7 @@ def prepare_model(model, mod_list=None):
     gmod_list.extend(mod_list)
     generate_model_info(model)
     logger.info(f"generated model info")  
-    for mod, name in parent_child_mod_dict:
+    for mod, name in parent_child_mod_dict.items():
         logger.info(f"mod: {mod}, name: {name}")
     register_patched_measure_modules(model, mod_list, observer_class, d_shapes)
 
