8ac225be01ca037e107bd2f045d189c0089cf0aa
Yi Liu <yiliu4@habana.ai>
2025-02-24 07:58:35 +0200

    add debug
    Change-Id: I1b82000eef0a6d00ba5c83f7ebb0f4a7e4264ba1
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- neural_compressor/torch/algorithms/fp8_quant/_core/measure.py
+++ neural_compressor/torch/algorithms/fp8_quant/_core/measure.py
@@ -156,6 +156,7 @@ def register_patched_measure_modules(model, mod_list, observer_class, d_shapes=N
                 )
                 logger.info(f"Patching measure module {name} {mod.__class__} ")
                 pmod = patch_module_measure(mod, mod_extra_config, mod_default_dict)
+                logger.info(f"Pacthed module pmod: {pmod}")
                 if pmod._mod_extra_config:
                     for param_name in pmod._mod_extra_config.params:
                         param = getattr(pmod, param_name)
