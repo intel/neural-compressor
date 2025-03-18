31282b7a0382c84fa4f6d3bb96f4851466dac207
Tomasz Bohutyn <tbohutyn@habana.ai>
2025-03-18 10:41:39 +0100

    [SW-222366] Switch tests to lazy mode (#174)

--- test/3x/torch/algorithms/fp8_quant/conftest.py
+++ test/3x/torch/algorithms/fp8_quant/conftest.py
@@ -1,5 +1,9 @@
 # Called once at the beginning of the test session
 def pytest_sessionstart():
+    import os
+
+    os.environ.setdefault("PT_HPU_LAZY_MODE", "1")
+
     import habana_frameworks.torch.core as htcore
     import torch
 
@@ -10,4 +14,3 @@ def pytest_sessionstart():
 
     # Fix the seed - just in case
     torch.manual_seed(0)
-
