be5f6f46c268344bcc46b009a4a947313103d56a
Tomasz Bohutyn <tbohutyn@habana.ai>
2025-03-19 11:54:02 +0100

    [SW-222366] Move env default to init (#178)

--- test/3x/torch/algorithms/fp8_quant/conftest.py
+++ test/3x/torch/algorithms/fp8_quant/conftest.py
@@ -1,9 +1,5 @@
 # Called once at the beginning of the test session
 def pytest_sessionstart():
-    import os
-
-    os.environ.setdefault("PT_HPU_LAZY_MODE", "1")
-
     import habana_frameworks.torch.core as htcore
     import torch
 
