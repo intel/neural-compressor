b3badef9e16448082d6b2aaed55aed9106d71f75
Tomasz Szulist <72727299+tszulist-hbn@users.noreply.github.com>
2025-03-25 13:54:48 +0100

    Revert "fp8 aware gptq (hybrid gptq) (#154)" (#184)
    This reverts commit 050dc44424debaa659f8dd58b4a4ec6bc9af7d68.

--- neural_compressor/torch/algorithms/mixed_low_precision/modules.py
+++ neural_compressor/torch/algorithms/mixed_low_precision/modules.py
@@ -9,30 +9,26 @@ from torch.nn import functional as F
 from ..weight_only.modules import HPUWeightOnlyLinear
 from neural_compressor.torch.utils import accelerator, logger
 
-cast_to_fp8_fcn = lambda x, dtype, scale_inv=None: torch.ops.hpu.cast_to_fp8_v2(x, scale_inv, False, False, dtype)[0]
 
 class HPUMixedPrecisionLinear(HPUWeightOnlyLinear):
     """Weight and Activations quant (W4A8 gptq) Linear for HPU device."""
 
     def __init__(
-        self, in_features, out_features, bias,
+        self, in_features, out_features,
         **kwargs,
     ):
         """Init the HPUMixedPrecisionLinear object.
         """
-        super(HPUMixedPrecisionLinear, self).__init__(in_features, out_features, bias=bias)
+        super(HPUMixedPrecisionLinear, self).__init__(in_features, out_features)
 
     def forward(self, input):
         """The forward function of HPUMixedPrecisionLinear."""
         input_dtype = input.dtype
         output_shape = input.shape[:-1] + (self.out_features,)
         scales = self.scales
-        scale_bf16_to_fp8 = self.scale_bf16_to_fp8
         qweight = self.qweight
         zeros = self.qzeros
-        self.matmul_internal.scale_other = torch.nn.Parameter(scale_bf16_to_fp8)
-        weight = torch.ops.hpu.convert_from_uint4(qweight, scales, zeros, torch.bfloat16)  # the uint4->fp8 is currently slower and with bugs. Jira ticket: https://jira.habana-labs.com/browse/SW-218009
-        weight = cast_to_fp8_fcn(weight, torch.float8_e4m3fn)
+        weight = torch.ops.hpu.convert_from_uint4(qweight, scales/self.matmul_internal.scale_other, zeros, torch.float8_e4m3fn)     # todo: div scales in init
         output = self.matmul_internal(input, weight)
         output = output.to(dtype=input_dtype).reshape(
             output_shape
@@ -42,8 +38,7 @@ class HPUMixedPrecisionLinear(HPUWeightOnlyLinear):
 
     @staticmethod
     def convert_from_weight_only(obj):
-        bias = obj.bias is not None
-        new_self = HPUMixedPrecisionLinear(obj.in_features, obj.out_features, bias)
+        new_self = HPUMixedPrecisionLinear(obj.in_features, obj.out_features)
         for attr, value in vars(obj).items():
             setattr(new_self, attr, value)
         new_self.matmul_internal.no_input_quant = True # flag for 8bit input, which shouldn't be quantized in matmul
