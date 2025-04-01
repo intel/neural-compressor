a162cb3065a26470e5b5e365355a3a3e628ef985
Xin He <xinhe3@habana.ai>
2025-04-01 10:09:49 +0300

    Workaround of [SW-208658]
    Signed-off-by: Xin He <xinhe3@habana.ai>

--- test/3x/torch/quantization/fp8_quant/test_layer_wise.py
+++ test/3x/torch/quantization/fp8_quant/test_layer_wise.py
@@ -11,6 +11,9 @@ htcore.hpu_set_env()
 
 def test_two_step_layer_wise():
     # layer-wise is based on memory mapping technique and https://github.com/huggingface/transformers/pull/31771
+    # Workaround of [SW-208658]: Memory mapping is blocked unreasonably
+    tmp_deterministic_algorithms_flag = torch.are_deterministic_algorithms_enabled()
+    torch.use_deterministic_algorithms(False)
     model_name = "facebook/opt-350m"
     config = AutoConfig.from_pretrained(model_name)
     # requires transformers >= 4.43.0, torch_dtype=config.torch_dtype
@@ -37,3 +40,4 @@ def test_two_step_layer_wise():
     cpu_mem2 = get_used_cpu_mem_MB()
     model = convert(new_model, qconfig)
     assert (cpu_mem2 - cpu_mem0) < 100, "model with memory mapping should use no more than 100MiB."
+    torch.use_deterministic_algorithms(tmp_deterministic_algorithms_flag)
