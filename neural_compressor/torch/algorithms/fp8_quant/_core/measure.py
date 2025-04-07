ccb90e5d7144c2111186710cdea289aa9f9b3fb9
Yi Liu <yiliu4@habana.ai>
2025-04-07 07:07:12 +0300

    fix moe op num_outputs
    Change-Id: Ic3e689f026497908f409d9ffc2a69971fe36d0cc
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- neural_compressor/torch/algorithms/fp8_quant/_core/measure.py
+++ neural_compressor/torch/algorithms/fp8_quant/_core/measure.py
@@ -149,9 +149,10 @@ def register_patched_measure_modules(model, mod_list, observer_class, d_shapes=N
                     logger.info(f"Patching measure module {name} {mod.__class__}")
                     num_info += 1
                 set_hqt_config(mod, top_level_config)  # set config in the module, as it consumed by the patched module
-                # override default number of outputs for dynamic moe
-                mod_types[mod_type].num_outputs = mod.num_experts+1
-                logger.warning(f"Dynamic moe num_outputs set to {mod.num_experts+1}")
+                if mod_type == "dynamic_moe" and hasattr(mod, "num_experts"):
+                    # override default number of outputs for dynamic moe
+                    mod_types[mod_type].num_outputs = mod.num_experts+1
+                    logger.warning(f"Dynamic moe num_outputs set to {mod.num_experts+1}")
                 mod_extra_config = (
                     init_measure_object(
                         mod,
