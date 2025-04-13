0fee3ffb20c31749dbca080914780ebc175e8d09
Yi Liu <yi4.liu@intel.com>
2025-04-13 12:53:07 +0800

    [SW-224851]Fix `PatchedMoeMatmul` and Get `num_experts` from Module #202 (#213)
    [SW-224851]Fix `PatchedMoeMatmul` and Get `num_experts` from Module 
    ---------
    
    Signed-off-by: Yi Liu <yiliu4@habana.ai>
    Co-authored-by: Yi Liu <yiliu4@habana.ai>
    Co-authored-by: Linoy Buchnik <linoybu@gmail.com>

--- neural_compressor/torch/algorithms/fp8_quant/_quant_common/helper_modules.py
+++ neural_compressor/torch/algorithms/fp8_quant/_quant_common/helper_modules.py
@@ -603,7 +603,7 @@ class PatchedMoeMatmul(PatchedLinearBase):
         if (self.quantization_mode == QuantMode.MEASURE) or (self.quantization_mode == QuantMode.SHAPE):
             measure_input((torch.tensor(0),), observer=self._mod_extra_config.inputs)
         else:
-            self.weight = self.weight.squeeze()
+            self.weight = torch.nn.Parameter(self.weight.squeeze(), requires_grad=False)
 
     def forward_qdq(self, input, *args, **kwargs):
         return self.run_linear_qdq(input, None)
