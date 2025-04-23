3e66fa1c49eb43f63758d6c0819f9b32573d5616
Yi Liu <yiliu4@habana.ai>
2025-04-23 14:26:49 +0300

    need torch 2.7
    Change-Id: I35097ec70490a301ebf1afe86770719a0972c417
    Signed-off-by: Yi Liu <yiliu4@habana.ai>

--- neural_compressor/torch/export/pt2e_export.py
+++ neural_compressor/torch/export/pt2e_export.py
@@ -19,7 +19,7 @@ import torch
 from torch.fx.graph_module import GraphModule
 
 from neural_compressor.common.utils import logger
-from neural_compressor.torch.utils import TORCH_VERSION_2_2_2, TORCH_VERSION_2_6, get_torch_version, is_ipex_imported
+from neural_compressor.torch.utils import TORCH_VERSION_2_2_2, TORCH_VERSION_2_7_0, get_torch_version, is_ipex_imported
 
 __all__ = ["export", "export_model_for_pt2e_quant"]
 
@@ -52,7 +52,7 @@ def export_model_for_pt2e_quant(
             # Note 1: `capture_pre_autograd_graph` is also a short-term API, it will be
             # updated to use the official `torch.export` API when that is ready.
             cur_version = get_torch_version()
-            if cur_version >= TORCH_VERSION_2_6:
+            if cur_version >= TORCH_VERSION_2_7_0:
                 export_func = torch.export.export_for_training
             else:
                 export_func = torch._export.capture_pre_autograd_graph
@@ -70,7 +70,7 @@ def export_model_for_pt2e_quant(
                 exported_model = export_func(  # pylint: disable=E1123
                     model, args=example_inputs, dynamic_shapes=dynamic_shapes
                 )
-            if cur_version >= TORCH_VERSION_2_6:
+            if cur_version >= TORCH_VERSION_2_7_0:
                 exported_model = exported_model.module()
             exported_model._exported = True
             logger.info("Exported the model to Aten IR successfully.")
