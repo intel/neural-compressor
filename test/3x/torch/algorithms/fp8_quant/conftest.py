a41206a86f75a0cbb95424b26f4c870a6ccb5fa5
Xin He <xinhe3@habana.ai>
2025-04-23 11:54:28 +0300

    add INC_3X_ONLY in __init__.py
    Signed-off-by: Xin He <xinhe3@habana.ai>

--- test/3x/torch/algorithms/fp8_quant/conftest.py
+++ test/3x/torch/algorithms/fp8_quant/conftest.py
@@ -1,9 +1,11 @@
 # Called once at the beginning of the test session
 def pytest_sessionstart():
+    import os
     import habana_frameworks.torch.core as htcore
     import torch
 
     htcore.hpu_set_env()
+    os.environ.setdefault("INC_3X_ONLY", "1")
 
     # Use reproducible results
     torch.use_deterministic_algorithms(True)
