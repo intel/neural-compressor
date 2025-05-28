43066e67e108044a14ee34915b4396b41c8e836e
Sun, Xuehao <xuehao.sun@intel.com>
2025-05-28 01:41:48 -0700

    update torch to 2.7.0 in CI
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/fwk_version.sh
+++ .azure-pipelines/scripts/fwk_version.sh
@@ -2,8 +2,8 @@
 
 echo "export FWs version..."
 export tensorflow_version='2.15.0-official'
-export pytorch_version='2.5.1+cpu'
-export torchvision_version='0.20.1'
-export ipex_version='2.5.0+cpu'
+export pytorch_version='2.7.0+cpu'
+export torchvision_version='0.22.0'
+export ipex_version='2.7.0+cpu'
 export onnx_version='1.17.0'
 export onnxruntime_version='1.20.0'
