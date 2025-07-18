c471574567e22d41afe34f209a539817d709cd0d
Xin He <xinhe3@habana.ai>
2025-07-18 17:49:06 +0300

    add version check for 1.22.0
    Signed-off-by: Xin He <xinhe3@habana.ai>

--- neural_compressor/torch/algorithms/weight_only/modules.py
+++ neural_compressor/torch/algorithms/weight_only/modules.py
@@ -25,7 +25,13 @@ import torch
 from torch.autograd import Function
 from torch.nn import functional as F
 
-from neural_compressor.torch.utils import accelerator, can_pack_with_numba, logger
+from neural_compressor.torch.utils import (
+    Version,
+    accelerator,
+    can_pack_with_numba,
+    get_hpex_version,
+    logger,
+)
 
 from .utility import quant_tensor
 
@@ -725,6 +731,7 @@ class HPUWeightOnlyLinear(WeightOnlyLinear):
             )
         else:
             self.g_idx = None
+        self.support_g_idx = True if get_hpex_version() >= Version("1.23.0") else False
 
         self.half_indim = self.in_features // 2
 
@@ -740,7 +747,11 @@ class HPUWeightOnlyLinear(WeightOnlyLinear):
         qweight = self.qweight
         zeros = self.qzeros
         g_idx = self.g_idx
-        weight = torch.ops.hpu.convert_from_uint4(qweight, scales, zeros, input_dtype, g_idx)
+        if self.support_g_idx:
+            weight = torch.ops.hpu.convert_from_uint4(qweight, scales, zeros, input_dtype, g_idx)
+        else:
+            # The released docker 1.22.0 only supports 4 arguments
+            weight = torch.ops.hpu.convert_from_uint4(qweight, scales, zeros, input_dtype)
         output = self.matmul_internal(input, weight)
         output = output.to(dtype=input_dtype).reshape(
             output_shape
