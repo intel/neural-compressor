65863322a975128e6a34cad04b2d0490e6b57e8f
Wang, Mengni <mengni.wang@intel.com>
2025-09-29 15:17:44 +0800

    Update setup.sh

--- examples/3.x_api/pytorch/multimodal-modeling/quantization/auto_round/llama4/setup.sh
+++ examples/3.x_api/pytorch/multimodal-modeling/quantization/auto_round/llama4/setup.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 pip install setuptools --upgrade
-pip install packaging -- upgrade
+pip install packaging --upgrade
 pip install -U "huggingface_hub[cli]"
 git clone -b mxfp4 https://github.com/mengniwang95/vllm-fork.git
 cd vllm-fork
