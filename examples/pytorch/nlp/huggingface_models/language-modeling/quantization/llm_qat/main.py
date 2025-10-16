3d50a05d24779fd19eea39d964ae00c0bad9b9e9
lkk <kaokao.lv@intel.com>
2025-10-16 03:20:48 +0000

    add mxfp8 qat example.

---
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/llm_qat/main.py
@@ -0,0 +1,189 @@
+import logging
+import os
+import sys
+from dataclasses import dataclass, field
+from warnings import warn
+
+import torch
+import transformers
+from transformers.trainer_utils import get_last_checkpoint
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    HfArgumentParser,
+    Trainer,
+    default_data_collator,
+    set_seed,
+    TrainerCallback,
+)
+
+from utils import (
+    get_metrics_with_perplexity,
+    make_supervised_data_module,
+)
+
+logger = logging.getLogger(__name__)
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: str = field(default="meta-llama/Llama-3.1-8B")
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    cache_dir: str | None = field(default=None)
+    model_max_length: int = field(
+        default=2048,
+        metadata={
+            "help": (
+                "Maximum sequence length. Sequences will be right padded (and possibly truncated)."
+            )
+        },
+    )
+    dataloader_drop_last: bool = field(default=True)
+    bf16: bool = field(default=True)
+
+@dataclass
+class DataArguments:
+    dataset: str = field(
+        default="Daring-Anteater",
+        metadata={"help": "Specify the dataset.", "choices": ["Daring-Anteater"]},
+    )
+    train_size: int = field(
+        default=0,
+        metadata={"help": "Number of training samples to use. If `0`, use default training size."},
+    )
+    eval_size: int = field(
+        default=0,
+        metadata={
+            "help": "Number of evaluation samples to use. If `0`, use default evaluation size."
+        },
+    )
+
+@dataclass
+class QuantizationArguments:
+    quant_scheme: str | None = field(
+        default=None,
+        metadata={
+            "help": (
+                "Specify the quantization format for PTQ/QAT. if specified, PTQ/QAT will be enabled"
+                " with the specified quantization format"
+            ),
+            "choices": ["MXFP8"],
+        },
+    )
+
+
+def train():
+    parser = HfArgumentParser(
+        (ModelArguments, TrainingArguments, DataArguments, QuantizationArguments)
+    )
+
+    model_args, training_args, data_args, quant_args = parser.parse_args_into_dataclasses()
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    # Log on each process the small summary:
+    logger.warning(
+        f"Process rank: {training_args.local_process_index}, device: {training_args.device}, n_gpu: {training_args.n_gpu}, "
+        + f"distributed training: {training_args.parallel_mode.value == 'distributed'}, 16-bits training: {training_args.fp16}"
+    )
+    # Set seed before initializing model.
+    set_seed(training_args.seed)
+
+    logger.info(f"arguments: {model_args}, {training_args}, {data_args}, {quant_args}")
+
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(training_args.output_dir) and training_args.do_train:
+        last_checkpoint = get_last_checkpoint(training_args.output_dir)
+        logger.info(f"Last checkpoint detected: {last_checkpoint}")
+
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_args.model_name_or_path,
+        cache_dir=training_args.cache_dir,
+        torch_dtype=torch.bfloat16,
+    )
+    model.generation_config.do_sample = True
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_args.model_name_or_path, model_max_length=training_args.model_max_length
+    )
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    # We set model.config.use_cache to False for training when gradient_checkpointing=False.
+    # Currently useful for FSDP2 to allow for setting activation_checkpointing=True in the config file.åå
+    model.config.use_cache = False
+
+    # prepare model for quantization
+    if quant_args.quant_scheme is not None:
+        from neural_compressor.torch.quantization.quantize import prepare_qat
+        # inplace
+        # default mxfp8
+        prepare_qat(model)
+
+        logger.info("Finish model preparation for QAT.")
+
+    logger.info("Loading dataset......")
+
+    # reuse the dataset function, TODO: preprocess a new dataset
+    data_module = make_supervised_data_module(
+        dataset=data_args.dataset,
+        tokenizer=tokenizer,
+        train_size=data_args.train_size,
+        eval_size=data_args.eval_size,
+    )
+
+    # Ensure calibration size doesn't exceed evaluation dataset size
+    eval_dataset_size = len(data_module["eval_dataset"])
+
+    # Training
+    checkpoint = None
+    if training_args.resume_from_checkpoint is not None:
+        checkpoint = training_args.resume_from_checkpoint
+    elif last_checkpoint is not None:
+        checkpoint = last_checkpoint
+
+    if checkpoint is not None and training_args.lora:
+        raise RuntimeError("Does not support LoRA resuming training yet!")
+
+    # Torch >= 2.4 throws an error if `use_reentrant` is not set explicitly
+    if training_args.gradient_checkpointing and training_args.gradient_checkpointing_kwargs is None:
+        training_args.gradient_checkpointing_kwargs = {"use_reentrant": True}
+
+    trainer = Trainer(
+        model=model,
+        processing_class=tokenizer,
+        args=training_args,
+        **data_module,
+    )
+
+    if training_args.do_train:
+        logger.info("Starting Train...")
+        trainer.train(resume_from_checkpoint=checkpoint)
+        logger.info("Training completed.")
+
+    if training_args.do_eval:
+        logger.info("Starting Evaluation...")
+        metrics = trainer.evaluate()
+        metrics = get_metrics_with_perplexity(metrics)
+        logger.info(f"Evaluation results: \n{metrics}")
+
+    if training_args.do_train and quant_args.quant_scheme is None:
+        logger.info("Saving the model...")
+        trainer.save_model(training_args.output_dir)
+    elif quant_args.quant_scheme is not None:
+        from neural_compressor.torch.export.export_hf import export_hf2compressored_model
+        # export quantized model for vllm inference using llm-compressor and compressed_tensor
+        export_hf2compressored_model(model, training_args.output_dir, quant_args.quant_scheme)
+        if tokenizer is not None:
+            tokenizer.save_pretrained(export_path)
+
+
+if __name__ == "__main__":
+    train()
---
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/llm_qat/utils.py
@@ -0,0 +1,148 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import gc
+import types
+from contextlib import contextmanager
+from functools import partial
+
+import datasets
+import torch
+
+import transformers
+from transformers import default_data_collator
+
+IGNORE_INDEX = -100
+
+
+@contextmanager
+def main_process_first():
+    """Context manager to run code on the main process first."""
+    if not torch.distributed.is_initialized():
+        yield
+        return
+
+    rank = torch.distributed.get_rank()
+    if rank == 0:
+        yield
+        torch.distributed.barrier()
+    else:
+        torch.distributed.barrier()
+        yield
+    torch.distributed.barrier()
+
+
+def get_daring_anteater(
+    tokenizer: transformers.AutoTokenizer,
+    split="train",
+    max_length=4096,
+    train_size=0,
+    eval_size=0,
+):
+    # sample = {
+    #     'system': '{system message}',
+    #     'conversations': [
+    #         {'from': 'User', 'value': '{turn 1 user message}', 'label': None},
+    #         {'from': 'Assistant', 'value': '{turn 1 assistant message}', 'label': '{turn 1 assistant label}'},
+    #         {'from': 'User', 'value': '{turn 2 user message}', 'label': None},
+    #         {'from': 'Assistant', 'value': '{turn 2 assistant message}', 'label': '{turn 2 assistant label}'},
+    #     ],
+    #     "mask": "User",
+    #     "type": "VALUE_TO_TEXT",
+    # }
+
+    def process_and_tokenize(sample):
+        conversations = sample["conversations"]
+        all_input_ids = [tokenizer.bos_token_id] if tokenizer.bos_token_id else []
+        all_labels = [IGNORE_INDEX] if tokenizer.bos_token_id else []
+
+        for conversation in conversations:
+            role = conversation["from"]
+            input_ids = tokenizer.encode(conversation["value"] + "\n", add_special_tokens=False)
+            labels = input_ids if role == "Assistant" else [IGNORE_INDEX] * len(input_ids)
+
+            all_input_ids.extend(input_ids)
+            all_labels.extend(labels)
+
+            if len(all_input_ids) > max_length:
+                break
+
+        all_input_ids.append(tokenizer.eos_token_id)
+        all_labels.append(IGNORE_INDEX)
+        all_attention_mask = [1] * len(all_input_ids)
+
+        cur_seq_length = len(all_input_ids)
+        if cur_seq_length < max_length:
+            pad_token = (
+                tokenizer.pad_token_id
+                if tokenizer.pad_token_id is not None
+                else tokenizer.eos_token_id
+            )
+            all_input_ids += [pad_token] * (max_length - cur_seq_length)
+            all_attention_mask += [0] * (max_length - cur_seq_length)
+            all_labels += [IGNORE_INDEX] * (max_length - cur_seq_length)
+
+        return {
+            "input_ids": all_input_ids[:max_length],
+            "attention_mask": all_attention_mask[:max_length],
+            "labels": all_labels[:max_length],
+        }
+
+    if hasattr(get_daring_anteater, "cached_dataset"):
+        dataset = get_daring_anteater.cached_dataset
+    else:
+        with main_process_first():
+            dataset = datasets.load_dataset("nvidia/Daring-Anteater", split="train")
+            # Shuffle and subsample the dataset
+            eval_size = 2000 if eval_size == 0 else eval_size
+            train_size = len(dataset) - eval_size if train_size == 0 else train_size
+            assert train_size + eval_size <= len(dataset) and train_size > 0 and eval_size > 0, (
+                "not enough data for train-eval split"
+            )
+            dataset = dataset.shuffle(seed=42).select(range(train_size + eval_size))
+            dataset = dataset.map(process_and_tokenize, remove_columns=list(dataset.features))
+            dataset = dataset.train_test_split(test_size=eval_size, shuffle=True, seed=42)
+        get_daring_anteater.cached_dataset = dataset
+    return dataset[split]
+
+
+def make_supervised_data_module(
+    dataset="Daring-Anteater",
+    tokenizer: transformers.PreTrainedTokenizer = None,
+    train_size: int = 0,
+    eval_size: int = 0,
+) -> dict:
+    """Make dataset and collmtor for supervised fine-tuning."""
+    if dataset == "Daring-Anteater":
+        train_dataset = get_daring_anteater(
+            tokenizer, "train", tokenizer.model_max_length, train_size, eval_size
+        )
+        val_dataset = get_daring_anteater(
+            tokenizer, "test", tokenizer.model_max_length, train_size, eval_size
+        )
+    else:
+        raise ValueError(f"Dataset {dataset} not supported")
+    return {
+        "train_dataset": train_dataset,
+        "eval_dataset": val_dataset,
+        "data_collator": default_data_collator,
+    }
+
+
+def get_metrics_with_perplexity(metrics):
+    """Add perplexity to the metrics."""
+    if "eval_loss" in metrics:
+        metrics["perplexity"] = float(torch.exp(torch.tensor(metrics["eval_loss"])))
+    return metrics
