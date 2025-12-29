4484882d7355d47471d9fbd4ea21f29195616598
chensuyue <suyue.chen@intel.com>
2025-12-29 17:12:36 +0800

    remove auto-round install
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/qwen/setup.sh
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/qwen/setup.sh
@@ -6,9 +6,5 @@ pip install -U "huggingface_hub[cli]"
 git clone -b ds-fp8kv  --single-branch --quiet https://github.com/yiliu30/vllm-fork.git && cd vllm-fork
 VLLM_USE_PRECOMPILED=1 pip install --editable . -v
 cd ..
-# Install auto-round
-git clone -b ds-fp8kv --single-branch --quiet https://github.com/intel/auto-round.git && cd auto-round
-pip install -e . -v
-cd ..
 # Uninstall flash_attn to avoid conflicts
 pip uninstall flash_attn -y
