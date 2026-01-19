cd5979d12768a597bdec9486d14d3abcf0650312
xinhe3 <xinhe3@habana.ai>
2026-01-19 07:23:00 +0200

    Remove skip since transformers fixed it
    Signed-off-by: xinhe3 <xinhe3@habana.ai>

--- test/3x/torch/quantization/fp8_quant/test_layer_wise.py
+++ test/3x/torch/quantization/fp8_quant/test_layer_wise.py
@@ -7,7 +7,6 @@ from neural_compressor.torch.quantization import FP8Config, convert, prepare, fi
 from neural_compressor.torch.utils import get_used_cpu_mem_MB
 
 
-@pytest.mark.skip(reason="https://github.com/huggingface/transformers/issues/43159")
 def test_two_step_layer_wise():
     # layer-wise is based on memory mapping technique and https://github.com/huggingface/transformers/pull/31771
     # Workaround of [SW-208658]: torch.use_deterministic_algorithms(True) will break memory mapping
