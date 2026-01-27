491a3e0ae6293ec7a9e1c3b9f4a1c9522bb9ef5e
yiliu30 <yi4.liu@intel.com>
2026-01-26 17:06:41 -0800

    fix ar attn
    Signed-off-by: yiliu30 <yi4.liu@intel.com>

--- examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/deepseek/setup.sh
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/deepseek/setup.sh
@@ -3,6 +3,10 @@ pip install setuptools --upgrade
 pip install packaging --upgrade
 pip install transformers==4.57.3
 pip install -U "huggingface_hub[cli]"
+# Intsall auto-round
+git clone -b fix-ds-attn https://github.com/intel/auto-round.git && cd auto-round
+pip install -e .
+cd ..
 # Install vllm
 git clone -b fused-moe-ar --single-branch --quiet https://github.com/yiliu30/vllm-fork.git && cd vllm-fork
 VLLM_USE_PRECOMPILED=1 pip install . -v
