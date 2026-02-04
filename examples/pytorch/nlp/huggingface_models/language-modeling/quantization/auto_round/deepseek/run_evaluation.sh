6b55f0a3b42257d95a1f7482495b1a97c4ace6b1
chensuyue <suyue.chen@intel.com>
2026-02-04 11:47:25 +0800

    freeze transformers version
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/deepseek/run_evaluation.sh
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/deepseek/run_evaluation.sh
@@ -106,7 +106,7 @@ elif [[ "$SCHEME" == "mxfp8" ]]; then
     VLLM_ENABLE_STATIC_MOE=0
     VLLM_USE_DEEP_GEMM=0
     VLLM_ENABLE_AR_EXT=1
-elif [[ "$SCHEME" == "bf16" ]]; then
+elif [[ "$SCHEME" == "fp8" ]]; then
     echo "No quantization scheme specified, run original model."
 else
     echo "Error: Invalid quantization scheme (-s). Must be 'mxfp4', 'nvfp4' or 'mxfp8'."
