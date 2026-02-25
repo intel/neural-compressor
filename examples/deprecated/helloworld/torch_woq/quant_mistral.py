998e48a2f83dcbbaa1ea69c2224682ff84e765a7
chensuyue <suyue.chen@intel.com>
2026-02-25 15:57:17 +0800

    remove 2x examples
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- examples/deprecated/helloworld/torch_woq/quant_mistral.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoModel
-from neural_compressor.config import PostTrainingQuantConfig
-from neural_compressor.quantization import fit
-
-float_model = AutoModel.from_pretrained("mistralai/Mistral-7B-v0.1")
-woq_conf = PostTrainingQuantConfig(approach="weight_only")
-quantized_model = fit(model=float_model, conf=woq_conf)
