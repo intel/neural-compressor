93aa8485b24d2b9068fc72a4814b27613171b91f
Sun, Xuehao <xuehao.sun@intel.com>
2026-04-08 10:55:32 +0800

    fix hpu ut
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/install_nc.sh
+++ .azure-pipelines/scripts/install_nc.sh
@@ -3,10 +3,14 @@ set -x
 echo -e "##[group]Install Neural Compressor ... "
 cd /neural-compressor
 
+if ! command -v uv &> /dev/null; then
+    pip install uv
+fi
+
 if [[ $1 = *"3x_pt"* ]]; then
     uv pip install --no-cache-dir -r requirements_pt.txt
     if [[ $1 = *"hpu"* ]]; then
-        pip uninstall neural_compressor_3x_pt -y || true
+        uv pip uninstall neural_compressor_3x_pt -y || true
     elif [[ $1 = *"xpu"* ]]; then
         echo -e "\n Install torch XPU ... "
         uv pip install torch==2.11.0 torchvision --index-url https://download.pytorch.org/whl/xpu
@@ -19,10 +23,10 @@ if [[ $1 = *"3x_pt"* ]]; then
     python setup.py pt bdist_wheel
     uv pip install --no-deps dist/neural_compressor*.whl --force-reinstall
 elif [[ $1 = *"3x_tf"* ]]; then
-	pip install tensorflow==2.19.0 uv
-    python -m pip install --no-cache-dir -r requirements_tf.txt
+	uv pip install tensorflow==2.19.0
+    uv pip install --no-cache-dir -r requirements_tf.txt
     python setup.py tf bdist_wheel
-    pip install dist/neural_compressor*.whl --force-reinstall
+    uv pip install dist/neural_compressor*.whl --force-reinstall
 elif [[ $1 = *"3x_jax"* ]]; then
     uv pip install --no-cache-dir -r requirements_jax.txt
     python setup.py jax bdist_wheel
