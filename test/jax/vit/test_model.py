582bc928cc0b8ad207e8800afb2037bd676f3235
Wojciech Piętka <wojciechx.pietka@intel.com>
2026-04-21 02:42:10 -0700

    Improvement after review

--- test/jax/vit/test_model.py
+++ test/jax/vit/test_model.py
@@ -16,21 +16,12 @@ from PIL import Image
 from neural_compressor.jax import DynamicQuantConfig, StaticQuantConfig, quantize_model
 
 
-def load_model_from_preset(model_type, preset, allow_download=False, dtype="float32"):
-    datasets_path = "/models/"
-    model_path = datasets_path + preset
+@pytest.fixture(scope="module")
+def colva_beach_sq():
+    repo_root_path = f"{os.path.dirname(__file__)}/../../.."
+    image_path = f"{repo_root_path}/examples/jax/keras/vit/colva_beach_sq.jpg"
+    target_size = (224, 224)
 
-    if os.path.exists(model_path):
-        return model_type.from_preset(model_path, dtype=dtype)
-    elif allow_download:
-        model = model_type.from_preset(preset, dtype=dtype)
-        model.save_to_preset(model_path)
-        return model
-    else:
-        raise Exception(f"Model path does not exist: {model_path}")
-
-
-def load_image(image_path, target_size=(224, 224)):
     img = Image.open(image_path)
     if img.mode != "RGB":
         img = img.convert("RGB")
@@ -40,10 +31,23 @@ def load_image(image_path, target_size=(224, 224)):
     return normalized_pixels
 
 
-def create_random_image():
+@pytest.fixture(scope="module")
+def random_image():
     key = random.PRNGKey(0)
     img = random.uniform(key, shape=(1, 224, 224, 3), minval=0.0, maxval=1.0, dtype=jnp.float32)
     return img
+
+
+def load_model_from_preset(model_type, preset, dtype="float32"):
+    datasets_path = os.environ.get("DATASETS_PATH")
+    if datasets_path is None:
+        datasets_path = "/models/"
+
+    model_path = f"{datasets_path}/{preset}"
+    if os.path.exists(model_path):
+        return model_type.from_preset(model_path, dtype=dtype)
+    else:
+        raise Exception(f"Model path does not exist: {model_path}")
 
 
 def classify_image(model, input, labels_n=1):
@@ -55,17 +59,13 @@ def classify_image(model, input, labels_n=1):
 
 @pytest.mark.parametrize("dynamic", [True, False], ids=["dynamic=True", "dynamic=False"])
 @pytest.mark.parametrize("model_dtype", ["float32", "bfloat16"], ids=["model_dtype=float32", "model_dtype=bfloat16"])
-def test_image_classification(dynamic, model_dtype):
-    repo_root_path = f"{os.path.dirname(__file__)}/../../.."
-    image_path = f"{repo_root_path}/examples/jax/keras/vit/colva_beach_sq.jpg"
+def test_image_classification(dynamic, model_dtype, colva_beach_sq, random_image):
     expected_labels = ["seashore", "sandbar", "lakeside", "promontory", "beacon"]
     quantization_dtype = "fp8_e4m3"
-
-    vit = load_model_from_preset(ViTImageClassifier, "vit_base_patch16_224_imagenet", False, model_dtype)
-    image = load_image(image_path)
+    vit = load_model_from_preset(ViTImageClassifier, "vit_base_patch16_224_imagenet", model_dtype)
 
     def calib_fn(model):
-        _ = model(create_random_image())
+        _ = model(random_image)
 
     if dynamic:
         config = DynamicQuantConfig(weight_dtype=quantization_dtype, activation_dtype=quantization_dtype)
@@ -74,5 +74,5 @@ def test_image_classification(dynamic, model_dtype):
         config = StaticQuantConfig(weight_dtype=quantization_dtype, activation_dtype=quantization_dtype)
         vit_q = quantize_model(vit, config, calib_fn)
 
-    actual_labels = classify_image(vit_q, image, len(expected_labels))
+    actual_labels = classify_image(vit_q, colva_beach_sq, len(expected_labels))
     assert expected_labels == actual_labels
