02b62ccddd6803260b1221b1a085f27432850544
Sun, Xuehao <xuehao.sun@intel.com>
2026-04-22 16:39:06 +0800

    add constraints
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/run_3x_pt_hpu.sh
+++ .azure-pipelines/scripts/ut/run_3x_pt_hpu.sh
@@ -12,9 +12,8 @@ echo "##[section]import check pass"
 # install requirements
 echo "##[group]set up UT env..."
 export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
-sed -i '/^auto-round/d;/^deepspeed/d;/^compressed-tensors/d' /neural-compressor/test/torch/requirements.txt
-pip install -r /neural-compressor/test/torch/requirements.txt
-pip install compressed-tensors>=0.15.0 deepspeed@git+https://github.com/HabanaAI/DeepSpeed.git@1.23.0 --no-deps
+sed -i '/^auto-round/d' /neural-compressor/test/torch/requirements.txt
+pip install -r /neural-compressor/test/torch/requirements.txt -c /neural-compressor/test/torch/constraints_hpu.txt
 pip install auto-round-hpu
 pip install pytest-cov pytest-html pytest-html-merger beautifulsoup4==4.13.5
 echo "##[endgroup]"
