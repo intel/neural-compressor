49048e765c32e07b5acff7025ced0ec4919e7d45
Sun, Xuehao <xuehao.sun@intel.com>
2026-04-22 13:47:03 +0800

    fix hpu install
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>

--- .azure-pipelines/scripts/ut/run_3x_pt_hpu.sh
+++ .azure-pipelines/scripts/ut/run_3x_pt_hpu.sh
@@ -14,9 +14,7 @@ echo "##[group]set up UT env..."
 export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
 sed -i '/^auto-round/d' /neural-compressor/test/torch/requirements.txt
 sed -i '/^deepspeed/d' /neural-compressor/test/torch/requirements.txt
-cat /neural-compressor/test/torch/requirements.txt
-pip install -r /neural-compressor/test/torch/requirements.txt
-pip install deepspeed@git+https://github.com/HabanaAI/DeepSpeed.git@1.23.0 --no-deps
+pip install -r /neural-compressor/test/torch/requirements.txt -c /neural-compressor/.azure-pipelines/scripts/hpu_constraints.txt
 pip install auto-round-hpu
 pip install pytest-cov pytest-html pytest-html-merger beautifulsoup4==4.13.5
 echo "##[endgroup]"
