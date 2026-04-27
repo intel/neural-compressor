59ecc836b73bd8aa67e17eeae9b52d307cd49226
chensuyue <suyue.chen@intel.com>
2026-04-27 16:37:41 +0800

    unify install
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/qwen/setup.sh
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/qwen/setup.sh
@@ -54,7 +54,7 @@ elif [[ "$DEVICE" == "gpu" ]]; then
         uv pip install vllm-qdq-plugin/ -v
     else
         # use default setting for AR format, required by fused-moe-ar
-        pip install torch==2.9.0
+        uv pip install torch==2.9.0
         git clone -b fused-moe-ar  --single-branch --quiet https://github.com/yiliu30/vllm-fork.git && cd vllm-fork
         VLLM_USE_PRECOMPILED=1 uv pip install --prerelease=allow . -v
         cd ..
