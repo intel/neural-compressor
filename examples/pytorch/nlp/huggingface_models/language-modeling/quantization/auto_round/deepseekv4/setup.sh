cd86e70a14c6c7f990501677e60c4a5b01cfe3e9
chensuyue <suyue.chen@intel.com>
2026-06-22 22:21:35 +0800

    update env setup
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/deepseekv4/setup.sh
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/deepseekv4/setup.sh
@@ -3,6 +3,7 @@ set -e
 
 uv pip install -U pip setuptools_rust setuptools_scm
 uv pip install -U evalscope lm_eval transformers datasets
+uv pip install git+https://github.com/intel/auto-round.git@main
 uv pip install compressed-tensors --no-deps
 bash <(curl -fsSL https://raw.githubusercontent.com/vllm-project/vllm/main/tools/install_deepgemm.sh)
 VLLM_USE_PRECOMPILED=1 uv pip install git+https://github.com/xin3he/vllm-fork.git@support_deepseekv4_mxfp --no-build-isolation
