0643ed8d55ed42878a441591dc17f093330b9824
chensuyue <suyue.chen@intel.com>
2026-08-12 07:43:50 +0800

    update deepgemm install
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- benchmark/agent_benchmarks/setup_vllm.sh
+++ benchmark/agent_benchmarks/setup_vllm.sh
@@ -36,8 +36,9 @@ install_deepseek_v4_vllm() {
     log "Installing DeepSeek-V4 vLLM stack"
     uv pip install -U pip setuptools_rust setuptools_scm
     uv pip install compressed-tensors --no-deps
-    bash <(curl -fsSL https://raw.githubusercontent.com/vllm-project/vllm/main/tools/install_deepgemm.sh)
     VLLM_USE_PRECOMPILED=1 uv pip install git+https://github.com/xin3he/vllm.git@support_deepseekv4_mxfp \
+        --no-build-isolation
+    pip install git+https://github.com/deepseek-ai/DeepGEMM.git@891d57b4db1071624b5c8fa0d1e51cb317fa709f \
         --no-build-isolation
 }
 
