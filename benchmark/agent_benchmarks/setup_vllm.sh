67328efff34a40f3584658bf24a48452ee5433a1
chensuyue <suyue.chen@intel.com>
2026-08-12 21:38:58 +0800

    fix(setup): update DeepGEMM installation command and add reference link
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- benchmark/agent_benchmarks/setup_vllm.sh
+++ benchmark/agent_benchmarks/setup_vllm.sh
@@ -37,9 +37,10 @@ install_deepseek_v4_vllm() {
     uv pip install -U pip setuptools_rust setuptools_scm
     uv pip install git+https://github.com/intel/auto-round.git@main
     uv pip install compressed-tensors --no-deps
-    pip install git+https://github.com/deepseek-ai/DeepGEMM.git@891d57b4db1071624b5c8fa0d1e51cb317fa709f \
-        --no-build-isolation
     VLLM_USE_PRECOMPILED=1 uv pip install git+https://github.com/xin3he/vllm.git@support_deepseekv4_mxfp \
+        --no-build-isolation
+    # reference: https://github.com/xin3he/vllm/blob/support_deepseekv4_mxfp/tools/install_deepgemm.sh 
+    uv pip install git+https://github.com/deepseek-ai/DeepGEMM.git@891d57b4db1071624b5c8fa0d1e51cb317fa709f \
         --no-build-isolation
 }
 
