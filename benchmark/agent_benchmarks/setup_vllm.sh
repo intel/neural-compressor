3f5eaa7c9f93b14fa0a8f5c0cd3cfeefcc555cc0
chensuyue <suyue.chen@intel.com>
2026-08-12 09:40:40 +0800

    minor update
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- benchmark/agent_benchmarks/setup_vllm.sh
+++ benchmark/agent_benchmarks/setup_vllm.sh
@@ -37,9 +37,9 @@ install_deepseek_v4_vllm() {
     uv pip install -U pip setuptools_rust setuptools_scm
     uv pip install git+https://github.com/intel/auto-round.git@main
     uv pip install compressed-tensors --no-deps
-    VLLM_USE_PRECOMPILED=1 uv pip install git+https://github.com/xin3he/vllm.git@support_deepseekv4_mxfp \
-        --no-build-isolation
     pip install git+https://github.com/deepseek-ai/DeepGEMM.git@891d57b4db1071624b5c8fa0d1e51cb317fa709f \
+        --no-build-isolation
+    VLLM_USE_PRECOMPILED=1 uv pip install git+https://github.com/xin3he/vllm.git@support_deepseekv4_mxfp \
         --no-build-isolation
 }
 
