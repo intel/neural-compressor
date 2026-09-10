ced0a1e5c52682e411570ebd6f420993af9a5cff
sys-lpot-val <sys_lpot_val@intel.com>
2026-09-09 17:49:44 -0700

    update dependency
    Signed-off-by: sys-lpot-val <sys_lpot_val@intel.com>

--- examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/kimi/setup.sh
+++ examples/pytorch/nlp/huggingface_models/language-modeling/quantization/auto_round/kimi/setup.sh
@@ -53,6 +53,7 @@ elif [[ "$DEVICE" == "gpu" ]]; then
         uv pip install ray
         git clone https://github.com/yiliu30/vllm-qdq-plugin.git
         uv pip install vllm-qdq-plugin/ -v
+        uv pip install flashinfer-python==0.6.18.post1
     else
         # use default setting for AR format, required by fused-moe-ar
         uv pip install torch==2.9.0
